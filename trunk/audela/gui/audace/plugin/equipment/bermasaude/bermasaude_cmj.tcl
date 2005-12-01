ef2f12fc7765c0a430cf6f2dcd607b277d043961
denismarchais <denismarchais@04217e9b-e68c-4f9b-beca-5a77afb74039>
2005-12-01 22:50:25 +0000

    no message

---
+++ trunk/audela/gui/audace/plugin/equipment/bermasaude/bermasaude_cmj.tcl
@@ -0,0 +1,13 @@
+#
+# Fichier : bermasaude_cmj.tcl
+# Description : Configuration des filtres de la roue pour une combinaison CMJ
+# Auteur : Robert DELMAS
+# Date de mise a jour : 16 mars 2005
+#
+
+set bermasaude(color_filtre_1) "white"	; # Pas de filtre
+set bermasaude(color_filtre_2) "cyan"	; # Filtre cyan
+set bermasaude(color_filtre_3) "magenta"	; # Filtre magenta
+set bermasaude(color_filtre_4) "yellow"	; # Filtre jaune
+set bermasaude(color_filtre_5) "white"	; # Pas de filtre
+
