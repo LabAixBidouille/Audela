56c96a641bb38c5cc469bd2c5eb0211cc1068644
Dark VADOR <vador@darkstar.com>
2005-12-03 21:50:39 +0000

    This commit was manufactured by cvs2svn to create tag 'HELPVIEWER-1_1'.

--- audela/gui/audace/plugin/tool/acqfc/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Date de mise a jour : 28 mars 2005
-#
-
-package ifneeded acqfc 2.1 [ list source [ file join $::audace(rep_plugin) tool acqfc acqfc.tcl ] ]
-
