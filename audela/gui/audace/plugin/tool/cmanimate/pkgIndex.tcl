f22b67e9c7240b616dc3f1ebfd1c4aad84ef4d04
Dark VADOR <vador@darkstar.com>
2005-12-03 23:05:39 +0000

    This commit was manufactured by cvs2svn to create tag 'TCLLIB-1_7'.

--- audela/gui/audace/plugin/tool/cmanimate/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Date de mise a jour : 17 octobre 2004
-#
-
-package ifneeded cmanimate 1.0 [ list source [ file join $::audace(rep_plugin) tool cmanimate cmanimate.tcl ] ]
-
