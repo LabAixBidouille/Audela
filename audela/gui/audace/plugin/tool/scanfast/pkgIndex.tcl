9804c53539495a1a96f4025b75d93e6628b8df66
Dark VADOR <vador@darkstar.com>
2005-12-03 21:31:40 +0000

    This commit was manufactured by cvs2svn to create tag 'TCLXML-2_6'.

--- audela/gui/audace/plugin/tool/scanfast/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Date de mise a jour : 17 octobre 2004
-#
-
-package ifneeded scanfast 1.0 [ list source [ file join $::audace(rep_plugin) tool scanfast scanfast.tcl ] ]
-
