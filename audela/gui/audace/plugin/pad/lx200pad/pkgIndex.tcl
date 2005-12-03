8ad5ef43a2c6bcbbc3fe4e78144daad5ebfde3a3
Dark VADOR <vador@darkstar.com>
2005-12-03 22:00:38 +0000

    This commit was manufactured by cvs2svn to create tag 'DDE-1_7_0'.

--- audela/gui/audace/plugin/pad/lx200pad/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Date de mise a jour : 17 octobre 2004
-#
-
-package ifneeded lx200pad 1.0 [ list source [ file join $::audace(rep_plugin) pad lx200pad lx200pad.tcl ] ]
-
