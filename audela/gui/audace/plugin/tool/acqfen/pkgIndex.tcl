498c0040846e347e7a289112df09cfc692e17f8c
Dark VADOR <vador@darkstar.com>
2005-12-03 23:05:40 +0000

    This commit was manufactured by cvs2svn to create tag 'TK-8_4'.

--- audela/gui/audace/plugin/tool/acqfen/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Date de mise a jour : 28 mars 2005
-#
-
-package ifneeded acqfen 1.2 [ list source [ file join $::audace(rep_plugin) tool acqfen acqfen.tcl ] ]
-
