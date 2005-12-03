7aaf9671450d572709e7d211bcb0b367b3b25b73
Dark VADOR <vador@darkstar.com>
2005-12-03 21:50:38 +0000

    This commit was manufactured by cvs2svn to create tag 'DIALOGWIN-1_1'.

--- audela/gui/audace/plugin/tool/visio/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Date de mise a jour : 28 mars 2005
-#
-
-package ifneeded visio 2.6 [ list source [ file join $::audace(rep_plugin) tool visio visio.tcl ] ]
-
