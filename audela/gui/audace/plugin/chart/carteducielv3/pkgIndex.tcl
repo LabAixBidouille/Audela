d76b1a15ce26a3a41e209405600a8e58dbef892e
Dark VADOR <vador@darkstar.com>
2005-12-03 21:50:40 +0000

    This commit was manufactured by cvs2svn to create tag 'REG-1_1'.

--- audela/gui/audace/plugin/chart/carteducielv3/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Date de mise a jour : 17 octobre 2004
-#
-
-package ifneeded carteducielv3 1.0 [ list source [ file join $::audace(rep_plugin) chart carteducielv3 carteducielv3.tcl ] ]
-
