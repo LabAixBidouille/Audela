c87447542e74b93546bbd17d44023df6e2091487
Dark VADOR <vador@darkstar.com>
2005-12-03 23:24:05 +0000

    This commit was manufactured by cvs2svn to create tag 'TKIMGVIDEO-1_1'.

--- audela/gui/audace/plugin/tool/gps/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Date de mise a jour : 28 mars 2005
-#
-
-package ifneeded gps 3.3 [ list source [ file join $::audace(rep_plugin) tool gps gps.tcl ] ]
-
--- audela/gui/audace/plugin/tool/viseur_polaire/pkgIndex.tcl
+++
@@ -1,9 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Date de mise a jour : 17 octobre 2004
-#
-
-package ifneeded viseur_polaire 1.0 [ list source [ file join $::audace(rep_plugin) tool viseur_polaire \
-   viseur_polaire_go.tcl ] ]
-
