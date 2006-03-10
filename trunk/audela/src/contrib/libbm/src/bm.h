772f5cb9f6b63f841b94a149a30d967eeeb5f885
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2006-03-10 22:20:55 +0000

    libbm  Version 1.5

--- trunk/audela/src/contrib/libbm/src/bm.h
+++ trunk/audela/src/contrib/libbm/src/bm.h
@@ -18,19 +18,8 @@
 #define PB  1
 #define PB2 2
 
-/* Declaration des structures */
-typedef struct {
-   float *ptr;           /* adresse du pointeur de l'image en interne */
-   float *ptr_audela;    /* adresse du pointeur de l'image dans AudeLA */
-   int naxis1;           /* nombre de pixels sur l'axe x */
-   int naxis2;           /* nombre de pixels sur l'axe y */
-   char dateobs[30];     /* date du debut de pose au format Fits */
-} descripteur_image;
-
 
 // --- Déclaration des fonctions ---
-int LecturePixel(descripteur_image image, int x, int y, double *pixel);
-int EcriturePixel(descripteur_image image, int x, int y, double pixel);
-int image_hard2visu(descripteur_image image, double seuil_haut, double seuil_bas, double *fonction_transfert);
-int soustrait(descripteur_image image1, descripteur_image image2);
+
+
 
