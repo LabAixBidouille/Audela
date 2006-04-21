f3a6c9551498e93714698580220d317c80af591e
denismarchais <Denis Marchais>
2006-04-21 20:32:58 +0000

    DM, compiling error fixes.

--- audela/src/external/libdcjpeg/linux/subdir.mk
+++ audela/src/external/libdcjpeg/linux/subdir.mk
@@ -19,10 +19,10 @@ libdcjpeg_dll.d \
 %.o: $(ROOT)/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	@echo gcc -I../../jpeg-6b -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -o$@ $<
-	@gcc -I../../jpeg-6b -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -o$@ $< && \
+	@echo gcc -I../jpeg-6b -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -o$@ $<
+	@gcc -I../jpeg-6b -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -o$@ $< && \
 	echo -n $(@:%.o=%.d) $(dir $@) > $(@:%.o=%.d) && \
-	gcc -MM -MG -P -w -I../../jpeg-6b -O0 -g3 -Wall -c -fmessage-length=0 -fPIC  $< >> $(@:%.o=%.d)
+	gcc -MM -MG -P -w -I../jpeg-6b -O0 -g3 -Wall -c -fmessage-length=0 -fPIC  $< >> $(@:%.o=%.d)
 	@echo 'Finished building: $<'
 	@echo ' '
 
