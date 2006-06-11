42b5dedebb02850ead58806b3b8e09c7ea525cf7
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2006-06-11 12:29:35 +0000

    modif option de compilation

--- trunk/audela/src/external/libdcjpeg/linux/src/subdir.mk
+++ trunk/audela/src/external/libdcjpeg/linux/src/subdir.mk
@@ -22,10 +22,10 @@ libdcjpeg_dll.d \
 src/%.o: $(ROOT)/src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	@echo gcc -I../jpeg-6b -O3 -Wall -c -fmessage-length=0 -o$@ $<
-	@gcc -I../jpeg-6b -O3 -Wall -c -fmessage-length=0 -o$@ $< && \
+	@echo gcc -fPIC -O2 -Wall -c -I../jpeg-6b -o$@ $<
+	@gcc -fPIC -O2 -Wall -c -I../jpeg-6b -o$@ $< && \
 	echo -n $(@:%.o=%.d) $(dir $@) > $(@:%.o=%.d) && \
-	gcc -MM -MG -P -w -I../jpeg-6b -O3 -Wall -c -fmessage-length=0  $< >> $(@:%.o=%.d)
+	gcc -fPIC -MM -MG -P -w -I../jpeg-6b -O3 -Wall -c $< >> $(@:%.o=%.d)
 	@echo 'Finished building: $<'
 	@echo ' '
 
