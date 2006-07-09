7f1d960435b3126ba522224c2c1999810b1c8209
denismarchais <Denis Marchais>
2006-07-09 09:29:38 +0000

    Modifcation du chargement de la BLT.

--- audela/gui/audace/scripts/spcaudace/spc_var.tcl
+++ audela/gui/audace/scripts/spcaudace/spc_var.tcl
@@ -13,11 +13,11 @@ global conf
 ## Il a ete necessaire que je fasse un lien symbolique : ln -s /usr/lib/libBLT.2.4.so.8.3 /usr/lib/libBLT.2.4.so
 #::console::affiche_resultat "$tcl_platform(os)\n"
 if {[string compare $tcl_platform(os) "Linux"] == 0 } {
-   load libBLT.2.4[info sharedlibextension]
+   package require BLT
+   #load libBLT.2.4[info sharedlibextension]
 } else {
    ## la lib BLT24.dll reste dans "lib" pas besoin qu'elle soit dans "system32"
    package require BLT
-   load BLT24
 }
 
 
