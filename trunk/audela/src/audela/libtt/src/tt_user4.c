86b2ebb0ee84a60fb1ae7bac977a8c8c966c050c
alainklotz <alainklotz@04217e9b-e68c-4f9b-beca-5a77afb74039>
2006-07-25 22:42:09 +0000

    Modifications for two new users (Mauclaire and Laas) : user2 and user5

--- trunk/audela/src/audela/libtt/src/tt_user4.c
+++ trunk/audela/src/audela/libtt/src/tt_user4.c
@@ -3,7 +3,7 @@
  * This file is part of the AudeLA project : <http://software.audela.free.fr>
  * Copyright (C) 1998-2004 The AudeLA Core Team
  *
- * Initial author : Alain KLOTZ <alain.klotz@free.fr>
+ * Initial author : Carole THIEBAULT <carole.thiebaut@cnes.fr>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
