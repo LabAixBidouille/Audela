90a5b8e94fada74b37a6e2d740df592c4571bf9f
robertdelmas <Robert Delmas>
2006-12-18 22:07:28 +0000

    no message

---
+++ audela/gui/audace/plugin/tool/spectro/spcaudace/plugins/specLhIII/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Date de mise a jour : 02 aout 2006 - F. Cochard (par copier-coller de pretrfc)
+#
+
+package ifneeded specLhIII 1.00 [ list source [ file join $::audace(rep_plugin) tool specLhIII specLhIII.tcl ] ]
+
