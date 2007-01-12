833f42f814a96db46638a9de859605f4ff4cbbc3
myrtillelaas <myrtillelaas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-01-12 10:58:40 +0000

    no message

--- trunk/audela/src/audela/libml/src/mltcl_1.c
+++ trunk/audela/src/audela/libml/src/mltcl_1.c
@@ -30,6 +30,539 @@
 /***************************************************************************/
 #include "mltcl.h"
 
+
+int Cmd_mltcl_getTimegps(ClientData clientData, Tcl_Interp *interp, int argc, char *argv[])
+/****************************************************************************/
+/* Recupération de données du gps                                           */
+/****************************************************************************/
+/****************************************************************************/
+{
+	int result;
+	HANDLE		hDevice;
+	FILETIME 	FileTime;
+	SYSTEMTIME_EX	SystemTime;
+	Tcl_DString dsptr;
+	char s[100];
+
+	// open device 0 for Read/Write access
+	if (TT_OpenDevice(0, GENERIC_READ | GENERIC_WRITE, &hDevice) != TT_SUCCESS)
+	{
+		sprintf(s,"%s :Error Opening Device Driver!\n", argv[0]);
+		Tcl_SetResult(interp,s,TCL_VOLATILE);
+		return TCL_ERROR;
+	}
+
+
+	if (TT_GPS_SIGNAL_INFO_NOT_AVAILABLE !=0)
+	{
+		sprintf(s,"%s :Error GPS Signal!\n", argv[0]);
+		Tcl_SetResult(interp,s,TCL_VOLATILE);
+		result = TCL_ERROR;
+	} else {
+			// Read the current Freeze Time for the desired device
+			if (TT_ReadTime(hDevice, &FileTime, TT_CONVERT_UTC2LOCAL) == TT_SUCCESS)
+		{
+			/// Convert the FileTime to SystemTimeEx format
+		if (TT_FileTimeToSystemTimeEx(&FileTime, &SystemTime) != TT_SUCCESS)
+		{
+				printf("Error converting time from file time to system time!\n");
+				result =  TT_SUCCESS;	  
+		 } else {
+			/*--- initialise la dynamic string ---*/
+			Tcl_DStringInit(&dsptr);
+			/* --- met en forme le resultat dans une chaine de caracteres ---*/
+			sprintf(s,"%d/%02d/%02d %02d:%02d:%02d.%03d%03d",SystemTime.wYear, SystemTime.wMonth, SystemTime.wDay,SystemTime.wHour, SystemTime.wMinute, SystemTime.wSecond, SystemTime.wMilliseconds, SystemTime.wMicroseconds);
+			/* --- on ajoute cette chaine a la dynamic string ---*/
+			Tcl_DStringAppend(&dsptr,s,-1);
+			/* --- a la fin, on envoie le contenu de la dynamic string dans */
+			/* --- le Result qui sera retourne a l'utilisateur. */
+			Tcl_DStringResult(interp,&dsptr);
+			/* --- desaloue la dynamic string. */
+			Tcl_DStringFree(&dsptr);
+			result = TT_SUCCESS;
+
+			}
+		}
+	}	
+	// close the device
+	TT_CloseDevice(hDevice);
+	return result;
+}
+
+
+
+int Cmd_mltcl_geostatident(ClientData clientData, Tcl_Interp *interp, int argc, char *argv[])
+/****************************************************************************/
+/* Identification des satellites en comparant les coordonnees               */
+/****************************************************************************/
+/****************************************************************************/
+/* pour tester dans Audace:*/
+/* ml_geostatident "C:/Program Files/Apache Group/Apache2/htdocs/ros/geostat/bdd0_20060927.txt" 
+"C:/Program Files/Apache Group/Apache2/htdocs/ros/geostat/bdd_20060927.txt" */
+{
+	int result,retour,diffjour,n_in,n_in1,kimage,nimages,kimage2,nimages2,k,k1,k2,k3,pareil,date,temp;
+	int nbreligneblanche,kmin,kmini,anglmin;
+	double distmin,ra0,dec0;
+	char s[1000],ligne[1000],home[35],im[40],lign[1000],toto[1000]; 
+	FILETIME ftCreate, ftAccess, ftWrite; 
+	SYSTEMTIME stUTC, stCreateLocal,stWriteLocal;
+	FILE *f_in1, *f_in2;
+	char lpszCreate[20]; 
+	char lpszWrite[20]; 
+	char tempspc[20]; 
+	SYSTEMTIME St;
+	HANDLE hFile ;
+	struct_ligsat *lignes,*lignes2;
+
+
+	if(argc<2) {
+      sprintf(s,"Usage: %s file_0 file_ident ?path_geo? ?path_http? ?url?", argv[0]);
+      Tcl_SetResult(interp,s,TCL_VOLATILE);
+      result = TCL_ERROR;
+	}  else {
+		/* --- decode les parametres obligatoires ---*/
+
+		/* --- decode les parametres facultatifs ---*/
+		 /* default: path= "C:\audela\audela\ros\src\grenouille" */
+		if(argc == 2) {
+			argv[3] = "c:/audela/audela/ros/src/grenouille/geo.txt";
+			argv[4] = "c:/Program Files/Apache Group/Apache2/htdocs/ros/tle.txt";
+			argv[5] = "celestrak.com/NORAD/elements/geo.txt";
+		} 
+		if(argc == 3) {
+			argv[4] = "c:/Program Files/Apache Group/Apache2/htdocs/ros/tle.txt";
+			argv[5] = "celestrak.com/NORAD/elements/geo.txt";
+		} 
+		if(argc == 4) {
+			argv[5] = "celestrak.com/NORAD/elements/geo.txt";
+		} 
+		/* récupère la date et heure des modifs du fichier */
+		hFile = CreateFile(argv[3],0,FILE_SHARE_READ | FILE_SHARE_WRITE,NULL,OPEN_EXISTING,0,NULL); 
+		retour = GetFileTime(hFile, &ftCreate, &ftAccess, &ftWrite);
+		
+		if (retour == 0) {
+			retour = ml_telechargertle (argv[3],argv[4],argv[5]);			
+		} else {
+			/* Converti le temps de creation en temps local */
+			FileTimeToSystemTime(&ftCreate, &stUTC); 
+			SystemTimeToTzSpecificLocalTime(NULL, &stUTC, &stCreateLocal);  
+  
+			/* Converti le temps dern.modif en temps local. */
+			FileTimeToSystemTime(&ftWrite, &stUTC); 
+			SystemTimeToTzSpecificLocalTime(NULL, &stUTC, &stWriteLocal); 
+			
+			wsprintf(lpszCreate, TEXT("%02d/%02d/%d %02d:%02d"), 
+			stCreateLocal.wMonth, stCreateLocal.wDay, stCreateLocal.wYear, 
+            stCreateLocal.wHour, stCreateLocal.wMinute); 
+  
+			wsprintf(lpszWrite, TEXT("%02d/%02d/%d %02d:%02d"), 
+            stWriteLocal.wMonth, stWriteLocal.wDay, stWriteLocal.wYear, 
+            stWriteLocal.wHour, stWriteLocal.wMinute); 
+
+			GetLocalTime(&St);
+			wsprintf(tempspc, TEXT("%02d/%02d/%d %02d:%02d"), 
+			St.wMonth, St.wDay, St.wYear, 
+            St.wHour, St.wMinute); 
+			
+			/* --- vérifie si le fichier est vieux d'un jour, si oui on le re-telecharge --- */
+			diffjour = ml_differencejour(stWriteLocal.wDay,stWriteLocal.wMonth,stWriteLocal.wYear,St.wDay,St.wMonth,St.wYear);
+			if ( diffjour>1 ){
+				retour = ml_telechargertle (argv[3],argv[4],argv[5]);
+			}
+		}
+		/* --- on fabrique un fichier_tle2=geo2.txt derriere lequel on ajoute les TLE personels --- */
+		retour = ml_file_copy ("c:/audela/audela/ros/src/grenouille/geo.txt","c:/audela/audela/ros/src/grenouille/tle2.txt");
+
+		f_in1=fopen(argv[4],"r");
+		if (f_in1==NULL) {
+			sprintf(s,"file %s not found, pas de fichier tle",argv[4]);		
+		} else {
+			n_in1=0;
+			while (feof(f_in1)==0) {
+				if (fgets(ligne,sizeof(ligne),f_in1)!=NULL) {
+					n_in1++;
+				}
+			}
+			fclose(f_in1);
+			if (n_in1!=0) {	
+				f_in2=fopen("c:/audela/audela/ros/src/grenouille/tle2.txt","a+");
+				if (f_in2==NULL) {
+					sprintf(s,"file %s not found",f_in2);					
+				}
+				n_in=0;
+				while (feof(f_in2)==0) {
+					if (fgets(ligne,sizeof(ligne),f_in2)!=NULL) {
+						n_in++;
+					}
+				}
+				/* --- dimensionne la structure des donnees d'entree ---*/
+				lignes=(struct_ligsat*)malloc(n_in1*sizeof(struct_ligsat));
+				if (lignes==NULL) {
+					sprintf(s,"error : lignes pointer out of memory (%d elements)",n_in1);
+					Tcl_SetResult(interp,s,TCL_VOLATILE);
+					return TCL_ERROR;
+				}
+				f_in1=fopen(argv[4],"r");
+				if (f_in1==NULL) {
+					sprintf(s,"file %s not found, pas de fichier tle",argv[4]);
+					Tcl_SetResult(interp,s,TCL_VOLATILE);			
+				}  
+				n_in1=0;
+				while (feof(f_in1)==0) {
+					if (fgets(ligne,sizeof(ligne),f_in1)!=NULL) {
+						strcpy(lignes[n_in1].texte,ligne);					
+						fprintf(f_in2,"%s",lignes[n_in1].texte);				
+						n_in1++;
+					}
+				}
+				fclose(f_in1);
+				fclose(f_in2);
+				free(lignes);
+			}
+		}	
+		/* est-ce que le fichier bdd exist?*/
+		f_in1=fopen(argv[2],"r");
+		if (f_in1==NULL) {	
+			f_in2=fopen(argv[1],"r");
+			if (f_in2==NULL) {
+				sprintf(s,"FILE: %s DOESN'T EXIST", argv[1]);
+				Tcl_SetResult(interp,s,TCL_VOLATILE);
+				result = TCL_ERROR;
+			} else {
+			retour = ml_file_copy (argv[1],argv[2]);
+			fclose(f_in2);
+			}
+		} else {
+			/* comme on ne recopie pas le fichier, on va chercher les lignes différentes pour compléter le fichier de sortie */
+			fclose(f_in1);
+			/* --- dimensionne la structure des donnees d'entree ---*/
+			n_in=0;
+			/* pour le fichier bdd0 */
+			f_in1=fopen(argv[1],"rt");
+			if (f_in1==NULL) {
+				sprintf(s,"file_00 %s not found",argv[1]);
+				Tcl_SetResult(interp,s,TCL_VOLATILE);
+				return TCL_ERROR;
+			}
+			while (feof(f_in1)==0) {
+				if (fgets(ligne,sizeof(ligne),f_in1)!=NULL) {
+					n_in++;
+				}
+			}							
+			lignes=(struct_ligsat*)malloc(n_in*sizeof(struct_ligsat));
+			if (lignes==NULL) {
+				sprintf(s,"error : lignes pointer out of memory (%d elements)",n_in);
+				Tcl_SetResult(interp,s,TCL_VOLATILE);
+				free(lignes);
+				return TCL_ERROR;
+			}
+			
+			fclose(f_in1);
+			n_in1=0;
+			/* pour le fichier bdd */
+			f_in2=fopen(argv[2],"rt");
+			if (f_in2==NULL) {
+				sprintf(s,"file_00 %s not found",argv[1]);
+				Tcl_SetResult(interp,s,TCL_VOLATILE);
+				return TCL_ERROR;
+			}
+			while (feof(f_in2)==0) {
+				if (fgets(ligne,sizeof(ligne),f_in2)!=NULL) {
+					n_in1++;
+				}
+			}
+			
+			lignes2=(struct_ligsat*)malloc(n_in1*sizeof(struct_ligsat));
+			if (lignes2==NULL) {
+				sprintf(s,"error : lignes pointer out of memory (%d elements)",n_in1);
+				Tcl_SetResult(interp,s,TCL_VOLATILE);
+				return TCL_ERROR;
+			}
+			fclose(f_in2);
+
+			n_in=0;
+			date=0;
+			kimage=0;
+			k=0;
+			f_in1=fopen(argv[1],"rt");
+			if (f_in1==NULL) {
+				sprintf(s,"file_00 %s not found",argv[1]);
+				Tcl_SetResult(interp,s,TCL_VOLATILE);
+				return TCL_ERROR;
+			}
+			while (feof(f_in1)==0) {
+				 if (fgets(ligne,sizeof(ligne),f_in1)!=NULL) {
+					strcpy(lignes[n_in].texte,ligne);
+					if (strlen(ligne)>=3) {
+						if ((ligne[0]=='I')&&(ligne[1]=='M')&&(ligne[2]=='_')) {
+							lignes[n_in].comment=0;
+						}
+						kimage++;
+						if (date == 0) {
+							lignes[n_in].nouvelledate=1;
+						} else {
+							lignes[n_in].nouvelledate=date;
+						}
+					
+					} else {						
+						if (n_in <= 2) {
+							date=0;
+							lignes[n_in].nouvelledate=1;
+						} else {	
+							k++;
+							date=k+1;
+							lignes[n_in].nouvelledate=k;
+						}
+					}
+				}	 
+				n_in++;
+			}
+			fclose(f_in1);
+			nimages=kimage-2;
+			n_in=n_in-1;
+		
+			kimage2=0;
+			k=0;
+			n_in1=0;
+			f_in2=fopen(argv[2],"rt");
+			if (f_in2==NULL) {
+				sprintf(s,"file_00 %s not found",argv[1]);
+				Tcl_SetResult(interp,s,TCL_VOLATILE);
+				return TCL_ERROR;
+			}
+			while (feof(f_in2)==0) {
+				 if (fgets(ligne,sizeof(ligne),f_in2)!=NULL) {
+					strcpy(lignes2[n_in1].texte,ligne);
+					if (strlen(ligne)>=3) {
+						if ((ligne[0]=='I')&&(ligne[1]=='M')&&(ligne[2]=='_')) {
+							lignes2[n_in1].comment=0;
+						}
+						kimage2++;
+					}	
+				 }
+				 n_in1++;
+			}
+			fclose(f_in2);
+			nimages2=kimage2-2;	
+			n_in1=n_in1-1;
+			/* --- on cherche les lignes qui manquent dans le fichier de sortie --- */	
+			/*tester*/
+			for (k=0;k<n_in;k++) {
+				if (lignes[k].comment!=0) {
+					lignes[k].kimage1 = 0;
+					continue;
+				}
+				for (k2=0;k2<n_in1;k2++) {
+					if (lignes2[k2].comment!=0) {
+						continue;
+					}
+					if (lignes2[k2].kimage1 == 0) {
+						continue;
+					}
+					pareil=0;
+					for (k1=0; k1<140;k1++) {
+						if (lignes2[k2].texte [k1] == lignes[k].texte [k1]) {
+							pareil++;
+						} else {
+							break;
+						}
+						if (pareil>=120) {
+							lignes[k].kimage1 = 0;
+							lignes2[k2].kimage1 = 0;
+							break;		
+						}
+					}
+					if (lignes[k].kimage1 == 0) {
+					break;
+					}
+				}
+			}
+			/* --- on sauve le resultat dans le fichier de sortie ---*/
+			f_in1=fopen(argv[2],"a+");
+			if (f_in1==NULL) {
+				sprintf(s,"file_0 %s not created",argv[1]);
+				Tcl_SetResult(interp,s,TCL_VOLATILE);
+				free(lignes);
+				free(lignes2);	
+				return TCL_ERROR;
+			}
+			temp=0;	
+			for (k=0;k<n_in;k++) {
+				if (lignes[k].kimage1 != 0) {	
+					/* on a une nouvelle ligne*/
+					/*il faut copier lignes[k1].ligne dans lignes[nimages2+1]*/	
+					if (temp == 0) {
+						fprintf(f_in1,"\n%s",lignes[k].texte);
+					} else {
+						
+						if ((lignes[k].nouvelledate != lignes[temp].nouvelledate)&&(temp!=0)) {
+							fprintf(f_in1,"\n%s",lignes[k].texte);
+						} else {
+							fprintf(f_in1,"%s",lignes[k].texte);
+						}
+					}
+					temp=k;
+				}
+			}				
+			fclose(f_in1);
+			free(lignes); 
+			free(lignes2);
+		}
+		
+		n_in1=0;
+		f_in2=fopen(argv[2],"rt");
+		if (f_in2==NULL) {
+			sprintf(s,"file_0 %s not found",argv[2]);
+			Tcl_SetResult(interp,s,TCL_VOLATILE);
+			return TCL_ERROR;
+		}
+		n_in1=0;
+		while (feof(f_in2)==0) {
+			if (fgets(ligne,sizeof(ligne),f_in2)!=NULL) {
+				n_in1++;
+			}
+		}
+		fclose(f_in2);
+		/* --- on recupère les données du fichier de sortie ---*/
+		lignes2=(struct_ligsat*)malloc(n_in1*sizeof(struct_ligsat));
+		if (lignes2==NULL) {
+			sprintf(s,"error : lignes pointer out of memory (%d elements)",n_in1);
+			Tcl_SetResult(interp,s,TCL_VOLATILE);
+			return TCL_ERROR;
+		}
+		lignes=(struct_ligsat*)malloc(n_in1*sizeof(struct_ligsat));
+		if (lignes==NULL) {
+			sprintf(s,"error : lignes pointer out of memory (%d elements)",n_in1);
+			Tcl_SetResult(interp,s,TCL_VOLATILE);
+			return TCL_ERROR;
+		}
+		n_in1=0;
+		kimage2=0;
+		strcpy(s,"");
+		nbreligneblanche=0;
+		f_in2=fopen(argv[2],"rt");
+		if (f_in2==NULL) {
+			sprintf(s,"file_00 %s not found",argv[1]);
+			Tcl_SetResult(interp,s,TCL_VOLATILE);
+			return TCL_ERROR;
+		}
+
+		while (feof(f_in2)==0) {
+			if (fgets(ligne,sizeof(ligne),f_in2)!=NULL) {
+				strcpy(lignes2[n_in1].texte,ligne);
+				if (n_in1==0) {
+					for (k=90;k<130;k++){
+						if ((ligne[k]=='G')&&(ligne[k+1]=='P')&&(ligne[k+2]=='S')) {
+							for (k2=k+4;k2<145;k2++){
+								if (ligne[k2]== ')') {
+									for (k3=k;k3<k2;k3++) { s[k3-k]=ligne[k3]; } ; s[k3-k]='\0';
+									strcpy(home,s);
+									break;
+								}
+							}
+							break;
+						}
+					}
+				}
+				if (strlen(ligne)>=3) {
+					if ((ligne[0]=='I')&&(ligne[1]=='M')&&(ligne[2]=='_')) {
+						lignes2[n_in1].comment=0;
+					}
+					kimage2++;
+				}
+				if (lignes2[n_in1].comment==0) {
+					k1=146 ; k2=149 ; for (k=k1;k<=k2;k++) { s[k-k1]=ligne[k]; } ; s[k-k1]='\0';
+					strcpy(lignes2[n_in1].ident,s);
+					if (lignes2[n_in1].ident == "    ") {
+						/* le satellite n'est pas identifiée */
+						k1=  38 ; k2= 60 ; for (k=k1;k<=k2;k++) { s[k-k1]=ligne[k]; } ; s[k-k1]='\0';
+						strcpy(im,s);
+						k1=104 ; k2=113 ; for (k=k1;k<=k2;k++) { s[k-k1]=ligne[k]; } ; s[k-k1]='\0';
+						lignes[n_in].dec=atof(s);
+						k1= 93 ; k2= 101 ; for (k=k1;k<=k2;k++) { s[k-k1]=ligne[k]; } ; s[k-k1]='\0';
+						lignes[n_in].ra=atof(s);
+						
+						strcpy(toto,"c:/toto.elm");
+						sprintf(lign,"Cmd_mctcl_tle2ephem %s",toto);
+						result = Tcl_Eval(interp,lign);
+
+						if (result==TCL_OK) {
+								// recupérer le résultat (cf. usage de la fonction
+								// Tcl_SplitList dans libmc/src/libmc_dates.c)
+						}
+
+/* atttention*////////////////////////////////////////////////////////////////////
+						/*ephemsat = Cmd_mctcl_tle2ephem(im,"c:/audela/audela/ros/src/grenouille/tle2.txt",home);*/
+												
+						// Cmd_mctcl_tle2ephem("2003-09-23T20:30:00.00" "C:/audela/audela/ros/src/grenouille/tle2.txt" "gps 6.92389 e 43.75222 1270" "TELECOM 2D");
+
+						/*renvoie une liste de listes des coord de tous les satellites dans le fichier tl2
+						de la forme:{{{ETS 8} {29656U} { 06059A   07004.5}} 140.581000136064720 -5.622260786338347 0.000311221351737812 0.000000000000000}
+						element 0 ; nom...
+						element 1 : ra 
+						element 2 : dec*/
+/* atttention*////////////////////////////////////////////////////////////////////
+						
+						kmin=0;
+						kmini=-1;
+						distmin=1e20;
+						anglmin=0;
+
+						ra0=lignes[n_in].ra;
+						dec0=lignes[n_in].dec;
+						 /*pour chauqe élémnt de ephemsat faire: {
+/* atttention*////////////////////////////////////////////////////////////////////						
+							//distang = mc_anglesep (ra0,dec0,ra,dec);
+							/*distang est une liste de deux éléments: dist et angl
+							/* a initialiser et déclarer: dist et angl (double), valid (int)
+/* atttention*////////////////////////////////////////////////////////////////////
+
+						/*	if (dist <= distmin) {
+								distmin = dist;
+								kmini=kmin;
+								anglmin=angl;
+							}	
+							kmin++;						
+						}*/
+						/*if (distmin<=0.3) {
+							valid=1;
+						} else {
+							valid=0;
+						}
+						if (kmini>=0) {
+							/* il faut rajouter à ligne[].texte satelname,noradname et cosparname
+						} else {
+						  on rajoute rien à  ligne[].texte
+						}*/
+
+					} else {
+						/* le satellite est deja identifiée */
+					}
+				
+				} else {
+					strcpy(lignes[n_in1].texte,"");
+					nbreligneblanche++; //a revoir car compte les deux premières lignes
+				}
+				if (n_in1<=2) {
+					strcpy(lignes[n_in1].texte,ligne);
+				}
+			}
+			n_in1++;
+		}
+		fclose(f_in2);
+		nimages2=kimage2-2;
+
+		free(lignes2);
+		free(lignes);
+
+		result = TCL_OK;
+	}
+	
+	return result;
+}
+
+
+
 int Cmd_mltcl_geostatreduc(ClientData clientData, Tcl_Interp *interp, int argc, char *argv[])
 /****************************************************************************/
 /* Reduction des objets susceptibles etre des satellites geostationnaires.  */
