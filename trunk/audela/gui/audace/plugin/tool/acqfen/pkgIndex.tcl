41751303611abbda2e3601f40dbd4cdee5e6ee16
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-01-13 21:14:29 +0000

    no message

--- trunk/audela/gui/audace/plugin/tool/acqfen/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/tool/acqfen/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.2 2006-06-20 20:44:46 robertdelmas Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.3 2007-01-13 21:14:02 robertdelmas Exp $
 #
 
-package ifneeded acqfen 1.2 [ list source [ file join $::audace(rep_plugin) tool acqfen acqfen.tcl ] ]
+package ifneeded acqfen 1.2.1 [ list source [ file join $::audace(rep_plugin) tool acqfen acqfen.tcl ] ]
 
--- trunk/audela/gui/audace/plugin/tool/visio/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/tool/visio/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.2 2006-06-20 22:01:28 robertdelmas Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.3 2007-01-13 21:14:29 robertdelmas Exp $
 #
 
-package ifneeded visio 2.6 [ list source [ file join $::audace(rep_plugin) tool visio visio.tcl ] ]
+package ifneeded visio 2.6.2 [ list source [ file join $::audace(rep_plugin) tool visio visio.tcl ] ]
 
