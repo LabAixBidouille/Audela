d78b437d0895ee8694e3dc06630d0ef922654652
robertdelmas <Robert Delmas>
2007-01-13 21:14:29 +0000

    no message

--- audela/gui/audace/plugin/link/quickremote/pkgIndex.tcl
+++ audela/gui/audace/plugin/link/quickremote/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.2 2006-06-20 19:24:57 robertdelmas Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.3 2007-01-13 21:13:50 robertdelmas Exp $
 #
 
-package ifneeded quickremote 1.0 [ list source [ file join $::audace(rep_plugin) link quickremote quickremote.tcl ] ]
+package ifneeded quickremote 1.1 [ list source [ file join $::audace(rep_plugin) link quickremote quickremote.tcl ] ]
 
--- audela/gui/audace/plugin/tool/pretrfc/pkgIndex.tcl
+++ audela/gui/audace/plugin/tool/pretrfc/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.2 2006-06-20 21:19:34 robertdelmas Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.3 2007-01-13 21:14:15 robertdelmas Exp $
 #
 
-package ifneeded pretrfc 1.38 [ list source [ file join $::audace(rep_plugin) tool pretrfc pretrfc.tcl ] ]
+package ifneeded pretrfc 1.40 [ list source [ file join $::audace(rep_plugin) tool pretrfc pretrfc.tcl ] ]
 
