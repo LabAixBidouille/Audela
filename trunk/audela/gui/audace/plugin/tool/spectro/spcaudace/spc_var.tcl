b42f3c52fb066329dc037745b1788b2089e82a17
denismarchais <denismarchais@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-01-13 09:03:37 +0000

    Bad loading of BLT under linux corrected.

--- trunk/audela/gui/audace/plugin/tool/spectro/spcaudace/spc_var.tcl
+++ trunk/audela/gui/audace/plugin/tool/spectro/spcaudace/spc_var.tcl
@@ -12,26 +12,13 @@ global conf
 ## ***** Chargement de la lib BLT *****
 ## Il a ete necessaire que je fasse un lien symbolique : ln -s /usr/lib/libBLT.2.4.so.8.3 /usr/lib/libBLT.2.4.so
 #::console::affiche_resultat "$tcl_platform(os)\n"
-if {[string compare $tcl_platform(os) "Linux"] == 0 } {
-   ##load libBLT.2.4[info sharedlibextension]
-   package require BLT
-   load libBLT.2.4[info sharedlibextension].$tcl_version
-
-   #package require BLT
-   #load $audace(rep_install)/lib/blt2.4/BLT24[info sharedlibextension]
-
-} else {
-   ## la lib BLT24.dll reste dans "lib" pas besoin qu'elle soit dans "system32"
-   package require BLT
-   load BLT24
-}
+package require BLT
+#load $audace(rep_install)/lib/blt2.4/BLT24[info sharedlibextension]
 
 
 #----------------------------------------------------------------------------------#
 set extsp "dat"
 set extdflt "fit"
-
-
 
 
 #----------------------------------------------------------------------------------#
