ed147a91ce0e1669af5bb3e8f6971da2ae036f11
denismarchais <Denis Marchais>
2007-01-22 21:53:28 +0000

    Compilation bug corrected on Linux for libgphoto2.

--- audela/src/external/libgphoto2/libgphoto2/linux/subdir.mk
+++ audela/src/external/libgphoto2/libgphoto2/linux/subdir.mk
@@ -62,7 +62,7 @@ C_DEPS += \
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I../../linux -I../../libgphoto2_port/libgphoto2_port -I.. -O3 -w -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	gcc -I../../linux -I../../libltdl -I../../libgphoto2_port/libgphoto2_port -I.. -O3 -w -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
