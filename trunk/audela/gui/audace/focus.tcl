542a8db0bdb4e8b522bc46a8817c96c5f4a02a32
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-02-03 20:22:48 +0000

    Formatage

--- trunk/audela/gui/audace/focus.tcl
+++ trunk/audela/gui/audace/focus.tcl
@@ -2,18 +2,19 @@
 # Fichier : focus.tcl
 # Description : Centralise les commandes du focus du telescope
 # Auteur : Michel PUJOL
-# Mise a jour $Id: focus.tcl,v 1.4 2007-01-31 22:52:56 michelpujol Exp $
+# Mise a jour $Id: focus.tcl,v 1.5 2007-02-03 20:22:48 robertdelmas Exp $
 #
 
 namespace eval ::focus {
 
 }
+
 #------------------------------------------------------------
 # ::focus::move
 #   demarre/arrete le mouvement du focuseur
 #     si command = "-" , demarre le mouvement du focus en intra focale
 #     si command = "+" , demarre le mouvement du focus en extra focale
-#     si command = "stop" , arrete le mouvement 
+#     si command = "stop" , arrete le mouvement
 # Exemple :
 #    ::focus::move "focuserquickr" "+"
 #    ::focus::move "focuserquickr" "-"
@@ -30,9 +31,6 @@ proc ::focus::move { focuserLabel command } {
 #     incremente la vitesse du focus et appelle la procedure setSpeed
 #------------------------------------------------------------
 proc ::focus::incrementSpeed { focuserLabel} {
-   global conf
-   global audace
-
    if { "$focuserLabel" != "" } {
       ::$focuserLabel\::incrementSpeed
    }
@@ -40,14 +38,14 @@ proc ::focus::incrementSpeed { focuserLabel} {
 
 #------------------------------------------------------------
 #  ::focus::setSpeed
-#     change la vitesse du focus 
+#     change la vitesse du focus
 #------------------------------------------------------------
 proc ::focus::setSpeed { focuserLabel { value "0" } } {
    if { "$focuserLabel" != "" } {
       ::$focuserLabel\::setSpeed $value
    }
 }
-   
+
 #------------------------------------------------------------
 #  ::focus::possedeControleEtendu
 #     retourne 1 si le telescope possede un controle etendu du focus (AudeCom)
