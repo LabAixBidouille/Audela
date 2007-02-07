94538bbee497a145c9ef3597addf83580665900f
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-02-07 18:56:34 +0000

    Formatage

--- trunk/audela/gui/audace/plugin/equipment/bermasaude/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/equipment/bermasaude/pkgIndex.tcl
@@ -1,9 +1,10 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.3 2007-01-31 22:43:12 michelpujol Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.4 2007-02-07 18:55:53 robertdelmas Exp $
 #
 
 package ifneeded bermasaude 1.0 [ list source [ file join $::audace(rep_plugin) equipment bermasaude bermasaude.tcl ] ]
 #--- je retourne le namespace du plugin
 return "bermasaude"
+
--- trunk/audela/gui/audace/plugin/equipment/focuserjmi/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/equipment/focuserjmi/pkgIndex.tcl
@@ -1,9 +1,10 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.1 2007-01-31 22:46:50 michelpujol Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.2 2007-02-07 18:56:13 robertdelmas Exp $
 #
 
 package ifneeded focuserjmi 1.0 [ list source [ file join $::audace(rep_plugin) equipment focuserjmi focuserjmi.tcl ] ]
 #--- je retourne le namespace du plugin
 return "focuserjmi"
+
