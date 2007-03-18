f10f02799a544b9672b66748ee554a8787f10b82
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-03-18 11:22:33 +0000

    Suppression du fichier pkgIndex.tcl du répertoire 'specLHIII' : Ce n'est plus un outil.

--- trunk/audela/gui/audace/plugin/tool/spectro/spcaudace/plugins/specLhIII/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.3 2007-01-02 16:11:13 robertdelmas Exp $
-#
-
-package ifneeded specLhIII 1.0 [ list source [ file join $::audace(rep_plugin) tool specLhIII specLhIII.tcl ] ]
-
