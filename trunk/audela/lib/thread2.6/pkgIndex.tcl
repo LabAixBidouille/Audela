11c00c49ddfca611798f88ed1aefa2aa3ebb0399
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-04-07 15:01:25 +0000

    update version number 2.6.3

--- trunk/audela/lib/thread2.6/pkgIndex.tcl
+++ trunk/audela/lib/thread2.6/pkgIndex.tcl
@@ -2,7 +2,7 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 8.4]} {
-    package ifneeded Thread 2.6 [list thread_load $dir]
+    package ifneeded Thread 2.6.3 [list thread_load $dir]
     proc thread_load {dir} {
         load [file join $dir thread26.dll]
     }
