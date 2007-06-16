624a88ef9c854359176ee27f44e7d885d3e10204
robertdelmas <Robert Delmas>
2007-06-16 11:01:02 +0000

    Script nouveau pour la 'pluginisation' des caméras.

---
+++ audela/gui/audace/plugin/camera/th7852a/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Mise a jour $Id: pkgIndex.tcl,v 1.1 2007-06-16 11:00:31 robertdelmas Exp $
+#
+
+package ifneeded th7852a 1.0 [ list source [ file join $dir th7852a.tcl ] ]
+
