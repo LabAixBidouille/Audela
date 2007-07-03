7a68bde5044727baf83a84f57f31456168293a98
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-07-03 20:23:28 +0000

    modification de la version du plugin updateaudela

--- trunk/audela/gui/audace/plugin/tool/updateaudela/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/tool/updateaudela/pkgIndex.tcl
@@ -1,7 +1,7 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.2 2007-06-29 22:42:39 michelpujol Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.3 2007-07-03 20:23:28 michelpujol Exp $
 #
 
 package ifneeded updateaudela 1.1 [ list source [ file join $dir updateaudela.tcl ] ]
