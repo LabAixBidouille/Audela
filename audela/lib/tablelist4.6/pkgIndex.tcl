80419aab6f3b5f8358b6c2bc16f38fbd8ed598e4
Michel PUJOL <michel-pujol@orange.fr>
2007-07-04 20:16:06 +0000

    no message

---
+++ audela/lib/tablelist4.6/pkgIndex.tcl
@@ -0,0 +1,26 @@
+#==============================================================================
+# Tablelist and Tablelist_tile package index file.
+#
+# Copyright (c) 2000-2007  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+#==============================================================================
+
+#
+# Regular packages:
+#
+package ifneeded tablelist         4.6 \
+	[list source [file join $dir tablelist.tcl]]
+package ifneeded tablelist_tile    4.6 \
+	[list source [file join $dir tablelist_tile.tcl]]
+
+#
+# Aliases:
+#
+package ifneeded Tablelist         4.6 { package require tablelist }
+package ifneeded Tablelist_tile    4.6 { package require tablelist_tile }
+
+#
+# Code common to all packages:
+#
+package ifneeded tablelist::common 4.6 \
+        "namespace eval ::tablelist { proc DIR {} {return [list $dir]} } ;\
+	 source [list [file join $dir tablelistPublic.tcl]]"
