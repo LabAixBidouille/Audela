71aab45914bcb8bc38f33e27272e947145fe8d66
alainklotz <alainklotz@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-08-11 09:31:13 +0000

    options dans ros

--- trunk/audela/gui/audace/ros.tcl
+++ trunk/audela/gui/audace/ros.tcl
@@ -2,10 +2,18 @@
 # Fichier : ros.tcl
 # Description : Function to launch Robotic Observatory Software installation
 # Auteur : Alain KLOTZ
-# Mise a jour $Id: ros.tcl,v 1.2 2007-03-12 22:54:39 robertdelmas Exp $
+# Mise a jour $Id: ros.tcl,v 1.3 2007-08-11 09:31:13 alainklotz Exp $
 #
 
-proc ros { } {
-   source [pwd]/../ros/ros_install.tcl
+proc ros { {action install} } {
+	global ros
+	set err [catch {wm withdraw .} msg]
+	set ros(withtk) 1
+	if {$action=="install"} {
+		if {$err==1} {
+			set ros(withtk) 0
+		}
+		source [pwd]/../ros/ros_install.tcl
+	}
 }
 
