1ef535b937281c18b8e9e2dac05949d001ae568d
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-09-01 16:24:54 +0000

    Changement du nom du namespace : 'ccdcolor' devient 'acqcolor'. Création de la procédure getPluginHelp et sa mise en oeuvre.

--- trunk/audela/gui/audace/plugin/tool/acqcolor/acqcolor_go.tcl
+++ trunk/audela/gui/audace/plugin/tool/acqcolor/acqcolor_go.tcl
@@ -3,14 +3,14 @@
 # Description : Outil pour l'acquisition d'images en couleur
 # Compatibilite : Cameras Audine Couleur et SCR1300XTC
 # Auteur : Alain KLOTZ
-# Mise a jour $Id: acqcolor_go.tcl,v 1.9 2007-05-06 14:28:46 robertdelmas Exp $
+# Mise a jour $Id: acqcolor_go.tcl,v 1.10 2007-09-01 16:24:54 robertdelmas Exp $
 #
 
 #============================================================
-# Declaration du namespace ccdcolor
+# Declaration du namespace acqcolor
 #    initialise le namespace
 #============================================================
-namespace eval ::ccdcolor {
+namespace eval ::acqcolor {
    package provide acqcolor 1.0
 
    #--- Chargement des captions pour recuperer le titre utilise par getPluginLabel
@@ -18,32 +18,40 @@ namespace eval ::ccdcolor {
 }
 
 #------------------------------------------------------------
-# ::ccdcolor::getPluginTitle
+# ::acqcolor::getPluginTitle
 #    retourne le titre du plugin dans la langue de l'utilisateur
 #------------------------------------------------------------
-proc ::ccdcolor::getPluginTitle { } {
+proc ::acqcolor::getPluginTitle { } {
    global caption
 
    return "$caption(acqcolor_go,acqcolor)"
 }
 
 #------------------------------------------------------------
-# ::ccdcolor::getPluginType
+#  ::acqcolor::getPluginHelp
+#     retourne le nom du fichier d'aide principal
+#------------------------------------------------------------
+proc ::acqcolor::getPluginHelp { } {
+   return "acqcolor.htm"
+}
+
+#------------------------------------------------------------
+# ::acqcolor::getPluginType
 #    retourne le type de plugin
 #------------------------------------------------------------
-proc ::ccdcolor::getPluginType { } {
+proc ::acqcolor::getPluginType { } {
    return "tool"
 }
 
 #------------------------------------------------------------
-# ::ccdcolor::getPluginProperty
+# ::acqcolor::getPluginProperty
 #    retourne la valeur de la propriete
 #
 # parametre :
 #    propertyName : nom de la propriete
 # return : valeur de la propriete ou "" si la propriete n'existe pas
 #------------------------------------------------------------
-proc ::ccdcolor::getPluginProperty { propertyName } {
+proc ::acqcolor::getPluginProperty { propertyName } {
    switch $propertyName {
       function     { return "acquisition" }
       subfunction1 { return "color" }
@@ -52,26 +60,26 @@ proc ::ccdcolor::getPluginProperty { propertyName } {
 }
 
 #------------------------------------------------------------
-# ::ccdcolor::initPlugin
+# ::acqcolor::initPlugin
 #    initialise le plugin
 #------------------------------------------------------------
-proc ::ccdcolor::initPlugin { tkbase } {
+proc ::acqcolor::initPlugin { tkbase } {
 
 }
 
 #------------------------------------------------------------
-# ::ccdcolor::createPluginInstance
+# ::acqcolor::createPluginInstance
 #    cree une nouvelle instance de l'outil
 #------------------------------------------------------------
-proc ::ccdcolor::createPluginInstance { { in "" } { visuNo 1 } } {
-   ::ccdcolor::createPanel $in.ccdcolor
+proc ::acqcolor::createPluginInstance { { in "" } { visuNo 1 } } {
+   ::acqcolor::createPanel $in.acqcolor
 }
 
 #------------------------------------------------------------
-# ::ccdcolor::deletePluginInstance
+# ::acqcolor::deletePluginInstance
 #    suppprime l'instance du plugin
 #------------------------------------------------------------
-proc ::ccdcolor::deletePluginInstance { visuNo } {
+proc ::acqcolor::deletePluginInstance { visuNo } {
    global audace
 
    if { [ winfo exists $audace(base).test ] } {
@@ -80,31 +88,31 @@ proc ::ccdcolor::deletePluginInstance { visuNo } {
 }
 
 #------------------------------------------------------------
-# ::ccdcolor::createPanel
+# ::acqcolor::createPanel
 #    prepare la creation de la fenetre de l'outil
 #------------------------------------------------------------
-proc ::ccdcolor::createPanel { this } {
+proc ::acqcolor::createPanel { this } {
    variable This
    global caption panneau
 
    #--- Initialisation du nom de la fenetre
    set This $this
    #--- Initialisation des captions
-   set panneau(ccdcolor,titre)  "$caption(acqcolor_go,acqcolor)"
-   set panneau(ccdcolor,aide)   "$caption(acqcolor_go,help_titre)"
-   set panneau(ccdcolor,titre1) "$caption(acqcolor_go,kaf0400)"
-   set panneau(ccdcolor,titre2) "$caption(acqcolor_go,kaf1600)"
-   set panneau(ccdcolor,titre3) "$caption(acqcolor_go,kac1310)"
-   set panneau(ccdcolor,acq)    "$caption(acqcolor_go,acqvisu)"
+   set panneau(acqcolor,titre)  "$caption(acqcolor_go,acqcolor)"
+   set panneau(acqcolor,aide)   "$caption(acqcolor_go,help_titre)"
+   set panneau(acqcolor,titre1) "$caption(acqcolor_go,kaf0400)"
+   set panneau(acqcolor,titre2) "$caption(acqcolor_go,kaf1600)"
+   set panneau(acqcolor,titre3) "$caption(acqcolor_go,kac1310)"
+   set panneau(acqcolor,acq)    "$caption(acqcolor_go,acqvisu)"
    #--- Construction de l'interface
-   ::ccdcolor::ccdcolorBuildIF $This
+   ::acqcolor::acqcolorBuildIF $This
 }
 
 #------------------------------------------------------------
-# ::ccdcolor::startTool
+# ::acqcolor::startTool
 #    affiche la fenetre de l'outil
 #------------------------------------------------------------
-proc ::ccdcolor::startTool { visuNo } {
+proc ::acqcolor::startTool { visuNo } {
    variable This
 
    #--- Chargement de la librairie de definition de la commande combit
@@ -116,20 +124,20 @@ proc ::ccdcolor::startTool { visuNo } {
 }
 
 #------------------------------------------------------------
-# ::ccdcolor::stopTool
+# ::acqcolor::stopTool
 #    masque la fenetre de l'outil
 #------------------------------------------------------------
-proc ::ccdcolor::stopTool { visuNo } {
+proc ::acqcolor::stopTool { visuNo } {
    variable This
 
    pack forget $This
 }
 
 #------------------------------------------------------------
-# ::ccdcolor::ccdcolorBuildIF
+# ::acqcolor::acqcolorBuildIF
 #    cree la fenetre de l'outil
 #------------------------------------------------------------
-proc ::ccdcolor::ccdcolorBuildIF { This } {
+proc ::acqcolor::acqcolorBuildIF { This } {
    global audace panneau
 
    frame $This -borderwidth 2 -relief groove
@@ -138,24 +146,24 @@ proc ::ccdcolor::ccdcolorBuildIF { This } {
       frame $This.fra1 -borderwidth 2 -relief groove
 
          #--- Label du titre
-         Button $This.fra1.but -borderwidth 1 -text $panneau(ccdcolor,titre) \
-            -command "::audace::showHelpPlugin tool acqcolor acqcolor.htm"
+         Button $This.fra1.but -borderwidth 1 -text $panneau(acqcolor,titre) \
+            -command "::audace::showHelpPlugin [ ::acqcolor::getPluginType ] acqcolor [ ::acqcolor::getPluginHelp ]"
          pack $This.fra1.but -in $This.fra1 -anchor center -expand 1 -fill both -side top -ipadx 5
-         DynamicHelp::add $This.fra1.but -text $panneau(ccdcolor,aide)
+         DynamicHelp::add $This.fra1.but -text $panneau(acqcolor,aide)
 
       pack $This.fra1 -side top -fill x
 
       #--- Frame Kaf-400 Couleur
       frame $This.fra2 -borderwidth 1 -relief groove
 
-         label $This.fra2.lab1 -borderwidth 0 -text $panneau(ccdcolor,titre1)
+         label $This.fra2.lab1 -borderwidth 0 -text $panneau(acqcolor,titre1)
          pack $This.fra2.lab1 -in $This.fra2  -anchor center -fill none -padx 4 -pady 1
 
          #--- Bouton d'ouverture de l'outil d'acquisition
-         button $This.fra2.but1 -borderwidth 2 -text $panneau(ccdcolor,acq) \
+         button $This.fra2.but1 -borderwidth 2 -text $panneau(acqcolor,acq) \
             -command {
                set audace(acqvisu,ccd) "kaf400"
-               set audace(acqvisu,ccd_model) $panneau(ccdcolor,titre1)
+               set audace(acqvisu,ccd_model) $panneau(acqcolor,titre1)
                source [ file join $audace(rep_plugin) tool acqcolor acqcolor.tcl ]
             }
          pack $This.fra2.but1 -in $This.fra2 -anchor center -fill none -pady 5 -ipadx 5 -ipady 3
@@ -165,14 +173,14 @@ proc ::ccdcolor::ccdcolorBuildIF { This } {
       #--- Frame Kaf-1600 Couleur
       frame $This.fra3 -borderwidth 1 -relief groove
 
-         label $This.fra3.lab1 -borderwidth 0 -text "$panneau(ccdcolor,titre2)"
+         label $This.fra3.lab1 -borderwidth 0 -text "$panneau(acqcolor,titre2)"
          pack $This.fra3.lab1 -in $This.fra3  -anchor center -fill none -padx 4 -pady 1
 
          #--- Bouton d'ouverture de l'outil d'acquisition
-         button $This.fra3.but1 -borderwidth 2 -text $panneau(ccdcolor,acq) \
+         button $This.fra3.but1 -borderwidth 2 -text $panneau(acqcolor,acq) \
             -command {
                set audace(acqvisu,ccd) "kaf1600"
-               set audace(acqvisu,ccd_model) $panneau(ccdcolor,titre2)
+               set audace(acqvisu,ccd_model) $panneau(acqcolor,titre2)
                source [ file join $audace(rep_plugin) tool acqcolor acqcolor.tcl ]
             }
          pack $This.fra3.but1 -in $This.fra3 -anchor center -fill none -pady 5 -ipadx 5 -ipady 3
@@ -182,14 +190,14 @@ proc ::ccdcolor::ccdcolorBuildIF { This } {
       #--- Frame Kac-1310 Couleur
       frame $This.fra4 -borderwidth 1 -relief groove
 
-         label $This.fra4.lab1 -borderwidth 0 -text "$panneau(ccdcolor,titre3)"
+         label $This.fra4.lab1 -borderwidth 0 -text "$panneau(acqcolor,titre3)"
          pack $This.fra4.lab1 -in $This.fra4  -anchor center -fill none -padx 4 -pady 1
 
          #--- Bouton d'ouverture de l'outil d'acquisition
-         button $This.fra4.but1 -borderwidth 2 -text $panneau(ccdcolor,acq) \
+         button $This.fra4.but1 -borderwidth 2 -text $panneau(acqcolor,acq) \
             -command {
                set audace(acqvisu,ccd) "kac1310"
-               set audace(acqvisu,ccd_model) $panneau(ccdcolor,titre3)
+               set audace(acqvisu,ccd_model) $panneau(acqcolor,titre3)
                source [ file join $audace(rep_plugin) tool acqcolor acqcolor.tcl ]
             }
          pack $This.fra4.but1 -in $This.fra4 -anchor center -fill none -pady 5 -ipadx 5 -ipady 3
