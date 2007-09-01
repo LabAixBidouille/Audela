a4598a5a64f6562167c1f6fa26f16d05848a0409
robertdelmas <Robert Delmas>
2007-09-01 12:27:18 +0000

    Changement du nom du namespace : 'Obj_Lune_Go' devient 'obj_lune'. Création de la procédure getPluginHelp et sa mise en oeuvre.

--- audela/gui/audace/plugin/tool/obj_lune/obj_lune_go.tcl
+++ audela/gui/audace/plugin/tool/obj_lune/obj_lune_go.tcl
@@ -2,14 +2,14 @@
 # Fichier : obj_lune_go.tcl
 # Description : Outil pour le lancement d'Objectif Lune
 # Auteur : Robert DELMAS
-# Mise a jour $Id: obj_lune_go.tcl,v 1.10 2007-05-06 14:56:24 robertdelmas Exp $
+# Mise a jour $Id: obj_lune_go.tcl,v 1.11 2007-09-01 12:27:18 robertdelmas Exp $
 #
 
 #============================================================
-# Declaration du namespace obj_lune_go
+# Declaration du namespace obj_lune
 #    initialise le namespace
 #============================================================
-namespace eval ::obj_lune_go {
+namespace eval ::obj_lune {
    package provide obj_lune 1.0
 
    #--- Chargement des captions pour recuperer le titre utilise par getPluginLabel
@@ -17,32 +17,40 @@ namespace eval ::obj_lune_go {
 }
 
 #------------------------------------------------------------
-# ::obj_lune_go::getPluginTitle
+# ::obj_lune::getPluginTitle
 #    retourne le titre du plugin dans la langue de l'utilisateur
 #------------------------------------------------------------
-proc ::obj_lune_go::getPluginTitle { } {
+proc ::obj_lune::getPluginTitle { } {
    global caption
 
    return "$caption(obj_lune_go,obj_lune)"
 }
 
 #------------------------------------------------------------
-# ::obj_lune_go::getPluginType
+#  ::obj_lune::getPluginHelp
+#     retourne le nom du fichier d'aide principal
+#------------------------------------------------------------
+proc ::obj_lune::getPluginHelp { } {
+   return "obj_lune.htm"
+}
+
+#------------------------------------------------------------
+# ::obj_lune::getPluginType
 #    retourne le type de plugin
 #------------------------------------------------------------
-proc ::obj_lune_go::getPluginType { } {
+proc ::obj_lune::getPluginType { } {
    return "tool"
 }
 
 #------------------------------------------------------------
-# ::obj_lune_go::getPluginProperty
+# ::obj_lune::getPluginProperty
 #    retourne la valeur de la propriete
 #
 # parametre :
 #    propertyName : nom de la propriete
 # return : valeur de la propriete ou "" si la propriete n'existe pas
 #------------------------------------------------------------
-proc ::obj_lune_go::getPluginProperty { propertyName } {
+proc ::obj_lune::getPluginProperty { propertyName } {
    switch $propertyName {
       function     { return "aiming" }
       subfunction1 { return "moon" }
@@ -51,18 +59,18 @@ proc ::obj_lune_go::getPluginProperty { propertyName } {
 }
 
 #------------------------------------------------------------
-# ::obj_lune_go::initPlugin
+# ::obj_lune::initPlugin
 #    initialise le plugin
 #------------------------------------------------------------
-proc ::obj_lune_go::initPlugin { tkbase } {
+proc ::obj_lune::initPlugin { tkbase } {
 
 }
 
 #------------------------------------------------------------
-# ::obj_lune_go::createPluginInstance
+# ::obj_lune::createPluginInstance
 #    cree une nouvelle instance de l'outil
 #------------------------------------------------------------
-proc ::obj_lune_go::createPluginInstance { { in "" } { visuNo 1 } } {
+proc ::obj_lune::createPluginInstance { { in "" } { visuNo 1 } } {
    global audace
 
    #--- Chargement du package Img pour visualiser les cartes de la Lune au format jpg
@@ -72,27 +80,27 @@ proc ::obj_lune_go::createPluginInstance { { in "" } { visuNo 1 } } {
 }
 
 #------------------------------------------------------------
-# ::obj_lune_go::deletePluginInstance
+# ::obj_lune::deletePluginInstance
 #    suppprime l'instance du plugin
 #------------------------------------------------------------
-proc ::obj_lune_go::deletePluginInstance { visuNo } {
+proc ::obj_lune::deletePluginInstance { visuNo } {
 
 }
 
 #------------------------------------------------------------
-# ::obj_lune_go::startTool
+# ::obj_lune::startTool
 #    affiche la fenetre de l'outil
 #------------------------------------------------------------
-proc ::obj_lune_go::startTool { visuNo } {
+proc ::obj_lune::startTool { visuNo } {
    #--- J'ouvre la fenetre
-   ::obj_Lune::run
+   ::obj_lune::run
 }
 
 #------------------------------------------------------------
-# ::obj_lune_go::stopTool
+# ::obj_lune::stopTool
 #    masque la fenetre de l'outil
 #------------------------------------------------------------
-proc ::obj_lune_go::stopTool { visuNo } {
+proc ::obj_lune::stopTool { visuNo } {
    #--- Rien a faire, car la fenetre est fermee par l'utilisateur
 }
 
