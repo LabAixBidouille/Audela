b5af06c1122af9d9dc586890d920591a79ef8e3f
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-09-06 17:51:16 +0000

    Passage à la version 1.16 de l'outil King.

--- trunk/audela/gui/audace/plugin/tool/methking/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/tool/methking/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.3 2007-04-07 00:38:34 robertdelmas Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.4 2007-09-06 17:51:16 robertdelmas Exp $
 #
 
-package ifneeded methking 1.14 [ list source [ file join $dir methking.tcl ] ]
+package ifneeded methking 1.16 [ list source [ file join $dir methking.tcl ] ]
 
