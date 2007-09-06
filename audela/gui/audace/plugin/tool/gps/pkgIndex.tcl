2b0a8db17912359e1184799518df3309b92df2bf
robertdelmas <Robert Delmas>
2007-09-06 17:34:00 +0000

    Passage à la version 3.4 de l'outil GPS.

--- audela/gui/audace/plugin/tool/gps/pkgIndex.tcl
+++ audela/gui/audace/plugin/tool/gps/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.3 2007-04-07 00:38:34 robertdelmas Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.4 2007-09-06 17:34:00 robertdelmas Exp $
 #
 
-package ifneeded gps 3.3 [ list source [ file join $dir gps.tcl ] ]
+package ifneeded gps 3.4 [ list source [ file join $dir gps.tcl ] ]
 
