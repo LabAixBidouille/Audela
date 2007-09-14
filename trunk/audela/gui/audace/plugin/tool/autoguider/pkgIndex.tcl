5626ded2a236cd9d8c230e55004000479d72225b
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-09-14 13:38:11 +0000

    ajout getPluginHelp , startCenterStar , startSearchStar

--- trunk/audela/gui/audace/plugin/tool/autoguider/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/tool/autoguider/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.4 2007-07-06 22:29:49 michelpujol Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.5 2007-09-14 13:38:11 michelpujol Exp $
 #
 
-package ifneeded autoguider 1.1 [ list source [ file join $dir autoguider.tcl ] ]
+package ifneeded autoguider 1.2 [ list source [ file join $dir autoguider.tcl ] ]
 
