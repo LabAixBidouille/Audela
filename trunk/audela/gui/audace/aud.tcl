7a46d3e0b8db38768e7413e317afbc70d819087e
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-10-14 15:49:02 +0000

    Suppression de l'initialisation de la variable 'audace(camNo)' à '0' (homogénéisation).

--- trunk/audela/gui/audace/aud.tcl
+++ trunk/audela/gui/audace/aud.tcl
@@ -2,7 +2,7 @@
 # Fichier : aud.tcl
 # Description : Fichier principal de l'application Aud'ACE
 # Auteur : Denis MARCHAIS
-# Mise a jour $Id: aud.tcl,v 1.77 2007-09-20 20:33:18 robertdelmas Exp $
+# Mise a jour $Id: aud.tcl,v 1.78 2007-10-14 15:49:02 robertdelmas Exp $
 
 #--- Chargement du package BWidget
 package require BWidget
@@ -194,8 +194,6 @@ namespace eval ::audace {
       set confgene(posobs,observateur,gps) $conf(posobs,observateur,gps)
       set audace(posobs,observateur,gps)   $confgene(posobs,observateur,gps)
       set confgene(fichier,compres)        $conf(fichier,compres)
-      #---
-      set audace(camNo) "0"
    }
 
    proc Default_exeutils { } {
