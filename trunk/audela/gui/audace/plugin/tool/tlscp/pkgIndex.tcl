4c0bb16c9a6dab937734376c4fc59e55c71f07ac
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-11-03 17:38:23 +0000

    Transport des scripts du répertoire 'tel' vers le répertoire 'tlscp'.

---
+++ trunk/audela/gui/audace/plugin/tool/tlscp/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Mise a jour $Id: pkgIndex.tcl,v 1.1 2007-11-03 17:38:23 robertdelmas Exp $
+#
+
+package ifneeded tlscp 1.1 [ list source [ file join $dir tlscp.tcl ] ]
+
