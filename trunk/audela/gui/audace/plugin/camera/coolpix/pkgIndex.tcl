c889cc1783f84b8ca83a47acff40afd260b9c056
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-11-08 21:35:03 +0000

    Suppression du plugin CoolPix.

--- trunk/audela/gui/audace/plugin/camera/coolpix/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.1 2007-06-16 10:58:36 robertdelmas Exp $
-#
-
-package ifneeded coolpix 1.0 [ list source [ file join $dir coolpix.tcl ] ]
-
