01ab9975ecfa419665cbc4fe2e001f008e846226
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2007-11-17 11:49:06 +0000

    Suppression des catch abusifs (2) de la procédure 'CemesDispTemp'.

--- trunk/audela/gui/audace/plugin/camera/cemes/cemes.tcl
+++ trunk/audela/gui/audace/plugin/camera/cemes/cemes.tcl
@@ -2,7 +2,7 @@
 # Fichier : cemes.tcl
 # Description : Configuration de la camera Cemes
 # Auteur : Robert DELMAS
-# Mise a jour $Id: cemes.tcl,v 1.25 2007-11-09 23:45:57 michelpujol Exp $
+# Mise a jour $Id: cemes.tcl,v 1.26 2007-11-17 11:49:06 robertdelmas Exp $
 #
 
 namespace eval ::cemes {
@@ -301,17 +301,19 @@ proc ::cemes::stop { camItem } {
 #
 proc ::cemes::CemesDispTemp { camItem } {
    variable private
-   global audace caption
+   global caption
 
-   catch {
+   if { [ info exists private(frm) ] } {
       set frm $private(frm)
-      if { [ winfo exists $frm ] == "1" && [ catch { set temp_ccd [ cam$private($camItem,camNo) temperature ] } ] == "0" } {
+      if { [ winfo exists $frm.frame1.frame3.frame5.frame7.temp_ccd ] == "1" && [ catch { set temp_ccd [ cam$private($camItem,camNo) temperature ] } ] == "0" } {
          set temp_ccd [ format "%+5.2f" $temp_ccd ]
          $frm.frame1.frame3.frame5.frame7.temp_ccd configure \
             -text "$caption(cemes,temperature_CCD) $temp_ccd $caption(cemes,deg_c)"
          set private(aftertemp) [ after 5000 ::cemes::CemesDispTemp $camItem ]
       } else {
-         catch { unset private(aftertemp) }
+         if { [ info exists private(aftertemp) ] == "0" } {
+            unset private(aftertemp)
+         }
       }
    }
 }
