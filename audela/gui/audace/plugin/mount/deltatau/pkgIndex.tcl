64a97578e47837b8f5c5421cff4bb435d97cdd90
alainklotz <Alain Klotz>
2007-12-19 01:10:40 +0000

    Interface utilisateur libtel DELTATAU (AK)

---
+++ audela/gui/audace/plugin/mount/deltatau/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Mise a jour $Id: pkgIndex.tcl,v 1.1 2007-12-19 01:10:40 alainklotz Exp $
+#
+
+package ifneeded deltatau 1.0 [ list source [ file join $dir deltatau.tcl ] ]
+
