343c27bb142b564906fe46576c75dd17312e316b
jacquesmichelet <jacquesmichelet@04217e9b-e68c-4f9b-beca-5a77afb74039>
2008-01-20 19:12:38 +0000

    Version 3.5 de gps : -- message d'erreur spécifique si l'utlisateur n'a aps les droits d'acces à l'horloge systeme de sa machine -- 2 caption rajoutes

--- trunk/audela/gui/audace/plugin/tool/gps/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/tool/gps/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.4 2007-09-06 17:34:00 robertdelmas Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.5 2008-01-20 19:12:38 jacquesmichelet Exp $
 #
 
-package ifneeded gps 3.4 [ list source [ file join $dir gps.tcl ] ]
+package ifneeded gps 3.5 [ list source [ file join $dir gps.tcl ] ]
 
