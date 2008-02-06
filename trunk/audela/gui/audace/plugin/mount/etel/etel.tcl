4998bda3f2f5f5bb693eb7acc6a036650de4aab4
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2008-02-06 21:48:35 +0000

    Remplacement de la propriété "multiMount" par "multiMountOuranos".

--- trunk/audela/gui/audace/plugin/mount/etel/etel.tcl
+++ trunk/audela/gui/audace/plugin/mount/etel/etel.tcl
@@ -2,7 +2,7 @@
 # Fichier : etel.tcl
 # Description : Configuration de la monture Etel
 # Auteur : Alain KLOTZ
-# Mise a jour $Id: etel.tcl,v 1.5 2008-02-02 11:31:12 robertdelmas Exp $
+# Mise a jour $Id: etel.tcl,v 1.6 2008-02-06 21:47:50 robertdelmas Exp $
 #
 
 namespace eval ::etel {
@@ -340,9 +340,9 @@ proc ::etel::confEtelInactif { } {
 #    propertyName : Nom de la propriete
 # return : Valeur de la propriete ou "" si la propriete n'existe pas
 #
-# multiMount :            Retourne la possibilite de connecter plusieurs montures differentes (1 : Oui, 0 : Non)
-# name :                  Retourne le modele de la monture
-# product :               Retourne le nom du produit
+# multiMountOuranos       Retourne la possibilite de se connecter avec Ouranos (1 : Oui, 0 : Non)
+# name                    Retourne le modele de la monture
+# product                 Retourne le nom du produit
 # hasCoordinates          Retourne la possibilite d'afficher les coordonnees
 # hasGoto                 Retourne la possibilite de faire un Goto
 # hasMatch                Retourne la possibilite de faire un Match
@@ -355,7 +355,7 @@ proc ::etel::getPluginProperty { propertyName } {
    variable private
 
    switch $propertyName {
-      multiMount              { return 0 }
+      multiMountOuranos       { return 0 }
       name                    {
          if { $private(telNo) != "0" } {
             return [ tel$private(telNo) name ]
