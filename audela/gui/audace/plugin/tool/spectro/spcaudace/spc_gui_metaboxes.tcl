36f4072cb8db31a42a89090279b298e52c72babe
bmauclaire <Benjixc@users.noreply.github.com>
2008-03-02 20:08:34 +0000

    Correction des pbs 0D0A en 0A

--- audela/gui/audace/plugin/tool/spectro/spcaudace/spc_gui_metaboxes.tcl
+++ audela/gui/audace/plugin/tool/spectro/spcaudace/spc_gui_metaboxes.tcl
@@ -5,7 +5,7 @@
 #*********************************************************************************#
 # Chargement : source $audace(rep_scripts)/spcaudace/spc_gui_boxes.tcl
 
-# Mise a jour $Id: spc_gui_metaboxes.tcl,v 1.11 2008-03-01 20:18:26 bmauclaire Exp $
+# Mise a jour $Id: spc_gui_metaboxes.tcl,v 1.12 2008-03-02 20:03:41 bmauclaire Exp $
 
 
 
