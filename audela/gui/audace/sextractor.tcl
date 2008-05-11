39459b8af62f5c03d88ff76a1cf6bbf9ae572631
alainklotz <Alain Klotz>
2008-05-11 06:20:19 +0000

    ajout pour linux executable

--- audela/gui/audace/sextractor.tcl
+++ audela/gui/audace/sextractor.tcl
@@ -2,7 +2,7 @@
 # Fichier : sextractor.tcl
 # Description : Functions to initialize configuration files for sextractor
 # Auteur : Alain KLOTZ
-# Mise a jour $Id: sextractor.tcl,v 1.7 2008-05-11 06:11:03 bmauclaire Exp $
+# Mise a jour $Id: sextractor.tcl,v 1.8 2008-05-11 06:20:19 alainklotz Exp $
 #
 
 global audace
@@ -272,8 +272,15 @@ proc sextractor { args } {
    set exefile [ file join ${pathbin} sextractor.exe ]
    set k [file exists "$exefile"]
    if {$k==0} {
-      # set exefile [ file join ${pathbin} sex.exe ]
+      set exefile [ file join ${pathbin} sex.exe ]
+      set k [file exists "$exefile"]
+   }
+   if {$k==0} {
       set exefile [ file join ${pathbin} sex ]
+      set k [file exists "$exefile"]
+   }
+   if {$k==0} {
+      set exefile [ file join ${pathbin} sextractor ]
       set k [file exists "$exefile"]
    }
    if {$k==0} {
