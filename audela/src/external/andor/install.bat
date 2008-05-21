aaf59ab378631375d08da8b9ec6d5e71e979e56a
alainklotz <Alain Klotz>
2008-05-21 15:55:30 +0000

    correction d'un bug de include

--- audela/src/external/andor/install.bat
+++ audela/src/external/andor/install.bat
@@ -1,6 +1,7 @@
 set ANDOR=andor
 mkdir ..\include
 mkdir ..\lib
-copy %ANDOR%\include\atmcd32d.h ..\include
+copy %ANDOR%\include\Atmcd32d.h ..\include
+copy %ANDOR%\include\Atmcd32d.h ..\include\win
 copy %ANDOR%\lib\atmcd32m.lib ..\lib
 copy %ANDOR%\bin\*.* ..\..\..\bin
