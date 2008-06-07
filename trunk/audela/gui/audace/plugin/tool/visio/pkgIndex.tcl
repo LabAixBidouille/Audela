f705196823df892587531c37beb28a5fd455e6ad
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2008-06-07 09:17:50 +0000

    Mise à jour du numéro de version.

--- trunk/audela/gui/audace/plugin/tool/visio/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/tool/visio/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.4 2007-04-07 00:38:36 robertdelmas Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.5 2008-06-07 09:17:50 robertdelmas Exp $
 #
 
-package ifneeded visio 2.6.2 [ list source [ file join $dir visio.tcl ] ]
+package ifneeded visio 2.6.5 [ list source [ file join $dir visio.tcl ] ]
 
