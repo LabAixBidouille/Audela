70021c3c477329370eeb47d94c270c2c9c8f0c38
bmauclaire <bmauclaire@04217e9b-e68c-4f9b-beca-5a77afb74039>
2008-06-08 11:05:32 +0000

    Correction de petits bugs et ameliorations de la gestion des variables defocntionnement pour les dependances selon les distros.

--- trunk/audela/src/packaging/make_packages.sh
+++ trunk/audela/src/packaging/make_packages.sh
@@ -1,28 +1,39 @@
 #!/bin/sh
 #
-# daily_package.sh [debian/ubuntu/mandriva]
+# make_packages.sh [debian/ubuntu/mandriva]
 #
-#   Ce script permet de generer une archive deb et rpm a partir d'un repertoire de dev.
+#     Ce script permet de generer une archive deb et rpm a partir d'un repertoire de dev.
+#
+# Version initiale : Denis Marchais
+# Version actuelle : Benjamin Mauclaire
 #
 
+
+#--- Variables de focntionnement :
+depends_debian="libc6, libgcc1, libgphoto2-2, libgsl0, libstdc++6, libusb-0.1-4, tcl8.4, tk8.4, tclthread, libx11-6, libxau6, gnuplot-x11, gzip, tclxml, tcllib, tclvfs, libtk-img, blt"
+depends_ubuntu="libc6, libgcc1, libgphoto2-2, libgsl0ldbl, libstdc++6, libusb-0.1-4, tcl8.4, tk8.4, gnuplot-x11, gzip, tclxml, tcllib, tclvfs, libtk-img, blt"
+depends_mandriva="libc6, libgcc1, libgphoto2-2, libgsl0ldbl, libstdc++6, libusb-0.1-4, tcl8.4, tk8.4, gnuplot-x11, gzip, tclxml, tcllib, tclvfs, libtk-img, blt"
+
+
+#--- Choix de la distro et parametrage en consequence :
 if test "$1" = "debian"
 then
     echo "La distribution est une Debian."
-    depends="libc6, libgcc1, libgphoto2-2, libgsl0, libstdc++6, libusb-0.1-4, tcl8.4, tk8.4, tclthread, libx11-6, libxau6, gnuplot-x11, gzip, tclxml, tcllib, tclvfs, libtk-img, blt"
+    depends="$depends_debian"
     ladistro="$1"
 elif test "$1" = "ubuntu"
 then
     echo "La distribution est une *Ubuntu."
-    depends="libc6, libgcc1, libgphoto2-2, libgsl0ldbl, libstdc++6, libusb-0.1-4, tcl8.4, tk8.4, gnuplot-x11, gzip, tclxml, tcllib, tclvfs, libtk-img, blt"
+    depends="$depends_ubuntu"
     ladistro="$1"
 elif test "$1" = "mandriva"
 then
     echo "La distribution est une Mandriva."
-    depends="libc6, libgcc1, libgphoto2-2, libgsl0ldbl, libstdc++6, libusb-0.1-4, tcl8.4, tk8.4, gnuplot-x11, gzip, tclxml, tcllib, tclvfs, libtk-img, blt"
+    depends="$depends_mandriva"
     ladistro="$1"
 elif "$1" = "-h"
 then
-    echo "daily_package_bm [debian/ubuntu/mandriva]"
+    echo "make_packages.sh [debian/ubuntu/mandriva]"
     exit 0
 elif test $# = 0
 then
@@ -33,24 +44,25 @@ then
     if test "`expr "$distro" : '.*\(Debian\).*'`" = "Debian"
     then
         echo "La distribution est une Debian."
-        depends="libc6, libgcc1, libgphoto2-2, libgsl0, libstdc++6, libusb-0.1-4, tcl8.4, tk8.4, tclthread, libx11-6, libxau6, gnuplot-x11, gzip, tclxml, tcllib, tclvfs, libtk-img, blt"
+        depends="$depends_debian"
         ladistro="debian"
     elif test "`expr "$distro" : '.*\(buntu\).*'`" = "buntu"
     then
         echo "La distribution est une *Ubuntu."
-        depends="libc6, libgcc1, libgphoto2-2, libgsl0ldbl, libstdc++6, libusb-0.1-4, tcl8.4, tk8.4, gnuplot-x11, gzip, tclxml, tcllib, tclvfs, libtk-img, blt"
+        depends="$depends_ubuntu"
         ladistro="ubuntu"
     else
         echo "La distribution est inconnue."
-        depends="libc6, libgcc1, libgphoto2-2, libgsl0ldbl, libstdc++6, libusb-0.1-4, tcl8.4, tk8.4, gnuplot-x11, gzip, tclxml, tcllib, tclvfs, libtk-img, blt"
+        depends="$depends_mandriva"
         ladistro="mandriva"
     fi
 else
-    echo "daily_package_bm [debian/ubuntu/mandriva]"
+    echo "make_packages.sh [debian/ubuntu/mandriva]"
     exit 0
 fi
 
 
+#--- Construit ne numero de version :
 #DAILY=20080509
 #DAILY=`date +"%Y%m%d"`
 noteversion=`grep "audela(version)" ../../bin/version.tcl | tr -d '"'`
@@ -65,7 +77,7 @@ BUILD_DIR=audela-$DAILY
 INST_DIR=/usr/lib/audela-$DAILY
 DIRECTORY=$BUILD_DIR$INST_DIR
 
-#--- Menage
+#--- Menage :
 sudo rm -rf audela-*
 #rm -f $BUILD_DIR.deb
 rm -rf rpm
@@ -73,7 +85,7 @@ rm -rf rpm
 #--- Creation des fichiers necessaire a l'empaquetage :
 echo "Creation des fichiers necessaire a l'empaquetage..."
 
-if test -e ../../bin/libtcl8.4.so
+if test -e ../../bin/libtcl8.4.so*
 then
     nom_paquet="audela-thread"
     lesuffixe="thread"
@@ -93,23 +105,34 @@ Depends: $depends
 Installed-Size: 57521
 Maintainer: Benjamin MAUCLAIRE <bmauclaire@gmail.com>
 Provides: audela
-Description: Logiciel libre multiplateforme (Win32, Linux, Mac), permettant les acquisitions CCD, le pilotage de télescopes, le traitement et l'exploitation des images. Automatisation possible grace aux scripts TCL. Il se couple avec Cartes du Ciel pour le pointage. Homepage : http://www.audela.org.
+Description: Logiciel libre multiplateforme (Win32, Linux, Mac), permettant les acquisitions CCD, le pilotage de telescopes, le traitement et l'exploitation des images. Automatisation possible grace aux scripts TCL. Il se couple avec Cartes du Ciel pour le pointage. Homepage : http://www.audela.org.
 " > $BUILD_DIR/DEBIAN/control
 
-echo "#! /bin/bash
+echo "#!/bin/sh
+set -e
 ln -s $INST_DIR/bin/audela.sh /usr/bin/audela
+# Compense un probleme dans l'edition de lien d'Audela : il lui faut lib...so au lieu de .so.0. Mais c'est mal de faire ainsi !
 if test -e /usr/lib/libtcl8.4.so ; then echo "" ; else ln -s /usr/lib/libtcl8.4.so.0 /usr/lib/libtcl8.4.so ; fi
 if test -e /usr/lib/libtcl8.4.so ; then echo "" ; else ln -s /usr/lib/libtk8.4.so.0 /usr/lib/libtk8.4.so ; fi
+# Automatically added by dh_installmenu
+if [ "$1" = "configure" ] && [ -x "`which update-menus 2>/dev/null`" ]; then
+        update-menus
+fi
+# End automatically added section
 " > $BUILD_DIR/DEBIAN/postinst
 chmod 555 $BUILD_DIR/DEBIAN/postinst
 
-echo "#! /bin/bash
+echo "#!/bin/sh
+set -e
 rm -f /usr/bin/audela
+# Automatically added by dh_installmenu
+if [ -x "`which update-menus 2>/dev/null`" ]; then update-menus ; fi
+# End automatically added section
 " > $BUILD_DIR/DEBIAN/postrm
 chmod 555 $BUILD_DIR/DEBIAN/postrm
 
 
-#--- Creation de l'arborescence, copie des repertoires entiers
+#--- Creation de l'arborescence, copie des repertoires entiers :
 echo "Creation de l'arborescence, copie des repertoires entiers..."
 mkdir -p $DIRECTORY
 cp -r ../../bin $DIRECTORY
@@ -117,7 +140,7 @@ cp -r ../../lib $DIRECTORY
 cp -r ../../gui $DIRECTORY
 cp -r ../../images $DIRECTORY
 
-#--- Petit menage
+#--- Petit menage :
 cp ../COPYING $DIRECTORY
 cp ../../readme.txt $DIRECTORY
 rm -f $DIRECTORY/bin/version.tcl.in
@@ -130,18 +153,18 @@ cp $DIRECTORY/readme.txt $DIRECTORY/bin/audela.txt
 
 
 
-#--- Creation du fichier de demarrage de audela
-echo "#!/bin/bash
+#--- Creation du fichier de demarrage d'Audela :
+echo "#!/bin/sh
 export LD_LIBRARY_PATH=$INST_DIR/bin:\$LD_LIBRARY_PATH
 cd $INST_DIR/bin
-./audela $*
+./audela \$*
 " > $DIRECTORY/bin/audela.sh
 chmod +x $DIRECTORY/bin/audela.sh
 sudo chown -R root.root $DIRECTORY/*
 #sudo chmod a+w $DIRECTORY/bin/audace.txt
 
 
-#--- Creation du paquet
+#--- Creation du paquet :
 echo "Creation du paquet Debian..."
 sudo dpkg -b $BUILD_DIR $BUILD_DIR-$lesuffixe.deb
 luser=`whoami`
@@ -149,7 +172,7 @@ sudo chown $luser.$luser *.deb
 echo "Paquet DEB cree."
 
 
-#--- Creation du rpm
+#--- Creation du rpm :
 if test "$ladistro" = "mandriva"
 then
     echo "Creation du paquet RPM..."
