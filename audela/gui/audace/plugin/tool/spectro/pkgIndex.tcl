f90db212b2cf89440d591a747dad9fba8def322e
bmauclaire <Benjixc@users.noreply.github.com>
2008-06-14 16:26:21 +0000

    Effacement du repertoire spectro car spcaudace a demenage dans plugin/tool.

--- audela/gui/audace/plugin/tool/spectro/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.3 2007-04-07 00:38:35 robertdelmas Exp $
-#
-
-package ifneeded spectro 1.0 [ list source [ file join $dir spectro.tcl ] ]
-
