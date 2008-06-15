926f21d37c17bf8947a9ac4b3a2d7cf5b2c2b049
Michel PUJOL <michel-pujol@orange.fr>
2008-06-15 16:27:12 +0000

    modification de l'arret des acquisitions et activation du multithread

--- audela/gui/audace/plugin/tool/acqfc/pkgIndex.tcl
+++ audela/gui/audace/plugin/tool/acqfc/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.4 2008-04-18 16:53:17 robertdelmas Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.5 2008-06-15 16:27:12 michelpujol Exp $
 #
 
-package ifneeded acqfc 3.0 [ list source [ file join $dir acqfc.tcl ] ]
+package ifneeded acqfc 4.0 [ list source [ file join $dir acqfc.tcl ] ]
 
