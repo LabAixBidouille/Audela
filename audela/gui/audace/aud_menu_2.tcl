5f274b9b469f3e31cdc0f75c07e20d107a6df813
robertdelmas <Robert Delmas>
2008-10-03 16:39:29 +0000

    Suppression de caractères frauduleux.

--- audela/gui/audace/aud_menu_2.tcl
+++ audela/gui/audace/aud_menu_2.tcl
@@ -1,7 +1,7 @@
-﻿#
+#
 # Fichier : aud_menu_2.tcl
 # Description : Script regroupant les fonctionnalites du menu Affichage
-# Mise a jour $Id: aud_menu_2.tcl,v 1.10 2008-10-03 15:56:29 robertdelmas Exp $
+# Mise a jour $Id: aud_menu_2.tcl,v 1.11 2008-10-03 16:39:29 robertdelmas Exp $
 #
 
 namespace eval ::audace {
@@ -182,7 +182,7 @@ namespace eval ::audace {
             }
          }
          4 {
-            #--- Fonction de transfert arctangente / sigmoïde
+            #--- Fonction de transfert arctangente / sigmo�de
             if {$conf(fonction_transfert,param4)==0} {
                #--- On est ramene au cas lineaire
                visu$visuNo pal $fichier_palette
