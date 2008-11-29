d21ff035c15b75eea4ced43eded629aae0439d92
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2008-11-29 14:31:47 +0000

    no message

---
+++ trunk/audela/src/external/libthread/thread26/generic/psGdbm.h
@@ -0,0 +1,26 @@
+/*
+ * psGdbm.h --
+ *
+ * See the file "license.txt" for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ * Rcsid: @(#)$Id: psGdbm.h,v 1.1 2008-11-29 14:31:47 michelpujol Exp $
+ * ---------------------------------------------------------------------------
+ */
+
+#ifndef _PSGDBM_H_
+#define _PSGDBM_H_
+
+void Sv_RegisterGdbmStore();
+
+#endif /* _PSGDBM_H_ */
+
+/* EOF $RCSfile */
+
+/* Emacs Setup Variables */
+/* Local Variables:      */
+/* mode: C               */
+/* indent-tabs-mode: nil */
+/* c-basic-offset: 4     */
+/* End:                  */
+
