a27f610e1a7ffd3429f5fce0221aac66655ee289
denismarchais <Denis Marchais>
2008-11-29 23:10:57 +0000

    Suppression tcllib v1.7.

--- audela/lib/tcllib1.7/http/pkgIndex.tcl
+++
@@ -1,6 +0,0 @@
-# pkgIndex.tcl for the tcllib http module.
-#
-# $Id: pkgIndex.tcl,v 1.1 2005/02/15 21:12:42 Administrateur Exp $
-
-if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded autoproxy 1.2.0 [list source [file join $dir autoproxy.tcl]]
