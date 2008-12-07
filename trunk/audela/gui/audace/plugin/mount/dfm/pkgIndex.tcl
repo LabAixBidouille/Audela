00f7c5eb257e50a7f4b634c4036965ae1457aefe
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2008-12-07 15:07:07 +0000

    Nouveau script (plugin monture DFM).

---
+++ trunk/audela/gui/audace/plugin/mount/dfm/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Mise a jour $Id: pkgIndex.tcl,v 1.1 2008-12-07 15:06:47 robertdelmas Exp $
+#
+
+package ifneeded dfm 1.0 [ list source [ file join $dir dfm.tcl ] ]
+
