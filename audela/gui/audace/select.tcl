597a7777a8480fd0696f356ed517c353377cffcc
Michel PUJOL <michel-pujol@orange.fr>
2008-12-07 22:01:55 +0000

    remplace This.fra2 par This.tool.fra2 a cause de la nouvelle gestion des outils dans la visu

--- audela/gui/audace/select.tcl
+++ audela/gui/audace/select.tcl
@@ -1,7 +1,7 @@
 #
 # Fichier : select.tcl
 # Description : Interface permettant la selection d'images
-# Mise a jour $Id: select.tcl,v 1.7 2007-02-10 17:47:24 robertdelmas Exp $
+# Mise a jour $Id: select.tcl,v 1.8 2008-12-07 22:01:55 michelpujol Exp $
 #
 
 namespace eval ::selectWindow {
@@ -36,67 +36,68 @@ namespace eval ::selectWindow {
       wm resizable $This 1 1
       wm protocol $This WM_DELETE_WINDOW ::selectWindow::end
 
-      frame $This.fra2 -bd 2 -relief groove
-
-      frame $This.fra2.f -bd 2 -relief groove
-         label $This.fra2.f.titre -text "$caption(select,entree)"
-         label $This.fra2.f.lne -text "$caption(select,nom)"
-         entry $This.fra2.f.ene -width 15 -textvariable selectWindow(nomEntree)
-         button $This.fra2.f.explore -text "$caption(select,parcourir)" -width 1 -command { ::selectWindow::parcourir 1 }
-         label $This.fra2.f.lie -text "$caption(select,index)"
-         entry $This.fra2.f.eie -width 3 -textvariable selectWindow(indexEntree)
-         grid $This.fra2.f.titre -row 0 -column 0 -columnspan 2 -sticky {}
-         grid $This.fra2.f.lne -row 1 -column 0 -sticky w
-         grid $This.fra2.f.ene -row 1 -column 1 -sticky e
-         grid $This.fra2.f.explore -row 1 -column 2 -sticky e -padx 10 -ipady 5
-         grid $This.fra2.f.lie -row 2 -column 0 -sticky w
-         grid $This.fra2.f.eie -row 2 -column 1 -sticky e
-
-      frame $This.fra2.g -bd 2 -relief groove
-         label $This.fra2.g.titre -text "$caption(select,sortie)"
-         label $This.fra2.g.lne -text "$caption(select,nom)"
-         entry $This.fra2.g.ene -width 15 -textvariable selectWindow(nomSortie)
-         button $This.fra2.g.explore -text "$caption(select,parcourir)" -width 1 -command { ::selectWindow::parcourir 2 }
-         label $This.fra2.g.lie -text "$caption(select,index)"
-         entry $This.fra2.g.eie -width 3 -textvariable selectWindow(indexSortie)
-         grid $This.fra2.g.titre -row 0 -column 0 -columnspan 2 -sticky {}
-         grid $This.fra2.g.lne -row 1 -column 0 -sticky w
-         grid $This.fra2.g.ene -row 1 -column 1 -sticky e
-         grid $This.fra2.g.explore -row 1 -column 2 -sticky e -padx 10 -ipady 5
-         grid $This.fra2.g.lie -row 2 -column 0 -sticky w
-         grid $This.fra2.g.eie -row 2 -column 1 -sticky e
-
-      frame $This.fra2.h -bd 2 -relief groove
-         button $This.fra2.h.b0 -text "$caption(select,demarrer)" -command { ::selectWindow::start }
-         button $This.fra2.h.b1 -text "$caption(select,inf)" -command { ::selectWindow::prev } -state disabled
-         entry  $This.fra2.h.e0 -textvariable selectWindow(indexEntree) -width 3
-         button $This.fra2.h.b2 -text "$caption(select,sup)" -command { ::selectWindow::next } -state disabled
-         button $This.fra2.h.b4 -text "$caption(select,auto)" -command { ::selectWindow::auto } -state disabled
-         button $This.fra2.h.b5 -text "$caption(select,aide)" -command { ::selectWindow::afficheAide } -state normal
-         button $This.fra2.h.b3 -text "$caption(select,record)" -command { ::selectWindow::record } -state disabled
-         pack $This.fra2.h.b0 $This.fra2.h.b1 $This.fra2.h.e0 $This.fra2.h.b2 $This.fra2.h.b4 -side left -padx 4 -pady 4 -ipady 5
-         pack $This.fra2.h.b3 $This.fra2.h.b5 -side right -padx 4 -pady 4 -ipady 5
-
-      frame $This.fra2.i
-         text $This.fra2.i.t -yscrollcommand "$This.fra2.i.vsb set" -xscrollcommand "$This.fra2.i.hsb set" -width 0 -height 0
-         scrollbar $This.fra2.i.vsb -orient vertical -command "$This.fra2.i.t yview"
-         scrollbar $This.fra2.i.hsb -orient horizontal -command "$This.fra2.i.t xview"
-         grid $This.fra2.i.t $This.fra2.i.vsb -sticky news
-         grid $This.fra2.i.hsb -sticky ew
-         grid rowconfigure $This.fra2.i 0 -weight 1
-         grid columnconfigure $This.fra2.i 0 -weight 1
-
-      grid $This.fra2.f -row 0 -column 0 -padx 2 -pady 4 -ipadx 4 -ipady 4 -sticky ew
-      grid $This.fra2.g -row 0 -column 1 -padx 2 -pady 4 -ipadx 4 -ipady 4 -sticky ew
-      grid $This.fra2.h -row 1 -column 0 -columnspan 2 -padx 2 -pady 0 -sticky ew
-      grid $This.fra2.i -row 2 -column 0 -columnspan 2 -padx 2 -pady 4 -sticky news; #-sticky ew
-      grid columnconfigure $This.fra2 0 -weight 1
-      grid rowconfigure $This.fra2 2 -weight 1
-
-      pack $This.fra2 -anchor center -expand 0 -fill y -side left
-
-      #--- Je cree la visu de la fenetre principale
+      #--- Je cree la visu de la fenetre de selection
       set selectWindow(visuNo) [::confVisu::create $This]
+
+      #--- j'ajoute l'ouil de selection
+      frame $This.tool.fra2 -bd 2 -relief groove
+
+      frame $This.tool.fra2.f -bd 2 -relief groove
+         label $This.tool.fra2.f.titre -text "$caption(select,entree)"
+         label $This.tool.fra2.f.lne -text "$caption(select,nom)"
+         entry $This.tool.fra2.f.ene -width 15 -textvariable selectWindow(nomEntree)
+         button $This.tool.fra2.f.explore -text "$caption(select,parcourir)" -width 1 -command { ::selectWindow::parcourir 1 }
+         label $This.tool.fra2.f.lie -text "$caption(select,index)"
+         entry $This.tool.fra2.f.eie -width 3 -textvariable selectWindow(indexEntree)
+         grid $This.tool.fra2.f.titre -row 0 -column 0 -columnspan 2 -sticky {}
+         grid $This.tool.fra2.f.lne -row 1 -column 0 -sticky w
+         grid $This.tool.fra2.f.ene -row 1 -column 1 -sticky e
+         grid $This.tool.fra2.f.explore -row 1 -column 2 -sticky e -padx 10 -ipady 5
+         grid $This.tool.fra2.f.lie -row 2 -column 0 -sticky w
+         grid $This.tool.fra2.f.eie -row 2 -column 1 -sticky e
+
+      frame $This.tool.fra2.g -bd 2 -relief groove
+         label $This.tool.fra2.g.titre -text "$caption(select,sortie)"
+         label $This.tool.fra2.g.lne -text "$caption(select,nom)"
+         entry $This.tool.fra2.g.ene -width 15 -textvariable selectWindow(nomSortie)
+         button $This.tool.fra2.g.explore -text "$caption(select,parcourir)" -width 1 -command { ::selectWindow::parcourir 2 }
+         label $This.tool.fra2.g.lie -text "$caption(select,index)"
+         entry $This.tool.fra2.g.eie -width 3 -textvariable selectWindow(indexSortie)
+         grid $This.tool.fra2.g.titre -row 0 -column 0 -columnspan 2 -sticky {}
+         grid $This.tool.fra2.g.lne -row 1 -column 0 -sticky w
+         grid $This.tool.fra2.g.ene -row 1 -column 1 -sticky e
+         grid $This.tool.fra2.g.explore -row 1 -column 2 -sticky e -padx 10 -ipady 5
+         grid $This.tool.fra2.g.lie -row 2 -column 0 -sticky w
+         grid $This.tool.fra2.g.eie -row 2 -column 1 -sticky e
+
+      frame $This.tool.fra2.h -bd 2 -relief groove
+         button $This.tool.fra2.h.b0 -text "$caption(select,demarrer)" -command { ::selectWindow::start }
+         button $This.tool.fra2.h.b1 -text "$caption(select,inf)" -command { ::selectWindow::prev } -state disabled
+         entry  $This.tool.fra2.h.e0 -textvariable selectWindow(indexEntree) -width 3
+         button $This.tool.fra2.h.b2 -text "$caption(select,sup)" -command { ::selectWindow::next } -state disabled
+         button $This.tool.fra2.h.b4 -text "$caption(select,auto)" -command { ::selectWindow::auto } -state disabled
+         button $This.tool.fra2.h.b5 -text "$caption(select,aide)" -command { ::selectWindow::afficheAide } -state normal
+         button $This.tool.fra2.h.b3 -text "$caption(select,record)" -command { ::selectWindow::record } -state disabled
+         pack $This.tool.fra2.h.b0 $This.tool.fra2.h.b1 $This.tool.fra2.h.e0 $This.tool.fra2.h.b2 $This.tool.fra2.h.b4 -side left -padx 4 -pady 4 -ipady 5
+         pack $This.tool.fra2.h.b3 $This.tool.fra2.h.b5 -side right -padx 4 -pady 4 -ipady 5
+
+      frame $This.tool.fra2.i
+         scrollbar $This.tool.fra2.i.vsb -orient vertical -command "$This.tool.fra2.i.t yview"
+         scrollbar $This.tool.fra2.i.hsb -orient horizontal -command "$This.tool.fra2.i.t xview"
+         text $This.tool.fra2.i.t -yscrollcommand "$This.tool.fra2.i.vsb set" -xscrollcommand "$This.tool.fra2.i.hsb set" -width 0 -height 0
+         grid $This.tool.fra2.i.t $This.tool.fra2.i.vsb -sticky news
+         grid $This.tool.fra2.i.hsb -sticky ew
+         grid rowconfigure $This.tool.fra2.i 0 -weight 1
+         grid columnconfigure $This.tool.fra2.i 0 -weight 1
+
+      grid $This.tool.fra2.f -row 0 -column 0 -padx 2 -pady 4 -ipadx 4 -ipady 4 -sticky ew
+      grid $This.tool.fra2.g -row 0 -column 1 -padx 2 -pady 4 -ipadx 4 -ipady 4 -sticky ew
+      grid $This.tool.fra2.h -row 1 -column 0 -columnspan 2 -padx 2 -pady 0 -sticky ew
+      grid $This.tool.fra2.i -row 2 -column 0 -columnspan 2 -padx 2 -pady 4 -sticky news; #-sticky ew
+      grid columnconfigure $This.tool.fra2 0 -weight 1
+      grid rowconfigure $This.tool.fra2 2 -weight 1
+
+      pack $This.tool.fra2 -anchor center -expand 0 -fill y -side left
 
       #--- Creation des variables audace dependant de la visu
       set selectWindow(buffer) [visu$selectWindow(visuNo) buf]
@@ -158,7 +159,7 @@ namespace eval ::selectWindow {
             lappend liste [ list $fwhm $img $dfwhm ]
          }
          set liste [ lsort $liste ]
-         $This.fra2.i.t insert insert "# $liste\n\n"
+         $This.tool.fra2.i.t insert insert "# $liste\n\n"
          set cmp [ buf$selectWindow(buffer) compress ]
          if { $cmp == "none" } {
             set cmp ""
@@ -166,15 +167,15 @@ namespace eval ::selectWindow {
             set cmp ".gz"
          }
          foreach elem $liste {
-            $This.fra2.i.t insert insert "# $elem\n"
+            $This.tool.fra2.i.t insert insert "# $elem\n"
             set org "[ lindex $elem 1 ]"
             set dest [ file join $audace(rep_images) $selectWindow(nomSortie)$selectWindow(indexSortie)[ buf$selectWindow(buffer) extension ]$cmp ]
             file copy -force "$org" "$dest"
-            $This.fra2.i.t insert insert "file copy -force $org $dest\n\n"
+            $This.tool.fra2.i.t insert insert "file copy -force $org $dest\n\n"
             incr selectWindow(indexSortie)
          }
-         $This.fra2.i.t insert insert "---------------------------------------------------------------------------------------------------------\n\n"
-         $This.fra2.i.t see insert
+         $This.tool.fra2.i.t insert insert "---------------------------------------------------------------------------------------------------------\n\n"
+         $This.tool.fra2.i.t see insert
       }
    }
 
@@ -192,7 +193,7 @@ namespace eval ::selectWindow {
       global selectWindow
 
       if { ( $selectWindow(nomSortie) != "" ) && ( $selectWindow(indexSortie) != "" ) } {
-         $This.fra2.h.b3 configure -state disabled
+         $This.tool.fra2.h.b3 configure -state disabled
          set cmp [ buf$selectWindow(buffer) compress ]
          if { $cmp == "none" } {
             set cmp ""
@@ -203,7 +204,7 @@ namespace eval ::selectWindow {
          set org "[ lindex $selectWindow(liste_entree) $selectWindow(index) ]"
          set dest [ file join $audace(rep_images) $selectWindow(nomSortie)$selectWindow(indexSortie)[ buf$selectWindow(buffer) extension ]$cmp ]
          file copy -force $org $dest
-         $This.fra2.i.t insert insert "file copy -force $org $dest\n"
+         $This.tool.fra2.i.t insert insert "file copy -force $org $dest\n"
          #--- Incrementation de l'index de sortie
          incr selectWindow(indexSortie)
          #--- L'image recopiee est supprimee de la liste de depart
@@ -213,8 +214,8 @@ namespace eval ::selectWindow {
          }
          if { $selectWindow(index) < "0" } {
             set selectWindow(indexEntree) "$caption(select,tiret)"
-            $This.fra2.h.b1 configure -state disabled
-            $This.fra2.h.b2 configure -state disabled
+            $This.tool.fra2.h.b1 configure -state disabled
+            $This.tool.fra2.h.b2 configure -state disabled
          }
          dispima $selectWindow(index)
       }
@@ -238,16 +239,16 @@ namespace eval ::selectWindow {
          }
          wm title $This [ lindex $selectWindow(liste_entree) $selectWindow(index) ]
          #--- Validation des boutons de navigation et enregistrement
-         $This.fra2.h.b3 configure -state normal
+         $This.tool.fra2.h.b3 configure -state normal
          if { $selectWindow(index) == [ expr [ llength $selectWindow(liste_entree) ] -1 ] } {
-            $This.fra2.h.b2 configure -state disabled
+            $This.tool.fra2.h.b2 configure -state disabled
          } else {
-            $This.fra2.h.b2 configure -state normal
+            $This.tool.fra2.h.b2 configure -state normal
          }
          if { $selectWindow(index) == "0" } {
-            $This.fra2.h.b1 configure -state disabled
+            $This.tool.fra2.h.b1 configure -state disabled
          } else {
-            $This.fra2.h.b1 configure -state normal
+            $This.tool.fra2.h.b1 configure -state normal
          }
       }
    }
@@ -283,17 +284,17 @@ namespace eval ::selectWindow {
          }
          $selectWindow(canvas) configure -height $h
          if { [ llength $selectWindow(liste_entree) ] != "1" } {
-            $This.fra2.h.b2 configure -state normal
+            $This.tool.fra2.h.b2 configure -state normal
          } else {
-            $This.fra2.h.b2 configure -state disabled
+            $This.tool.fra2.h.b2 configure -state disabled
          }
-         $This.fra2.h.b3 configure -state normal
-         $This.fra2.h.b4 configure -state normal
+         $This.tool.fra2.h.b3 configure -state normal
+         $This.tool.fra2.h.b4 configure -state normal
          foreach elem $selectWindow(liste_entree) {
-            $This.fra2.i.t insert insert "# $elem\n"
+            $This.tool.fra2.i.t insert insert "# $elem\n"
          }
-         $This.fra2.i.t insert insert "\n"
-         $This.fra2.i.t see insert
+         $This.tool.fra2.i.t insert insert "\n"
+         $This.tool.fra2.i.t see insert
       }
    }
 
