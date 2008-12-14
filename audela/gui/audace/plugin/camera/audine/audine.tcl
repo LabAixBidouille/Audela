0cb0c6318ec22d8f0630893bc76f91a3af1b7d88
robertdelmas <Robert Delmas>
2008-12-14 08:46:13 +0000

    Mise à jour du binning en Y pour le mode TDI avec l'EthernAude. La norme était plus ambitieuse que la dll.

--- audela/gui/audace/plugin/camera/audine/audine.tcl
+++ audela/gui/audace/plugin/camera/audine/audine.tcl
@@ -2,7 +2,7 @@
 # Fichier : audine.tcl
 # Description : Configuration de la camera Audine
 # Auteur : Robert DELMAS
-# Mise a jour $Id: audine.tcl,v 1.18 2008-11-01 15:42:03 robertdelmas Exp $
+# Mise a jour $Id: audine.tcl,v 1.19 2008-12-14 08:46:13 robertdelmas Exp $
 #
 
 namespace eval ::audine {
@@ -708,10 +708,10 @@ proc ::audine::getPluginProperty { camItem propertyName } {
             "parallelport" { return [ list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 ] }
             "quickaudine"  { return [ list "" ] }
             "audinet"      { return [ list "" ] }
-            "ethernaude"   { return [ list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 \
-                                               21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 \
-                                               41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 \
-                                               61 62 63 64 ] }
+            "ethernaude"   { return [ list 1 ] ; A la specification [ list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 \
+                                               15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 \
+                                               35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 \
+                                               55 56 57 58 59 60 61 62 63 64 ] }
          }
       }
       dynamic          { return [ list 32767 -32768 ] }
