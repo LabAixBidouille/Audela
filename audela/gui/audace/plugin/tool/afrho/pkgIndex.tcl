dc3ab400ff0090fd9d733695c959d872ffb0db99
robertdelmas <Robert Delmas>
2009-01-31 08:16:09 +0000

    Déplacement des fichiers associés à AfRho dans les plugins de type "tool" avec la propriété "analysis".

---
+++ audela/gui/audace/plugin/tool/afrho/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Mise a jour $Id: pkgIndex.tcl,v 1.1 2009-01-31 08:15:44 robertdelmas Exp $
+#
+
+package ifneeded afrho 1.0 [ list source [ file join $dir afrho.tcl ] ]
+
