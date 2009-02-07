088eeae70777986fbb46bb358e5186515532e7bc
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-02-07 11:26:19 +0000

    Suppression de la procédure obsolète : affiche_Outil Changement du nom de procédures : - "pas_Outil" devient "pasOutil" - "affiche_Outil_F2" devient "afficheOutilF2"

--- trunk/audela/gui/audace/aud_menu_6.tcl
+++ trunk/audela/gui/audace/aud_menu_6.tcl
@@ -1,66 +1,20 @@
 #
 # Fichier : aud_menu_6.tcl
 # Description : Script regroupant les fonctionnalites du menu Outils
-# Mise a jour $Id: aud_menu_6.tcl,v 1.4 2009-01-31 08:52:44 robertdelmas Exp $
+# Mise a jour $Id: aud_menu_6.tcl,v 1.5 2009-02-07 11:26:19 robertdelmas Exp $
 #
 
 namespace eval ::audace {
 }
 
    #
-   # pas_Outil
+   # pasOutil
    # Efface l'interface graphique de l'outil affichee dans la visu
    #
-   proc ::audace::pas_Outil { } {
+   proc ::audace::pasOutil { } {
       global audace
 
       ::confVisu::stopTool $audace(visuNo)
-   }
-
-   #
-   # affiche_Outil visuNo
-   # Fonction qui permet d'afficher tous les outils dans le menu Outils
-   #
-   proc ::audace::affiche_Outil { visuNo } {
-      global audace caption conf confgene panneau
-
-      set confgene(Choix_Outil,nbre) "0"
-      set i "0"
-      set liste ""
-      foreach m [array names panneau menu_name,*] {
-         lappend liste [list "$panneau($m) " $m]
-      }
-      foreach m [lsort $liste] {
-         set m [lindex $m 1]
-         set i [expr $i + 1]
-         #--- Initialisation des variables indispensable ici
-         if { ! [ info exists conf(panneau,n$i) ] }   { set conf(panneau,n$i)  "1" }
-         if { ! [ info exists conf(raccourci,n$i) ] } { set conf(raccourci,n$i) "" }
-         #---
-         set confgene(Choix_Outil,nbre) [ expr $confgene(Choix_Outil,nbre) + 1 ]
-         set confgene(Choix_Outil,n$confgene(Choix_Outil,nbre)) $conf(panneau,n$confgene(Choix_Outil,nbre))
-         if { $confgene(Choix_Outil,n$confgene(Choix_Outil,nbre)) == "1" } {
-            if { [scan "$m" "menu_name,%s" ns] == "1" } {
-               if { [ ::$ns\::getPluginProperty function ] != "analysis" } {
-                  Menu_Command $visuNo "$caption(audace,menu,outils)" "$panneau($m)" "::confVisu::selectTool $visuNo ::$ns"
-                  if { $conf(raccourci,n$i) != "" } {
-                     set raccourci(n$i) $conf(raccourci,n$i)
-                     if { [string range $raccourci(n$i) 0 3] == "Alt+" } {
-                        set raccourci(n$i) "Alt-[string tolower [string range $raccourci(n$i) 4 4]]"
-                     } elseif { [string range $raccourci(n$i) 0 4] == "Ctrl+" } {
-                        set raccourci(n$i) "Control-[string tolower [string range $raccourci(n$i) 5 5]]"
-                     }
-                     #---
-                     lappend audace(list_raccourcis) [ list $conf(raccourci,n$i) ]
-                     lappend audace(list_ns_raccourcis) [ list $ns ]
-                     #---
-                     Menu_Bind $visuNo $audace(base) <$raccourci(n$i)> "$caption(audace,menu,outils)" "$panneau($m)" "$conf(raccourci,n$i)"
-                               bind $audace(Console) <$raccourci(n$i)> "focus $audace(base) ; ::confVisu::selectTool $visuNo ::$ns"
-                  }
-               }
-            }
-         }
-      }
    }
 
 ###################################################################################
@@ -68,29 +22,16 @@ namespace eval ::audace {
 ###################################################################################
 
    #
-   # affiche_Outil_F2
+   # afficheOutilF2
    # Affiche automatiquement au demarrage l'outil ayant F2 pour raccourci
    #
-   proc ::audace::affiche_Outil_F2 { } {
-      global audace conf panneau
+   proc ::audace::afficheOutilF2 { } {
+      global audace conf
 
-      #---
-      set i "0"
-      set liste ""
-      foreach m [array names panneau menu_name,*] {
-         lappend liste [list "$panneau($m) " $m]
-      }
-      foreach m [lsort $liste] {
-         set m [lindex $m 1]
-         set i [expr $i + 1]
-         if { $conf(raccourci,n$i) != "" } {
-            set raccourci(n$i) $conf(raccourci,n$i)
-            if { $raccourci(n$i) == "F2" } {
-               if { [scan "$m" "menu_name,%s" ns] == "1" } {
-                  #--- Lancement automatique de l'outil ayant le raccourci F2
-                  ::confVisu::selectTool $audace(visuNo) ::$ns
-               }
-            }
+      foreach { namespace raccourci } $conf(afficheOutils) {
+         if { $raccourci == "F2" } {
+            #--- Lancement automatique de l'outil ayant le raccourci F2
+            ::confVisu::selectTool $audace(visuNo) ::$namespace
          }
       }
    }
