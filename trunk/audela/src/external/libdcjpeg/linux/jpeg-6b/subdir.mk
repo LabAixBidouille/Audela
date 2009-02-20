6501a56d8927cf30420993379cf029209b3bf7f7
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-02-20 21:25:39 +0000

    modification pour machine 64

--- trunk/audela/src/external/libdcjpeg/linux/jpeg-6b/subdir.mk
+++ trunk/audela/src/external/libdcjpeg/linux/jpeg-6b/subdir.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-# Add inputs and outputs from these tool invocations to the build variables 
+# Add inputs and outputs from these tool invocations to the build variables
 C_SRCS += \
 $(ROOT)/jpeg-6b/jcapimin.c \
 $(ROOT)/jpeg-6b/jcapistd.c \
@@ -49,7 +49,7 @@ $(ROOT)/jpeg-6b/jmemmgr.c \
 $(ROOT)/jpeg-6b/jmemnobs.c \
 $(ROOT)/jpeg-6b/jquant1.c \
 $(ROOT)/jpeg-6b/jquant2.c \
-$(ROOT)/jpeg-6b/jutils.c 
+$(ROOT)/jpeg-6b/jutils.c
 
 OBJS += \
 ./jpeg-6b/jcapimin.o \
@@ -97,7 +97,7 @@ OBJS += \
 ./jpeg-6b/jmemnobs.o \
 ./jpeg-6b/jquant1.o \
 ./jpeg-6b/jquant2.o \
-./jpeg-6b/jutils.o 
+./jpeg-6b/jutils.o
 
 DEPS += \
 ${addprefix ./jpeg-6b/, \
@@ -154,8 +154,8 @@ jutils.d \
 jpeg-6b/%.o: $(ROOT)/jpeg-6b/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	@echo gcc -I../jpeg-6b -O3 -Wall -c -fmessage-length=0 -o$@ $<
-	@gcc -I../jpeg-6b -O3 -Wall -c -fmessage-length=0 -o$@ $< && \
+	@echo gcc -I../jpeg-6b -O3 -Wall -c -fmessage-length=0 -fPIC -o$@ $<
+	@gcc -I../jpeg-6b -O3 -Wall -c -fmessage-length=0 -fPIC -o$@ $< && \
 	echo -n $(@:%.o=%.d) $(dir $@) > $(@:%.o=%.d) && \
 	gcc -MM -MG -P -w -I../jpeg-6b -O3 -Wall -c -fmessage-length=0  $< >> $(@:%.o=%.d)
 	@echo 'Finished building: $<'
