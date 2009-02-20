891e5ac8054ef42690a6a121425400a70af2064c
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-02-20 21:33:30 +0000

    modification pour machine 64

--- trunk/audela/src/external/libgphoto2/libltdl/linux/subdir.mk
+++ trunk/audela/src/external/libgphoto2/libltdl/linux/subdir.mk
@@ -2,25 +2,25 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-# Add inputs and outputs from these tool invocations to the build variables 
+# Add inputs and outputs from these tool invocations to the build variables
 C_SRCS += \
 ../libltdl.c \
-../ltdl.c 
+../ltdl.c
 
 OBJS += \
 ./libltdl.o \
-./ltdl.o 
+./ltdl.o
 
 C_DEPS += \
 ./libltdl.d \
-./ltdl.d 
+./ltdl.d
 
 
 # Each subdirectory must supply rules for building sources it contributes
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -DHAVE_CONFIG_H -I.. -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	gcc -DHAVE_CONFIG_H -I.. -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
