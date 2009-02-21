bffe0987c1841af4f79b2facdd1cb6b43309d6eb
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-02-21 13:47:02 +0000

    ajout des source de BLT2.4z

---
+++ trunk/audela/src/external/blt/vc60/install.bat
@@ -0,0 +1,16 @@
+rem  parametre %1 = Release ou Debug
+
+rem Si pas de parametre , la valeur par defaut est Release
+if not "%1" == "" set CONFIG=%1
+if "%1" == "" set CONFIG=Release
+
+set sourceDir=..\blt2.4z
+set destDir=..\..\..\..\lib\blt2.4
+
+mkdir %destDir%
+mkdir %destDir%\dd_protocols
+
+copy /Y %sourceDir%\library\*.* %destDir%
+copy /Y %sourceDir%\library\dd_protocols\* %destDir%
+echo on
+copy %CONFIG%\blt24.dll  %destDir%
