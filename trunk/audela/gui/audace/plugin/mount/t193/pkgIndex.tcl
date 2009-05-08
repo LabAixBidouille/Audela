fc51f378b1d7d1dc099ec28fe44d67e83847cd86
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-05-08 21:27:17 +0000

    Nouvelle monture (T193 de l'OHP)

---
+++ trunk/audela/gui/audace/plugin/mount/t193/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Mise a jour $Id: pkgIndex.tcl,v 1.1 2009-05-08 21:27:17 robertdelmas Exp $
+#
+
+package ifneeded t193 1.0 [ list source [ file join $dir t193.tcl ] ]
+
