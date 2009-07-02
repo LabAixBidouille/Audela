b2688fe2ec01f2f40a6f57bd4cedbd3a30c9c2cb
jberthier <jberthier@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-07-02 22:47:28 +0000

    Ajout script bddimages/site

---
+++ trunk/audela/gui/audace/plugin/tool/bddimages/site/default.tcl
@@ -0,0 +1,36 @@
+proc dateobs {tabkey} {
+
+  #	  0         1         2   
+  #       0123456789012345678901
+  # date <2006-06-23T20:22:36.08>
+  set dateobs ""
+  foreach keyval $tabkey {
+
+    set key [lindex $keyval 0]
+    set val [lindex [lindex $keyval 1] 1]
+   
+      switch $key {
+          "DATE-OBS" {
+	    set dateobs $val
+	    break
+	    }
+          default {
+ 	    }
+	  }
+          # fin switch
+      }
+      # fin foreach
+  if {$dateobs==""} { 
+    return [list 1 "-"]
+    } else {
+    set annee	 [string range $dateobs 0 3]
+    set mois	 [string range $dateobs 5 6]
+    set jour	 [string range $dateobs 8 9]
+    set heure	 [string range $dateobs 11 12]
+    set minute   [string range $dateobs 14 15]
+    set seconde  [string range $dateobs 17 end]
+    set dateiso "$annee-$mois-$jour\T$heure:$minute:$seconde"
+    }
+    
+return [list 0 $dateiso]
+}
