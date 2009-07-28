694436ae4c8e497e5bd96f360ecfa083ece63865
Michel PUJOL <michel-pujol@orange.fr>
2009-07-28 17:34:19 +0000

    mise a jour du numero de version et de l'installation automatique de la librairie

--- audela/gui/audace/plugin/camera/ascomcam/pkgIndex.tcl
+++ audela/gui/audace/plugin/camera/ascomcam/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.1 2009-07-18 12:18:57 michelpujol Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.2 2009-07-28 17:34:19 michelpujol Exp $
 #
 
-package ifneeded ascomcam 1.1 [ list source [ file join $dir ascomcam.tcl ] ]
+package ifneeded ascomcam 1.2 [ list source [ file join $dir ascomcam.tcl ] ]
 
