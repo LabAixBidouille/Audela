deb2981771d261997afb2382f3666dbe7eb8dbf8
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-08-30 20:44:26 +0000

    evolution pour inclure libqsi.dll dans le kit de mise à jour

--- trunk/audela/gui/audace/plugin/camera/qsi/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/camera/qsi/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.1 2009-03-28 14:34:53 michelpujol Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.2 2009-08-30 20:44:25 michelpujol Exp $
 #
 
-package ifneeded qsi 1.0 [ list source [ file join $dir qsi.tcl ] ]
+package ifneeded qsi 1.2 [ list source [ file join $dir qsi.tcl ] ]
 
