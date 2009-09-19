b938cbcbdf39e0560c0560edb46307c7a4c9789e
bmauclaire <bmauclaire@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-09-19 13:54:54 +0000

    Nouveaux parametres de filtrage de la RI dans spc_rinstrum.

--- trunk/audela/gui/audace/plugin/tool/spcaudace/spc_calibrage.tcl
+++ trunk/audela/gui/audace/plugin/tool/spcaudace/spc_calibrage.tcl
@@ -3,7 +3,7 @@
 # spc_fits2dat lmachholz_centre.fit
 # buf1 load lmachholz_centre.fit
 
-# Mise a jour $Id: spc_calibrage.tcl,v 1.9 2009-07-01 16:17:28 bmauclaire Exp $
+# Mise a jour $Id: spc_calibrage.tcl,v 1.10 2009-09-19 13:54:54 bmauclaire Exp $
 
 
 
@@ -3734,17 +3734,18 @@ proc spc_rinstrum { args } {
            #-- Meth 4 : interpolation polynomiale de 4 -> RI 3
            set rinstrum [ spc_polynomefilter $result_division 3 150 o ]
            file rename -force "$audace(rep_images)/$rinstrum$conf(extension,defaut)" "$audace(rep_images)/reponse_instrumentale-3$conf(extension,defaut)"
-
-
        } elseif { $flag_br==1 } {
            if { $dispersion<=1. } {
                #-- Lhires3+résos 600 t/mm et 1200 t/mm-kaf1600 :
-               set rinstrum [ spc_pwlfilter $result_division 280 o 51 201 10 2 50 ]
+               ## set rinstrum [ spc_pwlfilter $result_division 280 o 51 201 10 2 50 ]
+               # set rinstrum [  spc_lowresfilterfile $result_division "$spcaudace(reptelluric)/forgetlambda.dat" 1.3 10000 { 1.0 2.0 } "o" 18 ]
+               set rinstrum [  spc_lowresfilterfile $result_division "$spcaudace(reptelluric)/forgetlambda.dat" 1.0 1000000. { 1.0 1.0 10000000. 1. } "o" 50 ]
            } else {
            #-- Lhires3+résos 300 et 150 t/mm :
-               ## set rinstrum [ spc_pwlfilter $result_division 50 o 11 51 70 50 100 ]
-               # set rinstrum [ spc_pwlfilter $result_division 24 o 3 3 50 50 50 ]
-              set rinstrum [ spc_lowresfilterfile $result_division "$spcaudace(reptelluric)/forgetlambda.dat" 1.1 10 { 1.0 2.0 } "o" 18 ]
+              ## set rinstrum [ spc_pwlfilter $result_division 50 o 11 51 70 50 100 ]
+              # set rinstrum [ spc_pwlfilter $result_division 24 o 3 3 50 50 50 ]
+              # set rinstrum [ spc_lowresfilterfile $result_division "$spcaudace(reptelluric)/forgetlambda.dat" 1.1 10 { 1.0 2.0 } "o" 18 ]
+              set rinstrum [ spc_lowresfilterfile $result_division "$spcaudace(reptelluric)/forgetlambda.dat" 1.1 50. { 1. 5.1500. } "o" 18 ]
            }
            file rename -force "$audace(rep_images)/$rinstrum$conf(extension,defaut)" "$audace(rep_images)/reponse_instrumentale-br$conf(extension,defaut)"
        }
