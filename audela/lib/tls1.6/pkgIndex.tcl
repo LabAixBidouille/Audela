a929e44a73682a3430b061c357e72f623fa8a6ce
myrtillelaas <Myrtille Lass>
2009-10-15 09:56:51 +0000

    *** empty log message ***

--- audela/lib/tls1.6/pkgIndex.tcl
+++
@@ -1,2 +0,0 @@
-if {![package vsatisfies [package provide Tcl] 8.3]} {return}
-package ifneeded tls 1.6 "source \[file join [list $dir] tls.tcl\] ; tls::initlib [list $dir] tls16[info sharedlibextension]"
