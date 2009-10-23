9b2d2bec445fd3de2057a20f1203c2330ef9bc29
bmauclaire <bmauclaire@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-10-23 18:45:32 +0000

    Version 1.5.4 d'SpcAudACE

--- trunk/audela/gui/audace/plugin/tool/spcaudace/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/tool/spcaudace/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.6 2009-07-13 00:20:21 bmauclaire Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.7 2009-10-23 18:45:32 bmauclaire Exp $
 #
 
-package ifneeded spcaudace 1.5.3 [ list source [ file join $dir spcaudace.tcl ] ]
+package ifneeded spcaudace 1.5.4 [ list source [ file join $dir spcaudace.tcl ] ]
 
