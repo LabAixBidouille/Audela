9ac2acafe7cd470a58b63cacd3641884d2ca8d0d
robertdelmas <Robert Delmas>
2009-10-25 13:27:08 +0000

    Raquette pour le T193 de l'OHP.

---
+++ audela/gui/audace/plugin/pad/t193pad/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Mise a jour $Id: pkgIndex.tcl,v 1.1 2009-10-25 13:27:08 robertdelmas Exp $
+#
+
+package ifneeded t193pad 1.0 [ list source [ file join $dir t193pad.tcl ] ]
+
