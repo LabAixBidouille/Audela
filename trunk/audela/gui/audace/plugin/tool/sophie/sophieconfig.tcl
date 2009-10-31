7fc8ddd4e3d9c4b0895456f1ef42daa6af3acf8c
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-10-31 11:33:12 +0000

    Ajout d'un bouton pour accèder à l'interface des mots clés de l'en-tête FITS.

--- trunk/audela/gui/audace/plugin/tool/sophie/sophieconfig.tcl
+++ trunk/audela/gui/audace/plugin/tool/sophie/sophieconfig.tcl
@@ -2,7 +2,7 @@
 # @file     sophieconfig.tcl
 # @brief    Fichier du namespace ::sophie::config
 # @author   Michel PUJOL et Robert DELMAS
-# @version  $Id: sophieconfig.tcl,v 1.26 2009-10-25 13:29:41 michelpujol Exp $
+# @version  $Id: sophieconfig.tcl,v 1.27 2009-10-31 11:33:12 robertdelmas Exp $
 #------------------------------------------------------------
 
 ##------------------------------------------------------------
@@ -549,7 +549,18 @@ proc ::sophie::config::fillConfigAvanceePage { frm visuNo } {
          -command "::sophie::simul"
       grid $frm.simulation.but -in [ $frm.simulation getframe ] -row 0 -column 1 -sticky e -padx 2
 
-   pack $frm.simulation -side bottom -anchor w -fill x -expand 0
+   pack $frm.simulation -side left -anchor w -fill x -expand 1
+
+   #--- Frame pour le bouton d'acces aux mots cles de l'en-tete FITS
+   TitleFrame $frm.en-tete_fits -borderwidth 2 -relief ridge \
+      -text $::caption(sophie,en-tete_fits)
+
+      #--- Bouton de lancement de l'acces aux mots cles de l'en-tete FITS
+      Button $frm.en-tete_fits.but -text $::caption(sophie,mots_cles) -relief raised \
+         -command "::keyword::run $::audace(visuNo)"
+      grid $frm.en-tete_fits.but -in [ $frm.en-tete_fits getframe ] -row 0 -column 1 -sticky e -padx 2
+
+   pack $frm.en-tete_fits -side left -anchor w -fill x -expand 1
 }
 
 #----------------------------------------------------------------------------
