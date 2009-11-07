6be299a180fe07abb925ad330a71b732b057f119
Michel PUJOL <michel-pujol@orange.fr>
2009-11-07 09:25:05 +0000

    modification des commentaires

--- audela/src/external/ascom/install.bat
+++ audela/src/external/ascom/install.bat
@@ -1,11 +1,11 @@
 @echo -----------------------------------------------------------
-@echo --- Note pour le developpement de l'interface de camera QSI
+@echo --- Note pour le developpement de l'interface ASCOM
 @echo Les fichiers AscomMasterInterfaces.h et AscomMasterInterfaces.lib
 @echo ne sont plus fournis ASCOM. Ils sont generes automatiquement par
-@echo VisualC++ v9 @echo a partir du fichier AscomMasterInterfaces.tlb
+@echo VisualC++ v9 a partir du fichier AscomMasterInterfaces.tlb
 @echo en fonction du systeme d'exploitation.
 @echo
-@echo --- Note pour l'utilisation de l'interface de camera QSI
+@echo --- Note pour l'utilisation de l'interface de camera ASCOM
 @echo Avant de lancer Audela , l'utilisateur doit installer le driver de
 @echo ASCOM disponible sur http://ascom-standards.org
 @echo -----------------------------------------------------------
@@ -13,5 +13,5 @@
 set ASCOM=ascom
 @echo --- Je cree le repertoire external\lib
 mkdir ..\lib
-@echo --- Je copie le fichier QSICamera.dll
+@echo --- Je copie le fichier AscomMasterInterfaces.tlb
 copy %ASCOM%\lib\AscomMasterInterfaces.tlb ..\lib
