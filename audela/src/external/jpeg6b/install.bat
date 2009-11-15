7bc05eea0f62473c26c0e505806fbc9b0d1b7064
Michel PUJOL <michel-pujol@orange.fr>
2009-11-15 15:33:30 +0000

    ajout de jpeg6 dans la liste des librairies externes a compiler automatiquement

--- audela/src/external/jpeg6b/install.bat
+++ audela/src/external/jpeg6b/install.bat
@@ -1,4 +1,5 @@
-mkdir ..\include
-mkdir ..\lib
+if not exist jpeg-6b\release\jpeg.lib ( start /b /wait make.bat )
+if not exist ..\include (mkdir ..\include )
+if not exist ..\lib     (mkdir ..\lib)
 copy jpeg-6b\*.h ..\include
 copy jpeg-6b\release\jpeg.lib ..\lib
