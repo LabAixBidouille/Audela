251298f76e9887eda7b38af62f00bbd6d4a70724
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-12-05 09:00:31 +0000

    renomme focusIsMoving

--- trunk/audela/src/libtel/libt193/src/telescop.h
+++ trunk/audela/src/libtel/libt193/src/telescop.h
@@ -63,7 +63,7 @@ struct telprop {
    char dec[12] ;  // declinaison courante
    //int radecNotification ; // 1=marche 0=arret des notifications des coordonnees radec
    int radecIsMoving ;   // 1=mouvement en cours 0=pas de mouvement en cours 
-   int focIsMoving ;   // 1=mouvement en cours 0=pas de mouvement en cours de la focalisation
+   int focusIsMoving ;   // 1=mouvement en cours 0=pas de mouvement en cours de la focalisation
 };
 
 int tel_init(struct telprop *tel, int argc, char **argv);
