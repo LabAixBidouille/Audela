41273fdffbe7913b15ab6eb07225ec6fa090575e
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-12-15 17:33:35 +0000

    Prise en compte d'une nouvelle couleur (pour les scripts à Raymond).

--- trunk/audela/gui/audace/confcolor.tcl
+++ trunk/audela/gui/audace/confcolor.tcl
@@ -2,7 +2,7 @@
 # Fichier : confcolor.tcl
 # Description : Selection et mise a jour en direct des couleurs de l'interface Aud'ACE
 # Auteurs : Denis MARCHAIS
-# Mise a jour $Id: confcolor.tcl,v 1.31 2009-06-15 15:48:37 robertdelmas Exp $
+# Mise a jour $Id: confcolor.tcl,v 1.32 2009-12-15 17:33:35 robertdelmas Exp $
 #
 
 namespace eval confColor:: {
@@ -87,6 +87,7 @@ namespace eval confColor:: {
       set color(white)      "white"
       set color(blue)       "blue"
       set color(blue_pad)   #123456
+      set color(lightblue)  #E0E8F0
       set color(green)      "green"
       set color(green1)     #005500
       set color(lightgreen) #AAFFAA
