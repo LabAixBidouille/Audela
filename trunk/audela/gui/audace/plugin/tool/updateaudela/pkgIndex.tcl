7a2b3e2dd86577e55f87dd95969ef4da688a8f31
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2009-12-30 07:26:07 +0000

    Passage à la version 1.4.

--- trunk/audela/gui/audace/plugin/tool/updateaudela/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/tool/updateaudela/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.5 2009-05-02 15:17:56 michelpujol Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.6 2009-12-30 07:26:07 robertdelmas Exp $
 #
 
-package ifneeded updateaudela 1.3 [ list source [ file join $dir updateaudela.tcl ] ]
+package ifneeded updateaudela 1.4 [ list source [ file join $dir updateaudela.tcl ] ]
 
