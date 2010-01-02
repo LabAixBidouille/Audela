543fe18364554eef30d5d2baf7f8e8974a993284
Michel PUJOL <michel-pujol@orange.fr>
2010-01-02 17:38:21 +0000

    correction de l'erreur extra qualification CFile:: sur Linux

--- audela/src/audela/libaudela/src/cfile.h
+++ audela/src/audela/libaudela/src/cfile.h
@@ -4,17 +4,17 @@
  * Copyright (C) 1998-2004 The AudeLA Core Team
  *
  * Initial author : Michel Pujol
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or (at
  * your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -35,15 +35,15 @@ class CFile  {
 public:
    CFile();
    ~CFile();
-   
-   static void CFile::setTclInterp( Tcl_Interp *interp);
+
+   static void setTclInterp( Tcl_Interp *interp);
 
 
    static CFileFormat getFormatFromHeader(char * filename);
    static CFileFormat getFormatFromTkImg(char * filename);
 
    static CFileFormat loadFile(char * filename, int dataTypeOut, CPixels **pixels, CFitsKeywords **keywords);
-   static CFileFormat saveFile(char * filename, int dataTypeOut, CPixels *pixels, CFitsKeywords *keywords);   
+   static CFileFormat saveFile(char * filename, int dataTypeOut, CPixels *pixels, CFitsKeywords *keywords);
 
 
    static void loadFits(char * filename, int dataTypeOut, CPixels **pixels, CFitsKeywords **keywords);
@@ -61,8 +61,8 @@ public:
    static void saveTkimg(char * filename, unsigned char *dataIn, int width, int height);
 
 
-protected: 
-   static Tcl_Interp * CFile::interp;
+protected:
+   static Tcl_Interp * interp;
 
 };
 
