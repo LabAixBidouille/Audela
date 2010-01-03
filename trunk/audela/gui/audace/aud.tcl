7a99804b4f7114f87fe3c238ff325e6e43de81ae
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2010-01-03 18:49:36 +0000

    Remplacement du namespace "lconv2" par "conv2" (fait par Raymond).

--- trunk/audela/gui/audace/aud.tcl
+++ trunk/audela/gui/audace/aud.tcl
@@ -2,7 +2,7 @@
 # Fichier : aud.tcl
 # Description : Fichier principal de l'application Aud'ACE
 # Auteur : Denis MARCHAIS
-# Mise a jour $Id: aud.tcl,v 1.116 2010-01-01 13:58:40 robertdelmas Exp $
+# Mise a jour $Id: aud.tcl,v 1.117 2010-01-03 18:49:36 robertdelmas Exp $
 
 #--- Chargement du package BWidget
 package require BWidget
@@ -771,13 +771,13 @@ namespace eval ::audace {
 
       Menu_Separator $visuNo "$caption(audace,menu,pretraite)"
       Menu_Command   $visuNo "$caption(audace,menu,pretraite)" "$caption(audace,menu,raw2fits)..." \
-         { ::lconv2::run "raw2fits" }
-      Menu_Command   $visuNo "$caption(audace,menu,pretraite)" "$caption(audace,menu,cfa2rvb)..." \
-         { ::lconv2::run "cfa2rgb" }
-      Menu_Command   $visuNo "$caption(audace,menu,pretraite)" "$caption(audace,menu,rvb2r+v+b)..." \
-         { ::lconv2::run "rgb2r+g+b" }
+         { ::conv2::run "raw2fits" }
       Menu_Command   $visuNo "$caption(audace,menu,pretraite)" "$caption(audace,menu,r+v+b2rvb)..." \
-         { ::lconv2::run "r+g+b2rgb" }
+         { ::conv2::run "r+g+b2rgb" }
+      Menu_Command   $visuNo "$caption(audace,menu,pretraite)" "$caption(audace,menu,rvb2r+v+b)..." \
+         { ::conv2::run "rgb2r+g+b" }
+      Menu_Command   $visuNo "$caption(audace,menu,pretraite)" "$caption(audace,menu,cfa2rvb)..." \
+         { ::conv2::run "cfa2rgb" }
 
       Menu_Separator $visuNo "$caption(audace,menu,pretraite)"
       Menu_Command   $visuNo "$caption(audace,menu,pretraite)" "$caption(audace,menu,window1)..."\
