9fb13667a716f61bf25f4def5faabf26fdce5269
Michel PUJOL <michel-pujol@orange.fr>
2010-03-07 10:56:31 +0000

    correction des parametres de compilation en mode Debug

--- audela/src/external/fli/install.bat
+++ audela/src/external/fli/install.bat
@@ -1,7 +1,13 @@
+rem  parametre %1 = Release ou Debug
 echo on
+
+rem Si pas de parametre , la valeur par defaut est Release
+if not "%1" == "" set CONFIG=%1
+if "%1" == "" set CONFIG=Release
+
 set FLI=fli-dist-1.71\libfli
 mkdir ..\include
 mkdir ..\lib
-copy %FLI%\windows\release\libfli.dll ..\..\..\bin
-copy %FLI%\windows\release\libfli.lib ..\lib
+copy %FLI%\windows\%CONFIG%\libfli.dll ..\..\..\bin
+copy %FLI%\windows\%CONFIG%\libfli.lib ..\lib
 copy %FLI%\libfli.h ..\include
