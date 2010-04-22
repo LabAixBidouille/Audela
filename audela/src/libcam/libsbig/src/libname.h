74ff243f543941f2cea7a7266290750f1621ba37
bmauclaire <Benjixc@users.noreply.github.com>
2010-04-22 07:15:31 +0000

    MAj des regles du precmpilateur C pour gerer des librairies libsbigudrv differentes selon le port de communication choisis (parallele ou USB).

--- audela/src/libcam/libsbig/src/libname.h
+++ audela/src/libcam/libsbig/src/libname.h
@@ -27,19 +27,26 @@
  * Nom du point d'entree de la librairie, doit etre Xx_Init pour une librairie libxx
  * (la majuscule est importante pour permettre un chargement par load libxx).
  */
+#ifndef CAM_ENTRYPOINT 
 #define CAM_ENTRYPOINT Sbig_Init
+#endif
 
 /*
  * Informations sur le driver, le nom est celui qui apparait quand on fait "package names"
  * et la version apparait avec la commande Tcl "package require libxx"
  */
+#ifndef CAM_LIBNAME
 #define CAM_LIBNAME "libsbig"
+#endif
+
 #define CAM_LIBVER "1.1"
 
 /*
  * Initialisation d'informations indispensables pour la librairie xx.
  */
+#ifndef CAM_DRIVNAME
 #define CAM_DRIVNAME "sbig"
+#endif
 
 #define CAM_INI sbig_cam_ini
 #define CAM_DRV sbig_cam_drv
