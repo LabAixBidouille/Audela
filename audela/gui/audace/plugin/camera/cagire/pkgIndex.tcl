eb7d902d79012efc4cd7385da9f7e45ad9a1986b
alainklotz <Alain Klotz>
2010-04-29 00:28:00 +0000

    camera Cagire

---
+++ audela/gui/audace/plugin/camera/cagire/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Mise a jour $Id: pkgIndex.tcl,v 1.1 2010-04-29 00:28:00 alainklotz Exp $
+#
+
+package ifneeded cagire 1.0 [ list source [ file join $dir cagire.tcl ] ]
+
