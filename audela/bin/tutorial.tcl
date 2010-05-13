bda79caf89f077e8f256a068f354e7a43a123a8a
robertdelmas <Robert Delmas>
2010-05-13 17:40:02 +0000

    Prise en compte de l'encodage UTF8 dans le Tutoriel. Remplacement de "langage.tcl" par "langage.ini". Conversion UTF8 (si nécessaire).

--- audela/bin/tutorial.tcl
+++ audela/bin/tutorial.tcl
@@ -2,12 +2,16 @@
 # Fichier : tutorial.tcl
 # Description : Lancement du tutorial
 # Auteur : Michel PUJOL
-# Mise a jour $Id: tutorial.tcl,v 1.2 2010-05-01 08:11:19 robertdelmas Exp $
+# Mise à jour $Id: tutorial.tcl,v 1.3 2010-05-13 17:40:02 robertdelmas Exp $
 #
 
+#--- Prise en compte du codage UTF8
+encoding system utf-8
+
+#---
 source version.tcl
 set langage english
-catch { source [ file join $::env(HOME) .audela langage.tcl ] }
+catch { source [ file join $::env(HOME) .audela langage.ini ] }
 cd ../gui/tutorial
 source tuto.tcl
 
