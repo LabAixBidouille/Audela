68f9566a1aa298bb8ec78cc22341c49337a3c21c
robertdelmas <Robert Delmas>
2010-05-23 16:16:39 +0000

    Conversion UTF8.

--- audela/gui/audace/plugin/tool/acqvideo/pkgIndex.tcl
+++ audela/gui/audace/plugin/tool/acqvideo/pkgIndex.tcl
@@ -1,7 +1,7 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.1 2008-04-17 20:39:34 robertdelmas Exp $
+# Mise à jour $Id: pkgIndex.tcl,v 1.2 2010-05-23 16:16:39 robertdelmas Exp $
 #
 
 package ifneeded acqvideo 1.0 [ list source [ file join $dir acqvideo.tcl ] ]
