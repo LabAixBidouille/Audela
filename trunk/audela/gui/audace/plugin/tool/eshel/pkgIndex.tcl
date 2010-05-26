89c7742bdb1cb3a07211c13c9e91a88016a08e3c
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2010-05-26 17:40:57 +0000

    Conversion UTF8.

--- trunk/audela/gui/audace/plugin/tool/eshel/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/tool/eshel/pkgIndex.tcl
@@ -1,7 +1,7 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.1 2009-11-07 08:13:07 michelpujol Exp $
+# Mise à jour $Id: pkgIndex.tcl,v 1.2 2010-05-26 17:40:57 robertdelmas Exp $
 #
 
 package ifneeded eshel 2.1 [ list source [ file join $dir eshel.tcl ] ]
