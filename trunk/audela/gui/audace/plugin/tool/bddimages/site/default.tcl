840133309feb1f3dd8c39c5a26ede158acfdb96a
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2010-05-28 10:34:05 +0000

    Conversion UTF8 (il y en a encore...).

--- trunk/audela/gui/audace/plugin/tool/bddimages/site/default.tcl
+++ trunk/audela/gui/audace/plugin/tool/bddimages/site/default.tcl
@@ -1,36 +1,41 @@
+#
+# Mise à jour $Id: default.tcl,v 1.2 2010-05-28 10:33:24 robertdelmas Exp $
+#
+
 proc dateobs {tabkey} {
 
-  #	  0         1         2   
-  #       0123456789012345678901
-  # date <2006-06-23T20:22:36.08>
-  set dateobs ""
-  foreach keyval $tabkey {
+   #     0         1         2
+   #       0123456789012345678901
+   # date <2006-06-23T20:22:36.08>
+   set dateobs ""
+   foreach keyval $tabkey {
 
-    set key [lindex $keyval 0]
-    set val [lindex [lindex $keyval 1] 1]
-   
+      set key [lindex $keyval 0]
+      set val [lindex [lindex $keyval 1] 1]
+
       switch $key {
-          "DATE-OBS" {
-	    set dateobs $val
-	    break
-	    }
-          default {
- 	    }
-	  }
-          # fin switch
+         "DATE-OBS" {
+            set dateobs $val
+            break
+         }
+         default {
+         }
       }
-      # fin foreach
-  if {$dateobs==""} { 
-    return [list 1 "-"]
-    } else {
-    set annee	 [string range $dateobs 0 3]
-    set mois	 [string range $dateobs 5 6]
-    set jour	 [string range $dateobs 8 9]
-    set heure	 [string range $dateobs 11 12]
-    set minute   [string range $dateobs 14 15]
-    set seconde  [string range $dateobs 17 end]
-    set dateiso "$annee-$mois-$jour\T$heure:$minute:$seconde"
-    }
-    
+      # fin switch
+   }
+   # fin foreach
+   if {$dateobs==""} {
+      return [list 1 "-"]
+   } else {
+      set annee   [string range $dateobs 0 3]
+      set mois    [string range $dateobs 5 6]
+      set jour    [string range $dateobs 8 9]
+      set heure   [string range $dateobs 11 12]
+      set minute  [string range $dateobs 14 15]
+      set seconde [string range $dateobs 17 end]
+      set dateiso "$annee-$mois-$jour\T$heure:$minute:$seconde"
+   }
+
 return [list 0 $dateiso]
 }
+
--- trunk/audela/gui/audace/plugin/tool/bddimages/site/ohp-120.tcl
+++ trunk/audela/gui/audace/plugin/tool/bddimages/site/ohp-120.tcl
@@ -1,39 +1,41 @@
+#
+# Mise à jour $Id: ohp-120.tcl,v 1.2 2010-05-28 10:33:44 robertdelmas Exp $
+#
+
 proc dateobs {tabkey} {
 
-  #	  0         1         2   
-  #       0123456789012345678901
-  # date <2006-06-23T20:22:36.08>
+   #     0         1         2
+   #       0123456789012345678901
+   # date <2006-06-23T20:22:36.08>
 
-  foreach keyval $tabkey {
+   foreach keyval $tabkey {
 
-    set key [lindex $keyval 0]
-    set val [lindex [lindex $keyval 1] 1]
-    
-    
-    switch $key {
-          "DATE-OBS" {
-	    set dateobs $val
-	    }
-          "TM-START" {
-	    set duree [string trim $val]
- 	    }
-          default {
- 	    }
-	  }
-          # fin switch
+      set key [lindex $keyval 0]
+      set val [lindex [lindex $keyval 1] 1]
+
+      switch $key {
+         "DATE-OBS" {
+            set dateobs $val
+         }
+         "TM-START" {
+            set duree [string trim $val]
+         }
+         default {
+         }
       }
-      # fin foreach
+      # fin switch
+   }
+   # fin foreach
 
-    set annee   [string range $dateobs 6 9]
-    set mois    [string range $dateobs 3 4]
-    set jour    [string range $dateobs 0 1]
-    set heure   [expr int($duree / 3600)]
-    set minute  [expr int($duree / 60 - $heure * 60)]
-    set seconde [expr $duree - $heure * 3600 - $minute * 60]
+   set annee   [string range $dateobs 6 9]
+   set mois    [string range $dateobs 3 4]
+   set jour    [string range $dateobs 0 1]
+   set heure   [expr int($duree / 3600)]
+   set minute  [expr int($duree / 60 - $heure * 60)]
+   set seconde [expr $duree - $heure * 3600 - $minute * 60]
 
-    set dateiso "$annee-$mois-$jour\T$heure:$minute:$seconde"
+   set dateiso "$annee-$mois-$jour\T$heure:$minute:$seconde"
 
 return [list 0 $dateiso]
 }
-
 
