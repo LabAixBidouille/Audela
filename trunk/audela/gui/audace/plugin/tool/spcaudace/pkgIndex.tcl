bcb366aab698287a71af7dcafebfeef367aa2157
bmauclaire <bmauclaire@04217e9b-e68c-4f9b-beca-5a77afb74039>
2010-06-24 20:48:12 +0000

    - spc_metaf :  * spc_traite2rinstrum met a zero les extrema du spectre 1c de l'etoile obtenue a l'aide de spc_rmextrema ; affiche le profil de la lampe de calibration une fois calibree.  * spc_traite2srinstrum :   + 100623 : d'abord tilt, puis smilex.   => l'ordre n'influence pas sur la bonne correction du slant/smilex. - spc_filter2 : spc_lowresfilterfile -> mise a 0 des pixels <0 a la fin a l'aide de spc_rmneg. - spc_operations :  * N : spc_rmneg : mise a 0 des pixels <0  * N : spc_rmextrema : mise a zero les valeurs delirantes de l'intensite. - spc_geom :  * spc_smileximgs : ajout d'un file rootname pour enlever extension du nom.  * spc_findtilt : decoupe aussi les bords bas et haut de l'image pour operer a la detection sans artefacts des bords.  * spc_smilex : gestion de l'inversion du rayon de courbure. - spc_var : set spcaudace(smilex_inv) 1 -> variable permettant l'inversion du courbure de la correction du smilex. -> CVS 100624 => SpcAudace 1.6.2 - 24/06/2010

--- trunk/audela/gui/audace/plugin/tool/spcaudace/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/tool/spcaudace/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise à jour $Id: pkgIndex.tcl,v 1.14 2010-05-26 06:03:57 robertdelmas Exp $
+# Mise à jour $Id: pkgIndex.tcl,v 1.15 2010-06-24 20:48:12 bmauclaire Exp $
 #
 
-package ifneeded spcaudace 1.6.1 [ list source [ file join $dir spcaudace.tcl ] ]
+package ifneeded spcaudace 1.6.2 [ list source [ file join $dir spcaudace.tcl ] ]
 
