3aa2a992edaea58e93b1eeb5a66429b5550a94f4
robertdelmas <Robert Delmas>
2010-07-12 15:31:37 +0000

    Outil "Modèle de pointage" obsolète, remplacé par la version 2.

--- audela/gui/audace/plugin/tool/modpoi/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Mise à jour $Id: pkgIndex.tcl,v 1.4 2010-05-26 06:40:10 robertdelmas Exp $
-#
-
-package ifneeded modpoi 1.0 [ list source [ file join $dir modpoi_go.tcl ] ]
-
