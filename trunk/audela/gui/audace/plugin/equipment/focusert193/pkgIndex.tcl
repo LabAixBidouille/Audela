c7e9b8cf3f1703897d8c83b4a13c3284d44a7fde
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2010-07-16 16:26:23 +0000

    Ajout d'un nouveau focuser.

---
+++ trunk/audela/gui/audace/plugin/equipment/focusert193/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Mise à jour $Id: pkgIndex.tcl,v 1.1 2010-07-16 16:26:23 robertdelmas Exp $
+#
+
+package ifneeded focusert193 1.0 [ list source [ file join $dir focusert193.tcl ] ]
+
