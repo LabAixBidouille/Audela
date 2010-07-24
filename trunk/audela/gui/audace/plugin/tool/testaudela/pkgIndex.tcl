06ecc0b3de8efdeb89b7241742fcc0e2f08f4a3d
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2010-07-24 08:29:35 +0000

    Changement de version. Conversion UTF8.

--- trunk/audela/gui/audace/plugin/tool/testaudela/pkgIndex.tcl
+++ trunk/audela/gui/audace/plugin/tool/testaudela/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.1 2010-07-23 16:31:04 michelpujol Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.2 2010-07-24 08:29:35 robertdelmas Exp $
 #
 
-package ifneeded testaudela 1.9 [ list source [ file join $dir testaudela.tcl ] ]
+package ifneeded testaudela 1.10 [ list source [ file join $dir testaudela.tcl ] ]
 
