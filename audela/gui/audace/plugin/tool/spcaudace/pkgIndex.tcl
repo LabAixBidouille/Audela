f9446eeb0ad80e0f05ba295fac205f2ec8f4a63e
bmauclaire <Benjixc@users.noreply.github.com>
2010-08-29 18:43:43 +0000

    Insersion dans le menu de spc_degauss et spc_periodogram. SpcAudace 1.6.5

--- audela/gui/audace/plugin/tool/spcaudace/pkgIndex.tcl
+++ audela/gui/audace/plugin/tool/spcaudace/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise à jour $Id: pkgIndex.tcl,v 1.17 2010-07-29 18:25:19 bmauclaire Exp $
+# Mise à jour $Id: pkgIndex.tcl,v 1.18 2010-08-29 18:43:43 bmauclaire Exp $
 #
 
-package ifneeded spcaudace 1.6.4 [ list source [ file join $dir spcaudace.tcl ] ]
+package ifneeded spcaudace 1.6.5 [ list source [ file join $dir spcaudace.tcl ] ]
 
