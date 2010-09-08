0b32cd86f71b7c3a4d71809f414047e0ac0269c3
Frédéric VACHIER <vachier@imcce.fr>
2010-09-08 13:15:50 +0000

    Ajout du driver linux d andor et doc. maj des fonctions native. maj des appels a la camera

---
+++ audela/gui/audace/plugin/camera/andorusb/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Mise a jour $Id: pkgIndex.tcl,v 1.1 2010-09-08 13:15:50 fredvachier Exp $
+#
+
+package ifneeded andorusb 1.0 [ list source [ file join $dir andorusb.tcl ] ]
+
