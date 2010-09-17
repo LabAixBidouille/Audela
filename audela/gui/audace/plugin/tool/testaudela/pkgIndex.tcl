7b75b3ce47834e43e269d1ca744eb3cb01d11aa3
Michel PUJOL <michel-pujol@orange.fr>
2010-09-17 15:57:41 +0000

    ajout d'un menu pour télécharger les images affichage du resultat des tests au fur et a mesure de l'execution des tests

--- audela/gui/audace/plugin/tool/testaudela/pkgIndex.tcl
+++ audela/gui/audace/plugin/tool/testaudela/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Fichier : pkgIndex.tcl
 # Description : Definition du point d'entree du plugin
-# Mise a jour $Id: pkgIndex.tcl,v 1.2 2010-07-24 08:29:35 robertdelmas Exp $
+# Mise a jour $Id: pkgIndex.tcl,v 1.3 2010-09-17 15:57:41 michelpujol Exp $
 #
 
-package ifneeded testaudela 1.10 [ list source [ file join $dir testaudela.tcl ] ]
+package ifneeded testaudela 1.11 [ list source [ file join $dir testaudela.tcl ] ]
 
