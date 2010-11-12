da2609a4cd6c59c20aabf90202235b205ef703ad
robertdelmas <Robert Delmas>
2010-11-12 07:29:44 +0000

    Re-création de la caméra Audor USB.

---
+++ audela/gui/audace/plugin/camera/andorusb/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Mise à jour $Id: pkgIndex.tcl,v 1.4 2010-11-12 07:29:16 robertdelmas Exp $
+#
+
+package ifneeded andorusb 1.0 [ list source [ file join $dir andorusb.tcl ] ]
+
