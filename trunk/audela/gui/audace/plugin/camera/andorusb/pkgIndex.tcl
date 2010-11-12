82edda49449ada1ff072404795304f93f2e30575
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2010-11-12 07:19:49 +0000

    Problème : L'en-tête ne se met pas à jour.

--- trunk/audela/gui/audace/plugin/camera/andorusb/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Mise à jour $Id: pkgIndex.tcl,v 1.2 2010-11-12 07:13:47 robertdelmas Exp $
-#
-
-package ifneeded andorusb 1.0 [ list source [ file join $dir andorusb.tcl ] ]
-
