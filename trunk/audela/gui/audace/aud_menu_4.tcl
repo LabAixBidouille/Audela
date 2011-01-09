48c051eafee118f3eefba1750883cd34609c703b
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-01-09 10:31:25 +0000

    Script obsolète. Impacts de la réorganisation des ex-menus "Prétraitement" et "Traitement" dans les nouveaux menus "Images" et "Analyse --> Extraire".

--- trunk/audela/gui/audace/aud_menu_4.tcl
+++
@@ -1,5 +0,0 @@
-#
-# Fichier : aud_menu_4.tcl
-# Description : Script regroupant les fonctionnalites du menu Images
-# Mise à jour $Id: aud_menu_4.tcl,v 1.26 2011-01-09 10:27:48 robertdelmas Exp $
-#
