201174fb40808b3df7e017b5907f21f2db6e0cd9
jacquesmichelet <jacquesmichelet@04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-01-09 14:37:44 +0000

    Ajout d'un outil de photométrie différentielle. Vivement SVN

---
+++ trunk/audela/gui/audace/plugin/tool/photometrie/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# Fichier : pkgIndex.tcl
+# Description : Definition du point d'entree du plugin
+# Mise à jour $Id: pkgIndex.tcl,v 1.1 2011-01-09 14:36:20 jacquesmichelet Exp $
+#
+
+package ifneeded photometrie 1.0 [ list source [ file join $dir photometrie_if.tcl ] ]
+
