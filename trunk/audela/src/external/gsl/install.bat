217e205610b4cb3b36e675d7a19ff410a886fd14
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-01-18 22:14:30 +0000

    libgsl 1.8 pour Windows

--- trunk/audela/src/external/gsl/install.bat
+++ trunk/audela/src/external/gsl/install.bat
@@ -1,7 +1,23 @@
-mkdir ..\include
-mkdir ..\include\win
-mkdir ..\include\win\gsl
-@echo
-copy include\*.* ..\include\win\gsl
-copy lib\*.* ..\lib
-copy bin\*.* ..\..\..\bin
+
+rem  parametre %1 = Release ou Debug
+rem Si pas de parametre , la valeur par defaut est Release
+if not "%1" == "" set CONFIG=%1
+if "%1" == ""     set CONFIG=Release
+
+rem definition de la commande copy
+rem
+set COPY = xcopy /D /Y /L
+
+rem copie des entetes des sources *.h
+set sourceDir=..\..\gsl-1.8\gsl
+set include_dir= ..\..\..\include\win\gsl
+
+rem copie des entetes des librairies
+set lib_dir=..\..\..\lib
+%COPY% %CONFIG%\libgsl.lib          %lib_dir%
+%COPY% %CONFIG%\libgslcblas.lib     %lib_dir%
+
+rem copie des entetes des librairies *.dll
+set bin_dir=..\..\..\..\..\bin
+%COPY% %CONFIG%\libgsl.dll          %bin_dir%
+%COPY% %CONFIG%\libgslcblas.dll     %bin_dir%
