7d025fa6bb257450efc876c823c98d050ffe11aa
jberthier <jberthier@04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-01-21 11:07:12 +0000

    Maj bddimages - ajout site marly

---
+++ trunk/audela/gui/audace/plugin/tool/bddimages/site/marly.tcl
@@ -0,0 +1,37 @@
+#
+# Mise à jour $Id: marly.tcl,v 1.1 2011-01-21 11:07:12 jberthier Exp $
+#
+
+proc dateobs {tabkey} {
+
+   #     0         1         2
+   #       0123456789012345678901
+   # date <2006-06-23T20:22:36.08>
+
+   foreach keyval $tabkey {
+
+      set key [lindex $keyval 0]
+      set val [lindex [lindex $keyval 1] 1]
+
+      switch $key {
+         "DATE-OBS" {
+            set dateobs [string trim $val]
+         }
+         "TU-START" {
+            set heurobs [string trim $val]
+         }
+         default {
+         }
+      }
+      # fin switch
+   }
+   # fin foreach
+
+   set annee   [expr [string range $dateobs 6 9] + 1900]
+   set mois    [string range $dateobs 3 4]
+   set jour    [string range $dateobs 0 1]
+   set dateiso "$annee-$mois-$jour\T$heurobs"
+
+   return [list 0 $dateiso]
+}
+
