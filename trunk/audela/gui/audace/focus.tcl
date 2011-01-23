c49f9a816c6f245737bdcd0632d586d37643f6ac
michelpujol <michelpujol@04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-01-23 18:26:47 +0000

    ajout de la procedure ::focus::getPosition ajoute de la procedure ::focus::surveille_goto

--- trunk/audela/gui/audace/focus.tcl
+++ trunk/audela/gui/audace/focus.tcl
@@ -2,7 +2,7 @@
 # Fichier : focus.tcl
 # Description : Centralise les commandes du focus du telescope
 # Auteur : Michel PUJOL
-# Mise à jour $Id: focus.tcl,v 1.9 2010-07-15 15:44:30 robertdelmas Exp $
+# Mise à jour $Id: focus.tcl,v 1.10 2011-01-23 18:26:47 michelpujol Exp $
 #
 
 namespace eval ::focus {
@@ -40,6 +40,7 @@ proc ::focus::init { } {
 proc ::focus::move { focuserLabel command } {
    if { "$focuserLabel" != "" } {
       ::$focuserLabel\::move $command
+      set ::audace(focus,currentFocus) [getPosition $focuserLabel]
    }
 }
 
@@ -68,9 +69,76 @@ proc ::focus::setSpeed { focuserLabel { value "0" } } {
 #  ::focus::goto
 #     envoie le focaliseur a moteur pas a pas a une position predeterminee (AudeCom)
 #------------------------------------------------------------
-proc ::focus::goto { focuserLabel } {
+proc ::focus::goto { focuserLabel { blocking 0 } { gotoButton "" } } {
+   variable private
    if { "$focuserLabel" != "" } {
-      ::$focuserLabel\::goto
+
+      #--- Gestion des boutons Goto et Match
+      if { $gotoButton != "" } {
+         $gotoButton configure -state disabled
+         update
+      }
+
+      set catchError [catch {
+         ::$focuserLabel\::goto $blocking
+
+         if { $blocking == 0  } {
+            #--- Boucle tant que le focus n'est pas arretee (si on n'utilise pas le mode bloquant du goto)
+            set position [::focus::getPosition $focuserLabel]
+            #--- j'attends que le focus commence a bouger
+            #--- car sinon la boucle de surveillance va considerer que les
+            #--- coordonnees n'ont pas changé et va s'arreter immediatement
+            after 500
+            set private(gotoIsRunning) 1
+            set derniereBoucle [ ::focus::surveille_goto $focuserLabel $position ]
+            if { $derniereBoucle == 1 } {
+               #--- j'attends que la variable soit remise a zero
+               vwait ::focus::private(gotoIsRunning)
+            }
+         } else {
+            displayCurrentPosition $focuserLabel
+         }
+      }]
+
+      #--- je reactive du bouton
+      if { $gotoButton != "" } {
+         $gotoButton configure -state normal
+         update
+      }
+
+      if { $catchError != 0 } {
+        error $::errorInfo
+     }
+   }
+}
+
+#------------------------------------------------------------
+# surveille_goto
+#    Surveille si la fonction goto est active
+#
+# Parametres :
+#    position    : position du focuser
+# Return :
+#    0 si derniere boucle
+#    1 si nouvelle boucle est lancee
+#------------------------------------------------------------
+proc ::focus::surveille_goto { focuserLabel position } {
+   variable private
+
+   set position1 [::focus::getPosition $focuserLabel]
+   if { $position1 == "" } {
+      #--- j'arrete la boucle de surveillance car les coordonnees n'ont pas pu etre recuperees
+      set private(gotoIsRunning) "0"
+      return 0
+   }
+   if { [expr abs($position - $position1) ] > 0.1 } {
+      after 500 ::focus::surveille_goto $focuserLabel $position1
+      #--- je retourne 1 pour signaler que ce n'est pas pas la derniere boucle
+      return 1
+   } else {
+      #--- j'arrete la surveillance car le GOTO est termine
+      set private(gotoIsRunning) "0"
+      return 0
    }
 }
 
@@ -95,6 +163,17 @@ proc ::focus::initPosition { focuserLabel } {
       ::$focuserLabel\::initPosition
    }
 }
+
+#------------------------------------------------------------
+#  ::focus::getPosition
+#     initialise la position du focuser a 0
+#------------------------------------------------------------
+proc ::focus::getPosition { focuserLabel } {
+   if { "$focuserLabel" != "" } {
+      ::$focuserLabel\::getPosition
+   }
+}
+
 
 #------------------------------------------------------------
 #  ::focus::possedeControleEtendu
