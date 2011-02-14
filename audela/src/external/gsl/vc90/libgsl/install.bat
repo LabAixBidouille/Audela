fc1ba4d61f52354395f912c96a0bbd29cb8d552b
Michel PUJOL <michel-pujol@orange.fr>
2011-02-14 21:13:30 +0000

    deplace install.bat dans les sous-repertoires libgsl et libgslblas

--- audela/src/external/gsl/vc90/libgsl/install.bat
+++ audela/src/external/gsl/vc90/libgsl/install.bat
@@ -4,11 +4,9 @@ rem Si pas de parametre , la valeur par defaut est Release
 if not "%1" == "" set CONFIG=%1
 if "%1" == ""     set CONFIG=Release
 
-set sourceDir=..\gsl-1.8\gsl
-set include_dir=..\..\include\win\gsl
-
-set lib_dir=..\..\lib
-set bin_dir=..\..\..\..\bin
+set include_dir=..\..\..\include\win\gsl
+set lib_dir=..\..\..\lib
+set bin_dir=..\..\..\..\..\bin
 
 if %CONFIG% == clean  goto target_clean
 
@@ -17,15 +15,13 @@ rem
 set COPY=xcopy /D /Y /I
 
 rem copie des entetes des sources *.h
-%COPY% %sourceDir%\*.h                          %include_dir%
+%COPY% ..\..\gsl-1.8\gsl\*.h             %include_dir%
 
 rem copie des entetes des librairies
-%COPY% libgsl\%CONFIG%\libgsl.lib               %lib_dir%
-%COPY% libgslcblas\%CONFIG%\libgslcblas.lib     %lib_dir%
+%COPY% %CONFIG%\libgsl.lib               %lib_dir%
 
 rem copie des entetes des librairies *.dll
-%COPY% libgsl\%CONFIG%\libgsl.dll               %bin_dir%
-%COPY% libgslcblas\%CONFIG%\libgslcblas.dll     %bin_dir%
+%COPY% %CONFIG%\libgsl.dll               %bin_dir%
 
 goto target_end
 
@@ -33,10 +29,8 @@ goto target_end
 @del /Q /s %include_dir%
 
 del /Q %lib_dir%\libgsl.lib
-del /Q %lib_dir%\libgslcblas.lib
 
 del /Q %bin_dir%\libgsl.dll
-del /Q %bin_dir%\libgslcblas.dll
 
 :target_end
 
---
+++ audela/src/external/gsl/vc90/libgslcblas/install.bat
@@ -0,0 +1,35 @@
+@echo off
+rem  parametre %1 = Release ou Debug
+rem Si pas de parametre , la valeur par defaut est Release
+if not "%1" == "" set CONFIG=%1
+if "%1" == ""     set CONFIG=Release
+
+set sourceDir=..\..\gsl-1.8\gsl
+set include_dir=..\..\..\include\win\gsl
+
+set lib_dir=..\..\..\lib
+set bin_dir=..\..\..\..\..\bin
+
+if %CONFIG% == clean  goto target_clean
+
+rem definition de la commande copy
+rem
+set COPY=xcopy /D /Y /I
+
+rem copie des entetes des librairies
+%COPY% %CONFIG%\libgslcblas.lib     %lib_dir%
+
+rem copie des entetes des librairies *.dll
+%COPY% %CONFIG%\libgslcblas.dll     %bin_dir%
+
+goto target_end
+
+:target_clean
+@del /Q /s %include_dir%
+
+del /Q %lib_dir%\libgslcblas.lib
+
+del /Q %bin_dir%\libgslcblas.dll
+
+:target_end
+
