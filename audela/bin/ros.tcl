f351727911011afbabe6cac77f59e00634316d27
Frédéric VACHIER <vachier@imcce.fr>
2011-02-18 01:39:04 +0000

    Robotic Observatory Software : preparation de la phase d installation en vue du passage a SVN

--- audela/bin/ros.tcl
+++ audela/bin/ros.tcl
@@ -1,20 +1,32 @@
 #
 # Robotic Observatory Control
-# Mise à jour $Id: ros.tcl,v 1.8 2010-06-11 17:38:46 robertdelmas Exp $
+# Mise à jour $Id: ros.tcl,v 1.9 2011-02-18 01:39:04 fredvachier Exp $
 #
+puts "--------------------------------"
+puts "- Robotic Observatory Software -"
+puts "-       primaries logs"
+puts "--------------------------------"
 
 #--- Add audela/lib directory to ::auto_path if it doesn't already exist
 set audelaLibPath [file join [file join [file dirname [file dirname [info nameofexecutable]] ] lib]]
+puts "audelaLibPath=$audelaLibPath"
+
 if { [lsearch $::auto_path $audelaLibPath] == -1 } {
    lappend ::auto_path $audelaLibPath
 }
+puts "::auto_path=$::auto_path"
 
 set err [catch {wm withdraw .} msg]
 set ros(withtk) 1
 if {$err==1} {
    set ros(withtk) 0
+   puts "TK: Non actif"
+} else {
+   puts "TK: Actif"
 }
+
 set name [file tail [file rootname [info nameofexecutable]]]
+puts "Prog=$name"
 set k [string first _ "$name"]
 if {$k>0} {
    set k [expr $k-1]
@@ -22,19 +34,32 @@ if {$k>0} {
    set k end
 }
 set name [string range $name 0 $k]
-#set ros(ros_install,audelabin) [pwd]
+puts "name=$name"
+
+
 set ros(ros_install,audelabin) $::audela_start_dir
+puts "audelabin=$ros(ros_install,audelabin)"
+cd $ros(ros_install,audelabin)
+
 if {$name=="audela"} {
-   cd ../ros
-   set errno [catch {source ros_install.tcl} msg]
+   puts "log= Demarrage de l install automatique"
+   puts "pwd= [pwd]"
+   puts "Lancement= source [file join $::audela_start_dir ros_install.tcl]"
+   set errno [catch {source [file join $::audela_start_dir ros_install.tcl]} msg]
 } else {
+   puts "log= Demarrage du programme"
+   # programme ros en mode console
    cd ../ros/src/$name
    set errno [catch {source $name.tcl} msg]
 }
+
 puts "errno=$errno"
+puts "msg=$msg"
 if {$errno==1} {
+   puts "log= Erreur grave"
+   # se met dans ros
    cd ../..
-   set f [open check_error.txt a]
+   set f [open ros_check_error.txt a]
    puts $f "==============================\n[mc_date2iso8601 now] :"
    puts $f $msg
    close $f
@@ -45,6 +70,6 @@ if {$errno==1} {
    }
 }
 if {$ros(withtk)==1} {
-   exit
+   #exit
 }
 
