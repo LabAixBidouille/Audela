b1d63be7254c8a6ae94f56b08162941641001aaa
Frédéric VACHIER <vachier@imcce.fr>
2011-02-20 14:42:40 +0000

    Zadko : pad devenu obsolete, voire meme nuisible

--- audela/gui/audace/plugin/pad/ZADKOpad/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Mise à jour $Id: pkgIndex.tcl,v 1.2 2010-05-17 15:57:34 robertdelmas Exp $
-#
-
-package ifneeded zadkopad 1.0 [ list source [ file join $dir zadkopad.tcl ] ]
-
