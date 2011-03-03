88f4b5b7d021a90b969873ef3bdfacfc95b9c602
Jacques MICHELET <jacques.michelet@laposte.net>
2011-03-03 15:29:38 +0000

    Correctif pour la compilation en mode debug

--- audela/src/external/libdcraw/vc90/install.bat
+++ audela/src/external/libdcraw/vc90/install.bat
@@ -1,8 +1,14 @@
+@echo off
+rem  parametre %1 = Release ou Debug
+rem Si pas de parametre , la valeur par defaut est Release
+if not "%1" == "" set CONFIG=%1
+if "%1" == ""     set CONFIG=Release
+
 echo copy libdcraw.dll to bin directory
-copy Release\*.dll     ..\..\..\..\bin
+copy %CONFIG%\*.dll     ..\..\..\..\bin
 
 echo copy libdcraw.lib to lib directory
-copy Release\*.lib     ..\..\lib
+copy %CONFIG%\*.lib     ..\..\lib
 
 echo copy libdcraw.h to include directory
 copy ..\src\libdcraw.h ..\..\include
