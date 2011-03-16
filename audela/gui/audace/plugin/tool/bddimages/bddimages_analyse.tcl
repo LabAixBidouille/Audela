77ac14833e85a50e1ddcc8710f8c9ed7ea9478e8
Frédéric VACHIER <vachier@imcce.fr>
2011-03-16 14:36:45 +0000

    analyse

---
+++ audela/gui/audace/plugin/tool/bddimages/bddimages_analyse.tcl
@@ -0,0 +1,46 @@
+#--------------------------------------------------
+# source audace/plugin/tool/bddimages/bddimages_analyse.tcl
+#--------------------------------------------------
+#
+# Fichier        : bddimages_analyse.tcl
+# Description    : Environnement de recherche des images
+#                  dans la base de donnees
+# Auteur         : Frédéric Vachier
+# Mise à jour $Id: bddimages_liste.tcl 6858 2011-03-06 14:19:15Z fredvachier $
+#
+#--------------------------------------------------
+#
+# - namespace bddimages_analyse
+#
+#--------------------------------------------------
+#
+#   -- Fichiers source externe :
+#
+#  bddimages_analyse.cap
+#
+#--------------------------------------------------
+
+namespace eval bddimages_analyse {
+
+   global audace
+   global bddconf
+
+
+
+
+
+
+
+   proc ::bddimages_analyse::charge_cata { img_list } {
+
+      global bddconf
+
+      ::console::affiche_resultat "charge_cata\n"
+      ::console::affiche_resultat "img_list $img_list\n"
+
+   }
+
+
+
+}
+
