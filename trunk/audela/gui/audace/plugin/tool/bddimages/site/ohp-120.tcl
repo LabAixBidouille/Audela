4119310c75d3f94c46dbd419f67a88bc6332cc8b
jberthier <jberthier@04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-03-22 15:48:52 +0000

    Maj Audela - bddimages: site/ohp-120

--- trunk/audela/gui/audace/plugin/tool/bddimages/site/ohp-120.tcl
+++ trunk/audela/gui/audace/plugin/tool/bddimages/site/ohp-120.tcl
@@ -9,18 +9,25 @@ proc chg_tabkey { tabkey } {
    # date <2006-06-23T20:22:36.08>
 
    set dateobs [get_tabkey $tabkey "DATE-OBS"]
-   set duree   [get_tabkey $tabkey "TM-START"]
-   set annee   [string range $dateobs 6 9]
-   set mois    [string range $dateobs 3 4]
-   set jour    [string range $dateobs 0 1]
-   set heure   [expr int($duree / 3600)]
-   set minute  [expr int($duree / 60 - $heure * 60)]
-   set seconde [expr $duree - $heure * 3600 - $minute * 60]
-   set dateiso "$annee-$mois-$jour\T$heure:$minute:$seconde"
+   if { [regexp {(\d+)-(\d+)-(\d+)( |T)(\d+):(\d+):(\d+)(\.*\d*)} $dateobs dateiso aa mm jj sep h m s sd] } {
+      # Si date ISO
+      set dateiso $dateobs
+   } else {
+      # Sinon
+      set duree   [get_tabkey $tabkey "TM-START"]
+      set annee   [string range $dateobs 6 9]
+      set mois    [string range $dateobs 3 4]
+      set jour    [string range $dateobs 0 1]
+      set heure   [expr int($duree / 3600)]
+      set minute  [expr int($duree / 60 - $heure * 60)]
+      set seconde [expr $duree - $heure * 3600 - $minute * 60]
+      set dateiso "$annee-$mois-$jour\T$heure:$minute:$seconde"
+   }
    set tabkey   [update_tabkey $tabkey "DATE-OBS" $dateiso]
+
    set exposure [get_tabkey $tabkey "TM-EXPOS"]
    set tabkey   [add_tabkey $tabkey "EXPOSURE" $exposure]
 
-return $tabkey
+   return [list 0 $tabkey]
 }
 
