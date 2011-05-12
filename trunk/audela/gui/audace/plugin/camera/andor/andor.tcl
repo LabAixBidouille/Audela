789b6c3f9405f74af466c67a0148f33b2da7cada
robertdelmas <robertdelmas @04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-05-12 13:52:28 +0000

    Correction du bug suivant : J'ai constate ce probleme de blocage de l'obturateur dans sa derniere position d'enregistrement des parametres, lors de la connexion automatique de la camera au  demarrage d'Aud'ACE. Signale par Jerome CARON et confirme par Gerald ROUSSEAU.

--- trunk/audela/gui/audace/plugin/camera/andor/andor.tcl
+++ trunk/audela/gui/audace/plugin/camera/andor/andor.tcl
@@ -442,21 +442,25 @@ proc ::andor::setShutter { camItem shutterState ShutterOptionList } {
    set conf(andor,foncobtu) $shutterState
    set camNo $private($camItem,camNo)
 
-   if { [ info exists private(frm) ] } {
-      #--- Gestion du mode de fonctionnement
-      switch -exact -- $shutterState {
-         0  {
-            set private(foncobtu) $caption(andor,obtu_ouvert)
-            cam$camNo shutter "opened"
-         }
-         1  {
-            set private(foncobtu) $caption(andor,obtu_ferme)
-            cam$camNo shutter "closed"
-         }
-         2  {
-            set private(foncobtu) $caption(andor,obtu_synchro)
-            cam$camNo shutter "synchro"
-         }
+   #--- Gestion du mode de fonctionnement
+   switch -exact -- $shutterState {
+      0  {
+         #--- j'envoie la commande a la camera
+         cam$camNo shutter "opened"
+         #--- je mets a jour le widget dans la fenetre de configuration si elle est ouverte
+         set private(foncobtu) $caption(andor,obtu_ouvert)
+      }
+      1  {
+         #--- j'envoie la commande a la camera
+         cam$camNo shutter "closed"
+         #--- je mets a jour le widget dans la fenetre de configuration si elle est ouverte
+         set private(foncobtu) $caption(andor,obtu_ferme)
+      }
+      2  {
+         #--- j'envoie la commande a la camera
+         cam$camNo shutter "synchro"
+         #--- je mets a jour le widget dans la fenetre de configuration si elle est ouverte
+         set private(foncobtu) $caption(andor,obtu_synchro)
       }
    }
 }
