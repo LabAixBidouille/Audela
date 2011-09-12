0876384be46e9be97cd9ecc78ef32c63e6dce33a
robertdelmas <robertdelmas @04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-09-12 19:03:53 +0000

    Retour arriere suite fausse manip de Raymond.

--- trunk/audela/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Mise à jour $Id: pkgIndex.tcl 6795 2011-02-26 16:05:27Z michelpujol $
-#
-
-package ifneeded spytimer 1.0 [ list source [ file join $dir spytimer.tcl ] ]
-
