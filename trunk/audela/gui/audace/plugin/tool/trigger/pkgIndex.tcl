1191cb7a0c2aad5e86bfc39708823ecafa3df9c9
robertdelmas <robertdelmas @04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-09-12 18:53:45 +0000

    Retour arriere suite fausse manip de Raymond.

--- trunk/audela/gui/audace/plugin/tool/trigger/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Mise à jour $Id$
-#
-
-package ifneeded trigger 1.0 [ list source [ file join $dir trigger.tcl ] ]
-
