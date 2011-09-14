10e1f182c7d6f05177b6fdfbd12537bc6ea28cd6
alainklotz <alainklotz@04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-09-14 17:36:14 +0000

    copie les fichiers indispensables pour ETEL

--- trunk/audela/src/external/etel/install.bat
+++ trunk/audela/src/external/etel/install.bat
@@ -1,3 +1,9 @@
 copy include\*.h ..\include
+
 copy lib\dsa20c.lib   ..\lib
+
 copy bin\dsa20c.dll ..\..\..\bin
+copy bin\etb10c.dll ..\..\..\bin
+copy bin\lib10c.dll ..\..\..\bin
+copy bin\dmd10c.dll ..\..\..\bin
+copy bin\ekd10c.dll ..\..\..\bin
