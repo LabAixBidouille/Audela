058de0528b125913410574b691ffdcf026418ceb
jacquesmichelet <jacquesmichelet@04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-12-07 20:01:14 +0000

    Suppression d'avertissements par gcc

--- trunk/audela/src/include/libtel/libstruc.h
+++ trunk/audela/src/include/libtel/libstruc.h
@@ -4,17 +4,17 @@
  * Copyright (C) 1998-2004 The AudeLA Core Team
  *
  * Initial author : Alain KLOTZ <alain.klotz@free.fr>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or (at
  * your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -86,7 +86,7 @@ extern "C" {            /* Assume C declarations for C++ */
    struct telprop *next;
 
 /*Tcl_TimerToken timerToken; \ */
-   
+
 
 extern char *tel_ports[];
 
@@ -104,6 +104,8 @@ struct telini {
 /* --- Structure sur la liste des CCD disponibles pour ce ---*/
 /* --- telescope et leurs parametres d'initialisation           ---*/
 extern struct telini tel_ini[];
+
+struct telprop;
 
 struct tel_drv_t {
     int (*tel_correct) (struct telprop *tel, char *alphaDirection, double alphaDistance, char *deltaDirection, double deltaDistance);
