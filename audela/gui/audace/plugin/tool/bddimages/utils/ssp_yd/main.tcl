29474e98deb1f95d81021f91ad337bc73f1ae74c
damerdji <damerdji@04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-12-13 13:40:25 +0000

    ssp_yd

--- audela/gui/audace/plugin/tool/bddimages/utils/ssp_yd/main.tcl
+++ audela/gui/audace/plugin/tool/bddimages/utils/ssp_yd/main.tcl
@@ -1,5 +1,5 @@
 
-# source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/ssp/main.tcl
+# source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/ssp_yd/main.tcl
 
 ##
 # @file
@@ -9,7 +9,7 @@
 # @brief Traitement des archives dans bddimages a partir de la base de donnees SSP
 
 source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/astroid/get_one_image.tcl
-source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/astroid/get_cata.tcl
+source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/ssp_yd/get_cata.tcl
 source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/astroid/imprimlist.tcl
 source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/astroid/visu.tcl
 source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/astroid/astrometry.tcl
@@ -86,3 +86,41 @@ gren_info "****************************************************************** \n
 
 
 gren_info "****************************************************************** \n"
+# Recupere les noms des fichiers
+   set fitsfile "$bddconf(dirbase)/$ssp_image(fits_dir)/$ssp_image(fits_filename)"
+   ::console::affiche_resultat "FILE : $fitsfile\n"
+   set catafile "$bddconf(dirbase)/$ssp_image(dir_cata_file)/$ssp_image(cata_filename)"
+   ::console::affiche_resultat "CATA : $catafile\n"
+
+   # Recupere le catalogue des sources correspondant a l image
+   set listsources [get_cata $catafile]
+
+   # Affichage la liste des sources
+   #imprim_sources $listsources
+
+   # Affichage de l image
+   affich_image $fitsfile
+   ::console::affiche_resultat "IMAGE AFFICHEE\n"
+
+   # Affichage centre champ de l image
+   set centre [list {"CENTRE" {} {} } [list [list [list "CENTRE" [list $ssp_image(ra) $ssp_image(dec)] {}]]]]
+   affich_rond $centre "CENTRE" "red" 7
+   ::console::affiche_resultat "ROND ROUGE: centre du champ provenant du header fits\n"
+  
+
+   # Recupere le catalog astrometrique
+   set astrometric_list [get_astrometric_catalog $ssp_image(ra) $ssp_image(dec) 30]
+
+   # Affichage des ronds verts
+   affich_rond $astrometric_list "TYCHO2" "blue" 2
+   ::console::affiche_resultat "ROND BLEU: Etoiles tycho 2\n"
+
+   set astrometric_list [add_sun_star "TYCHO2" $astrometric_list]
+
+   # Affichage des ronds verts
+   affich_rond $astrometric_list "SUNLIKE" "yellow" 2
+   ::console::affiche_resultat "ROND JAUNE: Etoiles de type solaire\n"
+
+   set starident [ identification $listsources "USNOA2" $astrometric_list "TYCHO2" 30.0 10.0 10.0 ]
+   affich_rond $starident "TYCHO2" "red" 2
+
