a3a7e69fec3e611592f7ed8c657fd9caab1f3767
Frédéric VACHIER <vachier@imcce.fr>
2011-12-14 10:52:06 +0000

    ssp fv

---
+++ audela/gui/audace/plugin/tool/bddimages/utils/ssp_sex/main.tcl
@@ -0,0 +1,4 @@
+
+# source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/ssp_sex/main.tcl
+
+
--- audela/gui/audace/plugin/tool/bddimages/utils/ssp_yd/main.tcl
+++ audela/gui/audace/plugin/tool/bddimages/utils/ssp_yd/main.tcl
@@ -1,6 +1,7 @@
 
 # source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/ssp_yd/main.tcl
 
+
 ##
 # @file
 #
@@ -11,14 +12,20 @@
 source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/astroid/get_one_image.tcl
 source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/ssp_yd/get_cata.tcl
 source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/astroid/imprimlist.tcl
-source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/astroid/visu.tcl
+source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/ssp_yd/visu.tcl
 source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/astroid/astrometry.tcl
 source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/astroid/photometry.tcl
 source /srv/develop/audela/gui/audace/plugin/tool/bddimages/utils/astroid/identification.tcl
 source /srv/develop/audela/gui/audace/vo_tools.tcl
+source /srv/develop/audela/gui/audace/plugin/tool/av4l/av4l_extraction.tcl
 
 ::bddimagesXML::load_xml_config
 ::bddimagesXML::get_config bddimages_cador
+
+cleanmark
+
+gren_info "****************************************************************** \n"
+gren_info "** info de la configuration \n"
 
 gren_info "SSP_PLUGIN: name    : $bddconf(name)    \n"
 gren_info "SSP_PLUGIN: dbname  : $bddconf(dbname)  \n"
@@ -36,6 +43,16 @@ gren_info "SSP_PLUGIN: dirtmp  : $bddconf(dirtmp)  \n"
 gren_info "SSP_PLUGIN: limit   : $bddconf(limit)   \n"
 
 gren_info "****************************************************************** \n"
+gren_info "** info de la visu \n"
+
+   #set bddconf(visuno) [::visu::create $bddconf(bufno) 1]
+   set bddconf(visuno) 1
+   set bddconf(bufno) [::confVisu::getBufNo $bddconf(visuno)]
+   gren_info "SSP_PLUGIN: bufNo   : $bddconf(bufno)   \n"
+   gren_info "SSP_PLUGIN: visuNo  : $bddconf(visuno)   \n"
+
+gren_info "****************************************************************** \n"
+gren_info "** info de la base \n"
 
    # Selection de la bdd
    set err [catch {::bddimages_sql::sql query "use $bddconf(dbname);"} msg]
@@ -66,27 +83,30 @@ gren_info "****************************************************************** \n
 
    set nbimg $resultsql
 
-gren_info "****************************************************************** \n"
+   gren_info "****************************************************************** \n"
+   gren_info "** Chargement d'une image \n"
 
 
-       set r [get_one_image]
-       gren_info "SSP_PLUGIN: result one_ssp   : $r \n"
+   set r [get_one_image]
+   gren_info "SSP_PLUGIN: result one_ssp   : $r \n"
 
-       gren_info "SSP_PLUGIN:idbddcata  = $ssp_image(idbddcata)\n"
-       gren_info "SSP_PLUGIN:idbddimage = $ssp_image(idbddimg)\n"
-       gren_info "SSP_PLUGIN:cata       = $ssp_image(dir_cata_file)/$ssp_image(cata_filename)\n"
-       gren_info "SSP_PLUGIN:image      = $ssp_image(fits_dir)/$ssp_image(fits_filename)\n"
-       gren_info "SSP_PLUGIN:idheader   = $ssp_image(idheader)\n"
-       gren_info "SSP_PLUGIN:dateobs    = $ssp_image(dateobs)\n"
-       gren_info "SSP_PLUGIN:ra         = $ssp_image(ra)\n"
-       gren_info "SSP_PLUGIN:dec        = $ssp_image(dec)\n"
-       gren_info "SSP_PLUGIN:telescop   = $ssp_image(telescop)\n"
-       gren_info "SSP_PLUGIN:exposure   = $ssp_image(exposure)\n"
-       gren_info "SSP_PLUGIN:filter     = $ssp_image(filter)\n"
+   gren_info "SSP_PLUGIN:idbddcata  = $ssp_image(idbddcata)\n"
+   gren_info "SSP_PLUGIN:idbddimage = $ssp_image(idbddimg)\n"
+   gren_info "SSP_PLUGIN:cata       = $ssp_image(dir_cata_file)/$ssp_image(cata_filename)\n"
+   gren_info "SSP_PLUGIN:image      = $ssp_image(fits_dir)/$ssp_image(fits_filename)\n"
+   gren_info "SSP_PLUGIN:idheader   = $ssp_image(idheader)\n"
+   gren_info "SSP_PLUGIN:dateobs    = $ssp_image(dateobs)\n"
+   gren_info "SSP_PLUGIN:ra         = $ssp_image(ra)\n"
+   gren_info "SSP_PLUGIN:dec        = $ssp_image(dec)\n"
+   gren_info "SSP_PLUGIN:telescop   = $ssp_image(telescop)\n"
+   gren_info "SSP_PLUGIN:exposure   = $ssp_image(exposure)\n"
+   gren_info "SSP_PLUGIN:filter     = $ssp_image(filter)\n"
 
 
-gren_info "****************************************************************** \n"
-# Recupere les noms des fichiers
+   gren_info "****************************************************************** \n"
+   gren_info "** Lecture du CATA \n"
+
+   # Recupere les noms des fichiers
    set fitsfile "$bddconf(dirbase)/$ssp_image(fits_dir)/$ssp_image(fits_filename)"
    ::console::affiche_resultat "FILE : $fitsfile\n"
    set catafile "$bddconf(dirbase)/$ssp_image(dir_cata_file)/$ssp_image(cata_filename)"
@@ -104,26 +124,56 @@ gren_info "****************************************************************** \n
 
    # Affichage centre champ de l image
    set centre [list {"CENTRE" {} {} } [list [list [list "CENTRE" [list $ssp_image(ra) $ssp_image(dec)] {}]]]]
-   affich_rond $centre "CENTRE" "red" 7
-   ::console::affiche_resultat "ROND ROUGE: centre du champ provenant du header fits\n"
-  
+    affich_rond $centre "CENTRE" "red" 1
+ #   ::console::affiche_resultat "ROND ROUGE: centre du champ provenant du header fits\n"
 
-   # Recupere le catalog astrometrique
-   set astrometric_list [get_astrometric_catalog $ssp_image(ra) $ssp_image(dec) 30]
+   gren_info "****************************************************************** \n"
+# {IMG {ra dec poserr mag magerr} 
+#      {id flag xpos ypos instr_mag err_mag flux_sex err_flux_sex ra dec calib_mag 
+#      calib_mag_ss1 err_calib_mag_ss1 calib_mag_ss2 err_calib_mag_ss2 
+#      nb_neighbours radius background_sex x2_momentum_sex y2_momentum_sex 
+#      xy_momentum_sex major_axis_sex minor_axis_sex position_angle_sex fwhm_sex flag_sex}
+# }
+# {USNO2 {ra dec poserr mag magerr} 
+#        {} 
+# }
+::console::affiche_resultat "[lindex $listsources 0]  \n"
 
-   # Affichage des ronds verts
-   affich_rond $astrometric_list "TYCHO2" "blue" 2
-   ::console::affiche_resultat "ROND BLEU: Etoiles tycho 2\n"
+   set unesource [lindex [lindex $listsources 1] 10]
+   ::console::affiche_resultat "$unesource \n"
+   set ra  [lindex [lindex [lindex $unesource 0] 1] 0]
+   set dec [lindex [lindex [lindex $unesource 0] 1] 1]
+   set x [lindex [lindex [lindex $unesource 0] 2] 2]
+   set y [lindex [lindex [lindex $unesource 0] 2] 3]
+   set fwhm [lindex [lindex [lindex $unesource 0] 2] 24]
+   ::console::affiche_resultat "X $x / Y $y / FWHM $fwhm\n"
 
-   set astrometric_list [add_sun_star "TYCHO2" $astrometric_list]
+   set source [list {"SOURCE" {} {} } [list [list [list "SOURCE" [list $ra $dec] {}]]]]
+   affich_rond $source "SOURCE" "green" 1
 
-   # Affichage des ronds verts
-   affich_rond $astrometric_list "SUNLIKE" "yellow" 2
-   ::console::affiche_resultat "ROND JAUNE: Etoiles de type solaire\n"
+   gren_info "[::confVisu::screen2Canvas  $bddconf(visuno) [list $x $y]]\n"
+   gren_info "[::confVisu::canvas2Picture $bddconf(visuno) [list $x $y]]\n"
 
-   set starident [ identification $listsources "USNOA2" $astrometric_list "TYCHO2" 30.0 10.0 10.0 ]
-   affich_rond $starident "TYCHO2" "red" 2
+   affich_un_rond_xy $x $y "green" 5  4
+
+   set results [ ::av4l_photom::photom_methode $x $y $fwhm $bddconf(bufno) ]
+   set x2 [lindex $results 0]
+   set y2 [lindex $results 1]
+   ::console::affiche_resultat "X $x2 / Y $y2 \n"
+   affich_un_rond_xy $x2 $y2 "blue" 5  2
+   ::console::affiche_resultat "diff X [expr abs($x2-$x)] / Y [expr abs($y2-$y)] \n"
+   
+   gren_info "$results\n"
+
+ #   ::visu::delete $bddconf(visuno)
+
 
    # FIN
    gren_info "****************************************************************** \n"
    gren_info "FIN de script\n"
+
+
+# buf1 load  /astrodata/Observations/Images/bddimages/bddimages_cador/fits/tarot_chili/2009/11/19/IM_20091119_055755761_230749_54410102.fits.gz
+# set autocuts [buf1 autocuts]
+# visu1 disp [list [lindex $autocuts 0] [lindex $autocuts 1]]
+# loadima /astrodata/Observations/Images/bddimages/bddimages_cador/fits/tarot_chili/2009/11/19/IM_20091119_055755761_230749_54410102.fits.gz
