866f0f6f50e10eb98b8e88e5f0adf77df5ede4f3
bmauclaire <bmauclaire@04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-12-24 14:12:40 +0000

    spc_dynagraph : spectre dynamique d'une serie (finalisée) SpcAudace 2.12 - 25/12/2011

--- trunk/audela/gui/audace/plugin/tool/spcaudace/spc_var.tcl
+++ trunk/audela/gui/audace/plugin/tool/spcaudace/spc_var.tcl
@@ -12,7 +12,7 @@ global audela audace
 global spcaudace
 
 #--- Version d'SpcAudace :
-set spcaudace(version) "2.12 - 11/12/2011"
+set spcaudace(version) "2.12 - 25/12/2011"
 # ::audace::date_sys2ut ?Date?
 #set spcaudace(version) [ file mtime $spcaudace(repspc) ]
 
