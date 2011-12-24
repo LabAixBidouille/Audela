c216f323e08a5b8fa459187663c580c1a694a149
bmauclaire <bmauclaire@04217e9b-e68c-4f9b-beca-5a77afb74039>
2011-12-24 14:24:25 +0000

    spc_dynagraph : raccourci dans le menu Astrophysique

--- trunk/audela/gui/audace/plugin/tool/spcaudace/spc_gui.tcl
+++ trunk/audela/gui/audace/plugin/tool/spcaudace/spc_gui.tcl
@@ -288,6 +288,7 @@ proc spc_winini { } {
       .spc.menuBar.analyse add command -label $caption(spcaudace,gui,spc_npne_w) -command "spc_npne" -underline 0
       .spc.menuBar.analyse add command -label $caption(spcaudace,gui,spc_normahbeta) -command "spc_normahbeta" -underline 0
       .spc.menuBar.analyse add command -label $caption(spcaudace,gui,spc_file_space)
+      .spc.menuBar.analyse add command -label $caption(spcaudace,gui,spc_dynagraph) -command "spc_dynagraph" -underline 0
       .spc.menuBar.analyse add command -label $caption(spcaudace,gui,spc_periodogram) -command "spc_periodogram" -underline 0
       .spc.menuBar.analyse add command -label $caption(spcaudace,gui,spc_file_space)
       .spc.menuBar.analyse add command -label $caption(spcaudace,gui,spc_spectrum) -command "spc_spectrum" -underline 0
