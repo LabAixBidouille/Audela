2c1f0b462e49553ddba63929d0450eea6ee4dfc3
myrtillelaas <myrtillelaas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2012-04-20 10:30:26 +0000

    mise en page ML

--- trunk/audela/src/external/meinberg/src/libmeinberg.h
+++ trunk/audela/src/external/meinberg/src/libmeinberg.h
@@ -3,7 +3,7 @@
  * This file is part of the AudeLA project : <http://software.audela.free.fr>
  * Copyright (C) 1998-2004 The AudeLA Core Team
  *
- * Initial author : Alain KLOTZ <alain.klotz@free.fr>
+ * Initial author : Myrtille Laas-Bourez <Myrtille.Laas@oca.eu> 
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
