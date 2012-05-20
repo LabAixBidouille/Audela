67dd913cf00ccaa126feb98b1905be36a747d1d4
Frédéric VACHIER <vachier@imcce.fr>
2012-05-20 12:39:58 +0000

    suppression plugin ros

--- audela/gui/audace/plugin/tool/ros/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Mise à jour $Id$
-#
-
-package ifneeded ros 1.0 [ list source [ file join $dir ros_go.tcl ] ]
-
