e41ea359d0f27427cb490365d79ecbd8ec54d14d
Stéphane VAILLANT <vaillant@imcce.fr>
2012-09-01 11:15:37 +0000

    amelioration du script de lancement sous linux

--- audela/bin/audela.sh
+++ audela/bin/audela.sh
@@ -2,7 +2,19 @@
 #------ Benjamin Mauclaire 2004 for AudeLA's team ---------#
 
 echo -e "Lancement AudeLA..."
-chemin=`pwd`
-typeset -x LD_LIBRARY_PATH=$chemin
-#${chemin}/audela $*
-./audela $*
+
+link=`readlink -f $0`
+
+if [ -z "$link" ]; then
+ chemin=`dirname $0`
+else
+ chemin=`dirname $link`
+fi
+
+if [ -z "$LD_LIBRARY_PATH" ]; then
+ typeset -x LD_LIBRARY_PATH=$chemin
+else
+ typeset -x LD_LIBRARY_PATH="$chemin:$LD_LIBRARY_PATH"
+fi
+
+exec ${chemin}/audela "$@"
