7b1dba3e67a5ed9498ce3733b3343a754252208f
Frédéric VACHIER <vachier@imcce.fr>
2012-09-08 17:27:55 +0000

    bdi : creation du cata , wcs mode manuel, en chantier

---
+++ audela/gui/audace/plugin/tool/bddimages/bdi_tools_image.tcl
@@ -0,0 +1,191 @@
+#--------------------------------------------------
+# source audace/plugin/tool/bddimages/bddimages_admin_image.tcl
+#--------------------------------------------------
+#
+# Fichier        : bddimages_admin_image.tcl
+# Description    : Environnement de gestion des listes d images
+#                  dans la base de donnees
+# Auteur         : Frédéric Vachier
+# Mise à jour $Id: bddimages_liste.tcl 6969 2011-03-16 17:12:35Z fredvachier $
+#
+#--------------------------------------------------
+# - Namespace bddimages_admin_image
+# - Fichiers source externe :
+#       bddimages_liste.cap
+#--------------------------------------------------
+#
+#  Structure de la liste image
+#
+# {               -- debut de liste
+#
+#   {             -- debut d une image
+#
+#     {ibddimg 1}
+#     {ibddcata 2}
+#     {filename toto.fits.gz}
+#     {dirfilename /.../}
+#     {filenametmp toto.fit}
+#     {cataexist 1}
+#     {cataloaded 1}
+#     ...
+#     {tabkey {{NAXIS1 1024} {NAXIS2 1024}} }
+#     {cata {{{IMG {ra dec ...}{USNO {...]}}}} { { {IMG {4.3 -21.5 ...}} {USNOA2 {...}} } {source2} ... } } }
+#
+#   }             -- fin d une image
+#
+# }               -- fin de liste
+#
+#--------------------------------------------------
+#
+#  Structure du tabkey
+#
+# { {NAXIS1 1024} {NAXIS2 1024} etc ... }
+#
+#--------------------------------------------------
+#
+#  Structure du cata
+#
+# {               -- debut structure generale
+#
+#  {              -- debut des noms de colonne des catalogues
+#
+#   { IMG   {list field crossmatch} {list fields}} 
+#   { TYC2  {list field crossmatch} {list fields}}
+#   { USNO2 {list field crossmatch} {list fields}}
+#
+#  }              -- fin des noms de colonne des catalogues
+#
+#  {              -- debut des sources
+#
+#   {             -- debut premiere source
+#
+#    { IMG   {crossmatch} {fields}}  -> vue dans l image
+#    { TYC2  {crossmatch} {fields}}  -> vue dans le catalogue
+#    { USNO2 {crossmatch} {fields}}  -> vue dans le catalogue
+#
+#   }             -- fin premiere source
+#
+#  }              -- fin des sources
+#
+# }               -- fin structure generale
+#
+#--------------------------------------------------
+#
+#  Structure intellilist_i (dite inteligente)
+#
+#
+# {
+#   {name               ...  }
+#   {datemin            ...  }
+#   {datemax            ...  }
+#   {type_req_check     ...  }
+#   {type_requ          ...  }
+#   {choix_limit_result ...  }
+#   {limit_result       ...  }
+#   {type_result        ...  }
+#   {type_select        ...  }
+#   {reqlist           { 
+#                        { valide     ... }
+#                        { condition  ... }
+#                        { champ      ... }
+#                        { valeur     ... }
+#                      }
+#
+#   }
+#
+# }
+#
+#--------------------------------------------------
+#
+#  Structure intellilist_n (dite normale)
+#
+#
+# {
+#   {name               ...  }
+#   {datemin            ...  }
+#   {datemax            ...  }
+#   {type_req_check     ...  }
+#   {type_requ          ...  }
+#   {choix_limit_result ...  }
+#   {limit_result       ...  }
+#   {type_result        ...  }
+#   {type_select        ...  }
+#   {reqlist            { 
+#                         {image_34 {134 345 677}}
+#                         {image_38 {135 344 679}}
+#                       }
+#
+#   }
+#
+# }
+#
+#--------------------------------------------------
+
+namespace eval bdi_tools_image {
+
+   global audace
+   global bddconf
+
+
+
+
+   # Lecture du Tabkkey depuis le buffer
+   proc ::bdi_tools_image::get_tabkey_from_buffer {  } {
+   
+      set listtab [buf$::audace(bufNo) getkwds]
+      set tabkey {}
+      foreach key $listtab {
+         lappend tabkey [list $key [buf$::audace(bufNo) getkwd $key] ]
+      }
+      set result  [bddimages_entete_preminforecon $tabkey]
+      set err     [lindex $result 0]
+      
+      if {$err} {
+         return -code $err "Lecture du TABKEY impossible\n"
+      }
+      
+      return [lindex $result 1]
+
+   }
+
+
+   proc ::bdi_tools_image::clean_bdi_tabkey { tabkey } {
+
+
+      foreach key $tabkey {
+           
+      }
+
+   }
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#--- Fin Classe
+
+}
