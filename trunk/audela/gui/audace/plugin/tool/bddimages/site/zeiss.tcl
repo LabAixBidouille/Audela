afd54dc1e4b96bac499dc74523edcaf315953f40
fredvachier <fredvachier@04217e9b-e68c-4f9b-beca-5a77afb74039>
2012-09-13 11:47:11 +0000

    bdi: amelioration photometrique et identification des sources

--- trunk/audela/gui/audace/plugin/tool/bddimages/site/zeiss.tcl
+++ trunk/audela/gui/audace/plugin/tool/bddimages/site/zeiss.tcl
@@ -2,7 +2,81 @@
 # Mise à jour $Id$
 #
 
+#--------------------------------------------------
+#
+#  Structure du tabkey
+#
+# { {TELESCOP { {TELESCOP} {TAROT CHILI} string {Observatory name} } }
+#   {NAXIS2   { {NAXIS2}   {1024}        int    {}                 } }
+#    etc ...
+# }
+#
+#--------------------------------------------------
+#     0         1         2
+#       0123456789012345678901
+# date <2006-06-23T20:22:36.08>
+#--------------------------------------------------
+
 proc chg_tabkey {tabkey} {
+
+   set dateobs [lindex [::bddimages_liste::lget $tabkey "frame"] 1]
+
+   if { [regexp {(\d+)-(\d+)-(\d+)( |T)(\d+):(\d+):(\d+)(\.*\d*)} $dateobs dateiso aa mm jj sep h m s sd] } {
+
+      # Si date ISO Correcte
+      set dateiso "${aa}-${mm}-${jj}T${h}:${m}:${s}${sd}"
+
+      set key_dateiso [lreplace [::bddimages_liste::lget $tabkey "date-obs"] 1 1 $dateiso]
+      set tabkey [::bddimages_liste::lupdate $tabkey "date-obs" $key_dateiso]
+
+      set key_foclen  [list "FOCLEN" 7.973 "double" "Focal lenght" "m"]   
+      set tabkey [ ::bddimages_liste::ladd $tabkey "foclen" $key_foclen ]
+
+      set key_bin1 [::bddimages_liste::lget $tabkey "hbin"]
+      set key_bin1 [lreplace $key_bin1 0 0 "BIN1"]
+      set bin1     [lindex $key_bin1 1]
+      set tabkey   [ ::bddimages_liste::ladd $tabkey "bin1" $key_bin1 ]
+
+      set key_bin2 [::bddimages_liste::lget $tabkey "vbin"]
+      set key_bin2 [lreplace $key_bin2 0 0 "BIN2"]
+      set bin2     [lindex $key_bin2 1]
+      set tabkey   [ ::bddimages_liste::ladd $tabkey "bin2" $key_bin2 ]
+      
+      set pixsize1     [expr $bin1 * 13.5]
+      set key_pixsize1 [list "PIXSIZE1" $pixsize1 "double" "Pixel Size" "um"]   
+      set tabkey       [ ::bddimages_liste::ladd $tabkey "pixsize1" $key_pixsize1 ]
+      
+      set pixsize2     [expr $bin2 * 13.5]
+      set key_pixsize2 [list "PIXSIZE2" $pixsize2 "double" "Pixel Size" "um"]   
+      set tabkey       [ ::bddimages_liste::ladd $tabkey "pixsize2" $key_pixsize2 ]
+      
+      set key_exposure [::bddimages_liste::lget $tabkey "exposure"]
+      set exposure     [lindex $key_exposure 1]
+      set exposure     [regsub {,} $exposure "."]
+      set key_exposure [lreplace $key_exposure 1 1 $exposure]
+      set tabkey       [ ::bddimages_liste::lupdate $tabkey "exposure" $key_exposure ]
+
+
+      set key_uaicode  [list "IAU_CODE" 874 "int" "Observatory uai code" ""]   
+      set tabkey [ ::bddimages_liste::ladd $tabkey "iau_code" $key_uaicode ]
+
+
+      #::console::affiche_resultat "RA=$ra ($rah)\n"
+      #::console::affiche_resultat "EXPOSURE=$exposure\n"
+
+ 
+      return [list 0 $tabkey]
+
+   } else {
+
+      # Sinon
+      return [list 1 "-"]
+
+   }
+
+}
+
+proc chg_tabkey_avant_20120101 {tabkey} {
 
    #     0         1         2
    #       0123456789012345678901
