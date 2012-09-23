cee50b38ab48e6051bfad21e0956de8c7f0afd91
robertdelmas <robertdelmas @04217e9b-e68c-4f9b-beca-5a77afb74039>
2012-09-23 09:17:07 +0000

    Convertion UTF8.

--- trunk/audela/gui/audace/plugin/tool/echip/echip.tcl
+++ trunk/audela/gui/audace/plugin/tool/echip/echip.tcl
@@ -2,7 +2,7 @@
 # Fichier : echip.tcl
 # Description : GUI de la proc electronic_chip (surchaud.tcl)
 # Auteur : Raymond Zachantke
-# Mise � jour $Id$
+# Mise à jour $Id$
 #
 
 #============================================================
@@ -93,7 +93,7 @@ namespace eval ::echip {
 
    #------------------------------------------------------------
    # startTool : affiche la fenetre de l'outil
-   #  Parametres : N° de la visu
+   #  Parametres : NÂ° de la visu
    #------------------------------------------------------------
    proc startTool { visuNo } {
       variable private
@@ -105,7 +105,7 @@ namespace eval ::echip {
 
    #------------------------------------------------------------
    # stopTool : masque la fenetre de l'outil
-   #  Parametres : N° de la visu
+   #  Parametres : NÂ° de la visu
    #------------------------------------------------------------
    proc stopTool { visuNo } {
 
