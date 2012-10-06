ac845d08ff2bf02aa8dc9ed4a19662bb05ef3864
alainklotz <alainklotz@04217e9b-e68c-4f9b-beca-5a77afb74039>
2012-10-06 17:31:17 +0000

    Test de lecture des etats d'un joystick utilisant le wrapper SDL. C'est très intéressant d'examiner ce code pour intégrer la commande manuelle des télescopes (tel1 radec move, etc.) controllée avec un joystick.

---
+++ trunk/audela/gui/audace/scripts/joystick1.tcl
@@ -0,0 +1,75 @@
+# source $audace(rep_install)/gui/audace/scripts/joystick1.tcl
+# see functions in the folder audela/lib/mkLibsdl1.0
+
+package require mkLibsdl
+
+proc joy_event {} {
+   global base
+	set res [joystick event peek]
+	catch {$base.f.lab_value configure -text "$res"}
+}
+
+proc joy_infos {} {
+   global base
+	set count [joystick count]
+	console::affiche_resultat "There are $count joysticks connected\n"
+	for {set joyindex 0} {$joyindex<$count} {incr joyindex} {
+		console::affiche_resultat "=========================\n"
+		console::affiche_resultat "joystick index = $joyindex\n"
+		set name [joystick name $joyindex]
+		console::affiche_resultat "joystick name $joyindex = $name\n"
+		set opts {axe ball hat button}
+		console::affiche_resultat "-----------------------\n"
+		foreach opt $opts {
+			set joy($opt) [joystick info $joyindex ${opt}s]
+			console::affiche_resultat "joystick info $joyindex $opt = $joy($opt)\n"
+		}
+		console::affiche_resultat "-----------------------\n"
+		foreach opt $opts {
+			for {set control 0} {$control<$joy($opt)} {incr control} {
+				if {$opt!="axe"} { 
+					set opt2 $opt 
+				} else { 
+					set opt2 axis 
+				}
+				set value [joystick get $joyindex $opt2 $control]
+				console::affiche_resultat "joystick get $joyindex $opt2 $control = $value\n"
+			}				
+		}		
+	}
+}
+
+#--- Create the toplevel window
+set base .joystick1
+catch {destroy .joystick1}
+toplevel $base -class Toplevel
+wm geometry $base 300x100+10+10
+wm focusmodel $base passive
+wm minsize $base 300 100
+wm resizable $base 1 1
+wm deiconify $base
+wm title $base "Joystick"
+wm protocol $base WM_DELETE_WINDOW joysitck1_fermer
+bind $base <Destroy> { destroy .joystick1 }
+$base configure -bg #123456
+wm withdraw .
+focus -force $base
+
+frame $base.f -bg #123456
+   label $base.f.lab_titre \
+      -bg #123456 -fg #AAAA00 \
+      -text "The last joystick event is"
+   pack $base.f.lab_titre
+   label $base.f.lab_value \
+      -bg #123456 -fg #FFFF00 \
+      -text "nothing"
+   pack $base.f.lab_value
+pack $base.f -fill both
+
+proc joysitck1_fermer { } {
+   global base
+   destroy $base
+}
+
+joystick event eval joy_event
+joy_infos
