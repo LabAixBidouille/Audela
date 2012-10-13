0c8cee7eaba2e554cf7a88cea51409ca75f3eafd
fredvachier <fredvachier@04217e9b-e68c-4f9b-beca-5a77afb74039>
2012-10-13 18:35:53 +0000

    cloture du plugin av4l

--- trunk/audela/gui/audace/plugin/tool/av4l/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Mise à jour $Id$
-#
-
-package ifneeded av4l 1.0 [ list source [ file join $dir av4l_go.tcl ] ]
-
