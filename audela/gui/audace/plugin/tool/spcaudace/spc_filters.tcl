57999d80f3acc111475082c4b2511e1f7a73f0d4
patricklailly <patricklailly@04217e9b-e68c-4f9b-beca-5a77afb74039>
2012-10-13 08:44:00 +0000

    correction de bug dans spc_passebas

--- audela/gui/audace/plugin/tool/spcaudace/spc_filters.tcl
+++ audela/gui/audace/plugin/tool/spcaudace/spc_filters.tcl
@@ -557,7 +557,8 @@ proc spc_passebas { args } {
       set nordonnees3 [ lrange $ordonnees_ini $i_sup end ]
       set newordonnees [ concat $nordonnees1 $nordonnees $nordonnees3 ]
       set crval1 [ lindex $abscisses_ini 0 ]
-      set file_out [ spc_fileupdate $fichier $crval1 $dx $newordonnees ""]
+      set suff _pbas
+      set file_out [ spc_fileupdate $fichier $crval1 $dx $newordonnees $suff ]
       return $file_out
    } else {
       ::console::affiche_erreur "Usage: spc_passebas profil_de_raies.fit ? demi-largeur motif à gommer(25)?\n\n"
