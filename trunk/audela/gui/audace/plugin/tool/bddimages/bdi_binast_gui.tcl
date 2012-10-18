1c9d6c27aadc3c0a73fbf893a13dda559bd2f6e8
fredvachier <fredvachier@04217e9b-e68c-4f9b-beca-5a77afb74039>
2012-10-18 10:28:17 +0000

    BINAST IHM

--- trunk/audela/gui/audace/plugin/tool/bddimages/bdi_binast_gui.tcl
+++ trunk/audela/gui/audace/plugin/tool/bddimages/bdi_binast_gui.tcl
@@ -4,24 +4,33 @@ namespace eval bdi_binast_gui {
 
    proc ::bdi_binast_gui::inittoconf {  } {
 
-      global bddconf, conf
-      set ::tools_astrometry::science   "SKYBOT"
-      set ::tools_astrometry::reference "UCAC3"
-      set ::tools_astrometry::delta 15
-      set ::tools_astrometry::treshold 5
-      set ::gui_astrometry::factor 1000
-      set ::tools_cata::id_current_image 0
 
-      if {! [info exists ::tools_astrometry::ifortlib] } {
-         if {[info exists conf(bddimages,cata,ifortlib)]} {
-            set ::tools_astrometry::ifortlib $conf(bddimages,cata,ifortlib)
-         } else {
-            set ::tools_astrometry::ifortlib "/opt/intel/lib/ia32"
-         }
-      }
+
+      catch { unset ::bdi_binast_gui::fen                  }
+      catch { unset ::bdi_binast_gui::nbstars              }
+      catch { unset ::bdi_binast_gui::directaccess         }
+      catch { unset ::bdi_binast_gui::stateback            }
+      catch { unset ::bdi_binast_gui::statenext            }
+      catch { unset ::bdi_binast_gui::block                }
+      catch { unset ::bdi_binast_tools::nomobj             }
+      catch { unset ::bdi_binast_tools::savedir            }
+      catch { unset ::bdi_binast_tools::uncosm             }
+      catch { unset ::bdi_binast_tools::uncosm_param1      }
+      catch { unset ::bdi_binast_tools::uncosm_param2      }
+      catch { unset ::bdi_binast_tools::firstmagref        }
+      catch { unset ::bdi_binast_tools::current_image_name }
+      catch { unset ::bdi_binast_tools::current_image_date }
+      catch { unset ::bdi_binast_tools::id_current_image   }
+      catch { unset ::bdi_binast_tools::nb_img_list        }
+      catch { unset ::bdi_binast_tools::tabsource          }
+      catch { unset ::bdi_binast_tools::firstmagref        }
 
    }
    
+
+
+
+
 
 
    
