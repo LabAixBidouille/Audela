1e7d450b4e5a4be2f058d198858093798c93bec6
alainklotz <alainklotz@04217e9b-e68c-4f9b-beca-5a77afb74039>
2013-01-05 13:09:45 +0000

    Preparation de SN_Tarot au telescope Zadko.

--- trunk/audela/gui/audace/plugin/tool/sn_tarot/sn_tarot_go.tcl
+++ trunk/audela/gui/audace/plugin/tool/sn_tarot/sn_tarot_go.tcl
@@ -11,7 +11,7 @@
 #============================================================
 namespace eval ::sn_tarot {
    package provide sn_tarot 1.0
-
+   
    #--- Chargement des captions pour recuperer le titre utilise par getPluginLabel
    source [ file join [file dirname [info script]] sn_tarot_go.cap ]
    source [ file join [file dirname [info script]] sn_tarot.cap ]
@@ -113,7 +113,7 @@ proc ::sn_tarot::deletePluginInstance { visuNo } {
 #------------------------------------------------------------
 proc ::sn_tarot::createPanel { this } {
    variable This
-   global conf panneau rep
+   global conf panneau rep audace
 
    package require http
 
@@ -121,13 +121,20 @@ proc ::sn_tarot::createPanel { this } {
    set panneau(init_dir) [ file join $dir tarot ]
    set rep(archives) "[ file join $panneau(init_dir) archives ]" ; # chemin du repertoire des archives
    set rep(name1)    "[ file join $panneau(init_dir) night ]" ; # chemin du repertoire images de la nuit
-   set rep(name2)    "[ file join $panneau(init_dir) references ] " ; # chemin du repertoire images de reference galtarot
-   set rep(name3)    "[ file join $panneau(init_dir) dss ]" ; # chemin du repertoire images de reference dss
+   #set rep(name2)    "[ file join $panneau(init_dir) references ] " ; # chemin du repertoire images de reference galtarot
+   #set rep(name3)    "[ file join $panneau(init_dir) dss ]" ; # chemin du repertoire images de reference dss
 
    set panneau(sn_tarot,Tarot_Calern,url)    "http://tarot6.obs-azur.fr/ros/supernovae/zip/"
    set panneau(sn_tarot,Tarot_Chili,url)     "http://tarotchili5.oamp.fr/ros/supernovae/zip/"
+   set panneau(sn_tarot,Zadko_Australia,url) "http://121.200.43.11/ros/supernovae/zip/"
    set panneau(sn_tarot,ohp,url)             "http://cador.obs-hp.fr/tarot/"
-
+   
+   set audace(sn_tarot,ok_zadko) 0
+   set hostname [lindex [hostaddress] end]
+   if {$hostname=="astrostar"} {
+      set audace(sn_tarot,ok_zadko) 1
+   }
+   
    ::sn_tarot::updateFiles
    vwait panneau(sn_tarot,init)
 
@@ -167,7 +174,7 @@ proc ::sn_tarot::stopTool { visuNo } {
 proc ::sn_tarot::tarotBuildIF { } {
    variable This
    global audace caption panneau
-
+   
    frame $This -borderwidth 2 -relief groove
 
    #--- Frame du titre
@@ -187,6 +194,9 @@ proc ::sn_tarot::tarotBuildIF { } {
 
    #---
    set list_prefix [ list Tarot_Calern Tarot_Chili ]
+   if {$audace(sn_tarot,ok_zadko)==1} {
+      lappend list_prefix Zadko_Australia
+   }
    set width [expr {int([::tkutil::lgEntryComboBox $list_prefix]*6/7)}]
    ComboBox $This.fra2.site -width $width -relief sunken -borderwidth 1 \
       -textvariable panneau(sn_tarot,prefix) \
@@ -346,9 +356,10 @@ proc ::sn_tarot::Explore { } {
 proc ::sn_tarot::listArchives { } {
    global audace panneau rep snconfvisu
 
-   #--   liste le contenu du dossier archives ; masque l'exension .zip
+   #--   liste le contenu du dossier archives ; masque l'extension .zip
    regsub -all ".zip" [ glob -nocomplain -type f -tails -dir $rep(archives) *.zip ] "" list_archives
    #--   masque refgaltarot et dss
+   regsub "refgalzadko" $list_archives "" list_archives
    regsub "refgaltarot" $list_archives "" list_archives
    regsub "dss" $list_archives "" list_archives
 
@@ -372,7 +383,7 @@ proc ::sn_tarot::listArchives { } {
 #-----------------------------------------------------
 proc ::sn_tarot::updateFiles { } {
    global audace caption conf panneau
-
+   
    #--   raccourci
    set fram $audace(base).sn_tarot0
 
@@ -401,9 +412,54 @@ proc ::sn_tarot::updateFiles { } {
    set dir "$panneau(init_dir)"
    set subdir [ file join $dir references ]
    set dssdir [ file join $dir dss ]
+   set subdir_zadko [ file join $dir references_zadko ]
+   set dssdir_zadko [ file join $dir dss_zadko ]
+   #
    set list_of_data [ list folder folder "$dir" calern available "Tarot_Calern" \
       chili available "Tarot_Chili" ref ref refgaltarot.zip \
       refunzip refunzip "$subdir" dss dss dss.zip dssunzip dssunzip "$dssdir" ]
+   #
+   set list_of_data ""   
+   lappend list_of_data folder 
+   lappend list_of_data folder 
+   lappend list_of_data "$dir"
+   lappend list_of_data calern
+   lappend list_of_data available
+   lappend list_of_data "Tarot_Calern"
+   lappend list_of_data chili
+   lappend list_of_data available
+   lappend list_of_data "Tarot_Chili"
+   lappend list_of_data ref
+   lappend list_of_data ref
+   lappend list_of_data refgaltarot.zip
+   lappend list_of_data refunzip
+   lappend list_of_data refunzip
+   lappend list_of_data "$subdir"
+   lappend list_of_data dss
+   lappend list_of_data dss
+   lappend list_of_data dss.zip
+   lappend list_of_data dssunzip
+   lappend list_of_data dssunzip
+   lappend list_of_data "$dssdir"
+   if {$audace(sn_tarot,ok_zadko)==1} {
+      lappend list_of_data zadko
+      lappend list_of_data available
+      lappend list_of_data "Zadko_Australia"
+      lappend list_of_data refzadko
+      lappend list_of_data ref
+      lappend list_of_data refgalzadko.zip
+      lappend list_of_data refzadkounzip
+      lappend list_of_data refunzip
+      lappend list_of_data "$subdir_zadko"
+      lappend list_of_data dsszadko
+      lappend list_of_data dss
+      lappend list_of_data dss_zadko.zip
+      lappend list_of_data dsszadkounzip
+      lappend list_of_data dssunzip
+      lappend list_of_data "$dssdir_zadko"
+   }
+   #
+      
    foreach { child lab val } $list_of_data {
       checkbutton $fram.dat.$child -text [ format $caption(sn_tarot_go,$lab) $val ] \
          -indicatoron 1 -onvalue 1 -offvalue 0 -state disabled \
@@ -444,7 +500,12 @@ proc ::sn_tarot::sn_tarotStart { } {
 
    #--   cree les repertoires s'ils n'existent pas
    set dir $panneau(init_dir)
-   foreach sub_path [ list archives night dss ] {
+   set ls [ list archives night dss ]
+   if {$audace(sn_tarot,ok_zadko)==1} {
+      lappend ls references_zadko
+      lappend ls dss_zadko
+   }
+   foreach sub_path $ls {
       set rep($sub_path) [ file join $dir $sub_path ]
       if { ![ file exists $rep($sub_path) ] } {
          file mkdir $rep($sub_path)
@@ -455,9 +516,13 @@ proc ::sn_tarot::sn_tarotStart { } {
    update
 
    #--
-   foreach { prefix url var } [ list \
-     "Tarot_Calern" $panneau(sn_tarot,Tarot_Calern,url) calern \
-     "Tarot_Chili" $panneau(sn_tarot,Tarot_Chili,url) chili ] {
+   set ls ""
+   lappend ls "Tarot_Calern"    ; lappend ls "$panneau(sn_tarot,Tarot_Calern,url)" ; lappend ls calern
+   lappend ls "Tarot_Chili"     ; lappend ls "$panneau(sn_tarot,Tarot_Chili,url)" ; lappend ls chili
+   if {$audace(sn_tarot,ok_zadko)==1} {
+      lappend ls "Zadko_Australia" ; lappend ls "$panneau(sn_tarot,Zadko_Australia,url)" ; lappend ls zadko
+   }
+   foreach { prefix url var } $ls {
       ::sn_tarot::inventaire $prefix $url
       set panneau(sn_tarot,ini_$var) 1
       update
@@ -518,6 +583,65 @@ proc ::sn_tarot::sn_tarotStart { } {
    }
    #set panneau(sn_tarot,ini_refunzip) 1
    update
+   
+   if {$audace(sn_tarot,ok_zadko)==1} {
+
+      #--   si necessaire, telecharge refgalzadko.zip dans archives
+      set file [ file join $rep(archives) refgalzadko.zip ]
+      if { ![ file exists $file ] } {
+         ::sn_tarot::downloadFile $panneau(sn_tarot,ohp,url) refgalzadko.zip $file
+      }
+      set panneau(sn_tarot,ini_ref) 1
+      update
+
+      #--   si necessaire, dezippe refgaltarot.zip
+      set panneau(sn_tarot,references_zadko) [ file join $dir references_zadko ]
+
+      set nb [ llength [ glob -nocomplain -type f -tails -dir $panneau(sn_tarot,references_zadko) *$conf(extension,defaut) ] ]
+      if { $nb == 0 } {
+         #--   chemin de unzip.exe
+         if { $::tcl_platform(os) == "Linux" } {
+            set tarot_unzip unzip
+         } else {
+            set tarot_unzip [ file join $audace(rep_plugin) tool sn_tarot unzip.exe ]
+         }
+         #--   dezippe refgaltarot.zip
+         catch { exec $tarot_unzip -u -d $dir $file } ErrInfo
+
+         #--   change le nom du repertoire en reference
+         if {![file exists $panneau(sn_tarot,references_zadko)] && [ file exists [ file join $dir refgalzadko ] ] } {
+            file rename -force [ file join $dir refgalzadko ] $panneau(sn_tarot,references_zadko)
+         }
+      }
+      set panneau(sn_tarot,ini_refunzip) 1
+      update
+
+      #--   si necessaire, telecharge dss_zadko.zip dans archives
+      set file [ file join $rep(archives) dss_zadko.zip ]
+      if { ![ file exists $file ] } {
+         ::sn_tarot::downloadFile $panneau(sn_tarot,ohp,url) dss_zadko.zip $file
+      }
+      set panneau(sn_tarot,ini_ref) 1
+      update
+
+      #--   si necessaire, dezippe dss_zadko.zip
+      set panneau(sn_tarot,dss_zadko) [ file join $dir .. dss ]
+
+      set nb [ llength [ glob -nocomplain -type f -tails -dir $panneau(sn_tarot,dss_zadko) *$conf(extension,defaut) ] ]
+      if { $nb == 0 } {
+         #--   chemin de unzip.exe
+         if { $::tcl_platform(os) == "Linux" } {
+            set tarot_unzip unzip
+         } else {
+            set tarot_unzip [ file join $audace(rep_plugin) tool sn_tarot unzip.exe ]
+         }
+         #--   dezippe dss.zip
+         catch { exec $tarot_unzip -u -d $dir $file } ErrInfo
+
+      }
+      update
+      
+   }
 
    ::sn_tarot::changeUpdateState normal
 
@@ -562,6 +686,8 @@ proc ::sn_tarot::inventaire { prefix url } {
 
    #--   cree la liste des dates specifique a chaque telescope
    lassign [ ::sn_tarot::httpcopy $prefix $url ] error list_zip
+   
+   set typetel [lindex [split $prefix _] 0]
 
    if { $error eq "0" } {
 
@@ -569,6 +695,7 @@ proc ::sn_tarot::inventaire { prefix url } {
       switch -exact $prefix {
          Tarot_Calern {set home {GPS 6.92353 E 43.75203 1320} }
          Tarot_Chili  {set home {GPS 70.7326 W -29.259917 2398} }
+         Zadko_Australia {set home {GPS 115.7140 E -31.3567 50} }
       }
 
       #--   recherche la date courante et le creneau horaire
@@ -591,7 +718,11 @@ proc ::sn_tarot::inventaire { prefix url } {
 
       #--   si echec de connexion au site, liste le contenu du dossier archives
       #--   masque refgaltarot
-      regsub -all "refgaltarot" [ glob -nocomplain -type f -dir $rep(archives) -tails *.zip ] "" list_archives
+      set repref refgaltarot
+      if {$typetel=="Zadko"} {
+         set repref refgalzadko
+      }
+      regsub -all "$repref" [ glob -nocomplain -type f -dir $rep(archives) -tails *.zip ] "" list_archives
       foreach archive $list_archives {
          regexp "${prefix}_(\[0-9\]\{8\})\.zip" $archive match date
          if { [ info exists date ] } {
