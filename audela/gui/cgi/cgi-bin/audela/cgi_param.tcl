a52698091609befca06aaf4d6e0bb012bf6f7c11
robertdelmas <Robert Delmas>
2013-02-02 17:51:27 +0000

    Erreur de ma part (retour arriere).

--- audela/gui/cgi/cgi-bin/audela/cgi_param.tcl
+++
@@ -1,4 +0,0 @@
-set cgi(audela,rep_userCatalog) "D:/catalog"
-set cgi(audela,rep_images) "C:/Documents and Settings/Robert/Mes documents/audela/images"
-set cgi(audela,home) {GPS 1.376722 E 43.659778 142}
-set cgi(audela,rep_catalogues) "C:/Astronomie/AudeLA SourceForge/audela/gui/audace/catalogues"
