33fed581013ea2e9de9488ac60870383536d8131
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2013-02-09 22:59:43 +0000

    Acceleration de l'ouverture des fenetres : - Configuration de la camera, - Configuration de la monture, - Configuration de la liaison, en executant la recherche des ports COM dans un autre thread (by Michel).

---
+++ trunk/audela/gui/audace/plugin/link/serialport/searchport.tcl
@@ -0,0 +1,84 @@
+#
+# Fichier : searchport.tcl
+# Description : Recherche des ports COMs disponibles
+# Auteurs : Robert DELMAS et Michel PUJOL
+# Mise à jour $Id$
+#
+
+#------------------------------------------------------------
+#  searchPorts
+#     recherche les ports com disponible sur le PC
+#
+#  return rien
+#------------------------------------------------------------
+proc ::searchPorts { mainThreadNo port_exclus } {
+   variable private
+   global conf
+
+   #--- Recherche le ou les ports COM exclus
+   set kd ""
+   set port_exclus ""
+   set nbr_port_exclus [ llength $port_exclus ]
+   if { $nbr_port_exclus == "1" } {
+      set kd [string index $port_exclus [expr [string length $port_exclus]-1]]
+   } else {
+      for { set i 0 } { $i < $nbr_port_exclus } { incr i } {
+         set a [ lindex $port_exclus $i ]
+         set kdd [string index $a [expr [string length $a]-1]]
+         lappend kd $kdd
+         set kd [ lsort $kd ]
+      }
+   }
+
+   #--- Suivant l'OS
+   if { $::tcl_platform(os) == "Linux" } {
+      set port_com     "/dev/ttyS"
+      set port_com_usb "/dev/ttyUSB"
+      set kk  "0"
+      set kkt "20"
+   } else {
+      set port_com "COM"
+      set kk  "1"
+      set kkt "20"
+   }
+
+   #--- Recherche des ports com
+   set comlist            ""
+   set comlist_usb        ""
+   set private(portsList) ""
+
+   set i "0"
+   for { set k $kk } { $k < $kkt } { incr k } {
+      if { $k != "[ lindex $kd $i]" } {
+         set errnum [ catch { open $port_com$k r+ } msg ]
+         if { $errnum == "0" } {
+            lappend comlist $k
+            close $msg
+         }
+      } else {
+         incr i
+      }
+   }
+   set long_com [ llength $comlist ]
+
+   for { set k 0 } { $k < $long_com } { incr k } {
+      lappend private(portsList) "$port_com[ lindex $comlist $k ]"
+   }
+
+   if { $::tcl_platform(os) == "Linux" } {
+      for { set k $kk } { $k < 20 } { incr k } {
+         set errnum [ catch { open $port_com_usb$k r+ } msg ]
+         if { $errnum == "0" } {
+            lappend comlist_usb $k
+            close $msg
+         }
+      }
+      set long_com_usb [ llength $comlist_usb ]
+
+      for { set k 0 } { $k < $long_com_usb } { incr k } {
+         lappend private(portsList) "$port_com_usb[ lindex $comlist_usb $k ]"
+      }
+   }
+   thread::send $mainThreadNo "set ::serialport::private(portList) \"$private(portsList)\" "
+}
+
