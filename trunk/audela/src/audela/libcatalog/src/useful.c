c5788e43629d34c065fe88ef5de2ffd7afbb9e58
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2013-03-24 13:44:48 +0000

    Corrections mineures (remplacement de 2 "usage :" par "usage:" comme tous les autres usages).

--- trunk/audela/src/audela/libcatalog/src/useful.c
+++ trunk/audela/src/audela/libcatalog/src/useful.c
@@ -16,13 +16,13 @@ int decodeInputs(char* const outputLogChar, const int argc, char* const argv[],
 
 
 	if((argc == 2) && (strcmp(argv[1],"-h") == 0)) {
-		sprintf(outputLogChar,"Help usage : %s pathOfCatalog ra(deg) dec(deg) radius(arcmin) magnitudeMin(mag)? magnitudeMax(mag)?",
+		sprintf(outputLogChar,"Help usage: %s pathOfCatalog ra(deg) dec(deg) radius(arcmin) magnitudeMin(mag)? magnitudeMax(mag)?",
 				argv[0]);
 		return (1);
 	}
 
 	if((argc != 5) && (argc != 7)) {
-		sprintf(outputLogChar,"usage : %s pathOfCatalog ra(deg) dec(deg) radius(arcmin) magnitudeMax(mag)? magnitudeMin(mag)?",
+		sprintf(outputLogChar,"usage: %s pathOfCatalog ra(deg) dec(deg) radius(arcmin) magnitudeMax(mag)? magnitudeMin(mag)?",
 				argv[0]);
 		return (1);
 	}
