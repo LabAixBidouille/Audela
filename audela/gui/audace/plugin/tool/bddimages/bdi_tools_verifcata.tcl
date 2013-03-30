d76d1a92e01f4c835c235b9cbcbad7238abf3263
Frédéric VACHIER <vachier@imcce.fr>
2013-03-30 10:42:48 +0000

    gestion psf et verification

--- audela/gui/audace/plugin/tool/bddimages/bdi_tools_verifcata.tcl
+++ audela/gui/audace/plugin/tool/bddimages/bdi_tools_verifcata.tcl
@@ -16,6 +16,7 @@ namespace eval tools_verifcata {
          foreach {idd listsources} [array get ::gui_cata::cata_list] {
 
             set satraster 0
+            set catadouble 0
             set err 0
 
             set sources [lindex $listsources 1] 
@@ -25,8 +26,18 @@ namespace eval tools_verifcata {
                set star  "n"
                set aster "n"
                set catas ""
+               set doubl "n"
                foreach cata $s {
                   set name [lindex $cata 0]
+                  
+                  # Test si plusieurs catas du meme nom pour une meme source
+                  set p [lsearch $catas $name]
+                  if {$p!=-1} {
+                     incr err
+                     incr catadouble
+                     set doubl "y"
+                  }
+                  # identifie la source, stellaire ou systeme solaire
                   append catas " $name"
                   switch $name {
                       "SKYBOT" { set aster "y" } 
@@ -36,13 +47,20 @@ namespace eval tools_verifcata {
                       default {}
                   }
                }
+               # Denomination de la source
                set namable [::manage_source::namable $s]
                if {$namable==""} {
                   set namesource ""
                } else {
                   set namesource [::manage_source::naming $s $namable]
                } 
-
+               
+               # Test si plusieurs catas du meme nom pour une meme source
+               if {$doubl == "y"} {
+                  lappend source_list [list $ids $idd $idd "DoubleCata" $namesource $catas]
+               }
+               
+               # Test si la source est a la fois stellaire et systeme solaire
                if {$star == "y" && $aster == "y"} {
                   lappend source_list [list $ids $idd $idd "Star&Aster" $namesource $catas]
                   incr satraster
