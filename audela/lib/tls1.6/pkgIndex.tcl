add5284c2d249e4723103f8fc4e4a6416821d947
mschiav <mschiav@04217e9b-e68c-4f9b-beca-5a77afb74039>
2013-06-06 08:39:57 +0000

    Reverted unwanted changes in the last commit

--- audela/lib/tls1.6/pkgIndex.tcl
+++ audela/lib/tls1.6/pkgIndex.tcl
@@ -1,3 +1,3 @@
-package ifneeded tls 1.6 \
+package ifneeded tls 1.6.1 \
     "[list source [file join $dir tls.tcl]] ; \
-     [list tls::initlib $dir libtls1.6.so]"
+     [list tls::initlib $dir tls161.dll]"
