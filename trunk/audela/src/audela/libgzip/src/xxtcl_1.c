2e7e621f0d0548f241e5fcc9037bc733925347b6
jberthier <jberthier@04217e9b-e68c-4f9b-beca-5a77afb74039>
2013-06-25 08:17:56 +0000

    Maj Audela - bug gzip

--- trunk/audela/src/audela/libgzip/src/xxtcl_1.c
+++ trunk/audela/src/audela/libgzip/src/xxtcl_1.c
@@ -81,7 +81,7 @@ int Cmd_xxtcl_gzip(ClientData clientData, Tcl_Interp *interp, int argc, char *ar
       if (isfile==1) {
          strcpy(lignetcl,"set gzip_name");
          Tcl_Eval(interp,lignetcl);
-         sprintf(lignetcl,"%s \"%s\"",argv[0],Tcl_GetStringResult(interp));
+         sprintf(lignetcl,"%s \"%s\"",strndup(argv[0]+2,strlen(argv[0])),Tcl_GetStringResult(interp));
          Tcl_SplitList(interp,lignetcl,&argcc,(char***)&argvv);
          result=gzipmain(argcc,argvv);
          if (result!=2) {
@@ -96,4 +96,3 @@ int Cmd_xxtcl_gzip(ClientData clientData, Tcl_Interp *interp, int argc, char *ar
    Tcl_SetResult(interp,lignetcl,TCL_VOLATILE);
    return TCL_OK;
 }
-
