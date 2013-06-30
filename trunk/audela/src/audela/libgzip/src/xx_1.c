2b029782be7a88f4e99bbaecd6775a8db1f4a6da
alainklotz <alainklotz@04217e9b-e68c-4f9b-beca-5a77afb74039>
2013-06-30 17:58:58 +0000

    Ajout de la fonction strndup dans xx_1.c pour assurer la compatibilité Windows/Linux.

--- trunk/audela/src/audela/libgzip/src/xx_1.c
+++ trunk/audela/src/audela/libgzip/src/xx_1.c
@@ -29,5 +29,21 @@
 /***************************************************************************/
 #include "xx.h"
 
+#if defined (OS_WIN) 
+char *strndup (const char *s, size_t n)
+{
+  char *result;
+  size_t len = strlen (s);
 
+  if (n < len)
+    len = n;
+
+  result = (char *) malloc (len + 1);
+  if (!result)
+    return 0;
+
+  result[len] = '\0';
+  return (char *) memcpy (result, s, len);
+}
+#endif
 
