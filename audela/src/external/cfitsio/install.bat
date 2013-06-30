581946a18129e70ba47aad2f75e1eb8037103a04
alainklotz <Alain Klotz>
2013-06-30 01:25:51 +0000

    Update de FITSIO (3210->3340)

--- audela/src/external/cfitsio/install.bat
+++ audela/src/external/cfitsio/install.bat
@@ -3,7 +3,7 @@ rem  parametre %1 = Release ou Debug
 rem Si pas de parametre , la valeur par defaut est Release
 if not "%1" == "" set CONFIG=%1
 if "%1" == "" set CONFIG=Release
-set FITSIO=..\cfitsio3210
+set FITSIO=..\cfitsio3340
 @echo on
 mkdir ..\..\include
 mkdir ..\..\lib
