35cbc097fbab36606f017bb794923a1631713418
robertdelmas <robertdelmas@04217e9b-e68c-4f9b-beca-5a77afb74039>
2013-07-21 07:41:07 +0000

    Suppression le l'onglet "Carte du Ciel V2" (outil de cartographie obsolète).

--- trunk/audela/gui/audace/plugin/chart/carteducielv2/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#
-# Fichier : pkgIndex.tcl
-# Description : Definition du point d'entree du plugin
-# Mise à jour $Id$
-#
-
-package ifneeded carteducielv2 1.1 [ list source [ file join $dir carteducielv2.tcl ] ]
-
