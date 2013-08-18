6dd1ca8c028dfe0187d7b4dc98619d21ecaca353
alainklotz <alainklotz@04217e9b-e68c-4f9b-beca-5a77afb74039>
2013-08-18 21:48:39 +0000

    Ajout d'un delais avant de tuer le thread telescope lorsque l'on ferme le driver telscript.

--- trunk/audela/src/libtel/libtelscript/src/telescop.c
+++ trunk/audela/src/libtel/libtelscript/src/telescop.c
@@ -767,6 +767,7 @@ int tel_close(struct telprop *tel)
 {
 	// close the channel
 	telthread->exit=1; // envoie un signal de sortie
+	libtel_sleep(500); // attente superieur a la duree d'une boucle
 	my_pthread_mutex_unlock(&mutex);
    pthread_mutexattr_destroy(&mutexAttr);
    pthread_mutex_destroy(&mutex);
