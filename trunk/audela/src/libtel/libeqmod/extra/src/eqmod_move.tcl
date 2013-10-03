290e3c964ff3b865d635015d65f55ab7555d03a7
fredvachier <fredvachier@04217e9b-e68c-4f9b-beca-5a77afb74039>
2013-10-03 08:01:38 +0000

    heq5 fred

--- trunk/audela/src/libtel/libeqmod/extra/src/eqmod_move.tcl
+++ trunk/audela/src/libtel/libeqmod/extra/src/eqmod_move.tcl
@@ -2,130 +2,103 @@
 #   avec l aide et conseil de Alain KLOTZ <alain.klotz@free.fr>
 
 # source /srv/develop/audela/src/libtel/libeqmod/extra/src/lowlevel_funcs.tcl
+# source [file join $audace(rep_install) src libtel libeqmod extra src eqmod_move.tcl]
 
 namespace eval eqmod_move {
 
 
 
    proc ::eqmod_move::move_to_north_pole_counterweight_bottom { } {
-
-      move_to_coord_celestial -90 90
-
-      # move_to_coord_mount 0 90
-
+      ::eqmod::move_to_coord_celestial -90 90
+      return 0
    }
 
    proc ::eqmod_move::move_to_north_pole_counterweight_east { } {
-
-      set r [celestial_to_mount -180 90]
-      set hm [lindex $r 0]
-      set dm [lindex $r 1]
-
-      move_to_coord_mount $hm $dm
-
-      # move_to_coord_mount 90 90
-
+      ::eqmod::move_to_coord_celestial -180 90
+      return 0
    }
 
    proc ::eqmod_move::move_to_north_pole_counterweight_west { } {
-
-      set r [celestial_to_mount 0 90]
-      set hm [lindex $r 0]
-      set dm [lindex $r 1]
-
-      move_to_coord_mount $hm $dm
-
-      # move_to_coord_mount -90 90
-
+      ::eqmod::move_to_coord_celestial 0 90
+      return 0
    }
 
-
-   proc ::eqmod_move::move_to_east { } {
-
-      set r [celestial_to_mount -90 180]
-      set hm [lindex $r 0]
-      set dm [lindex $r 1]
-
-      move_to_coord_mount $hm $dm
-
-      # move_to_coord_mount 0 0
-
+   proc ::eqmod_move::move_to_east_counterweight_top { } {
+      ::eqmod::move_to_coord_celestial -90 0
+      return 0
    }
 
-   proc ::eqmod_move::move_to_east2 { } {
-
-      set r [celestial_to_mount 90 0]
-      set hm [lindex $r 0]
-      set dm [lindex $r 1]
-
-      move_to_coord_mount $hm $dm
-
-      # move_to_coord_mount -180 180
-
+   proc ::eqmod_move::move_to_east_counterweight_bottom { } {
+      ::eqmod::move_to_coord_celestial 90 180
+      return 0
    }
 
-   proc ::eqmod_move::move_to_west { } {
-
-      set r [celestial_to_mount -90 0]
-      set hm [lindex $r 0]
-      set dm [lindex $r 1]
-
-      move_to_coord_mount $hm $dm
-
-      #move_to_coord_mount 0 180
-
+   proc ::eqmod_move::move_to_west_counterweight_top { } {
+      ::eqmod::move_to_coord_celestial -90 180
+      return 0
    }
 
-   proc ::eqmod_move::move_to_west2 { } {
-
-      set r [celestial_to_mount -270 180]
-      set hm [lindex $r 0]
-      set dm [lindex $r 1]
-
-      move_to_coord_mount $hm $dm
-
-      #move_to_coord_mount 180 0
-
+   proc ::eqmod_move::move_to_west_counterweight_bottom { } {
+      ::eqmod::move_to_coord_celestial 90 0
+      return 0
    }
 
    proc ::eqmod_move::move_to_south { } {
-
       set l [lindex $::eqmod::home 3]
-      ::console::affiche_resultat "Lat : $l\n"
-      set de2 [expr 180 + $l]
-
-      move_to_coord_mount -90 $de2
-
+      set d [expr $l -90]
+      ::eqmod::move_to_coord_celestial 0 $d
+      return 0
    }
 
    proc ::eqmod_move::move_to_zenith { } {
-
       set l [lindex $::eqmod::home 3]
-      set de2 [expr 90 + $l]
-      move_to_coord_mount -90 $de2
-
+      ::eqmod::move_to_coord_celestial 0 $l
+      return 0
    }
 
    proc ::eqmod_move::move_to_north { } {
-
       set l [lindex $::eqmod::home 3]
-      move_to_coord_mount -90 $l
+      set d [expr $l + 90]
+      ::eqmod::move_to_coord_celestial 0 $d
+      return 0
    }
 
    proc ::eqmod_move::move_to_equator_south { } {
-
-      move_to_coord_mount -90 180
-
+      ::eqmod::move_to_coord_celestial 0 0
+      return 0
    }
 
    proc ::eqmod_move::move_to_equator_south_m1H { } {
-
-      move_to_coord_mount -75 180
-
+      ::eqmod::move_to_coord_celestial -15 0
+      return 0
    }
    proc ::eqmod_move::move_to_equator_south_p1H { } {
+      ::eqmod::move_to_coord_celestial 15 0
+      return 0
+   }
 
-      move_to_coord_mount -105 180
+   proc ::eqmod_move::diurnal_motion { a } {
+   
+      if {$a=="start"} {
+         tel$::eqmod::telno put :K1
+         tel$::eqmod::telno put :G110
+         tel$::eqmod::telno put :I16C0200
+         tel$::eqmod::telno put :J1
+      }
+      if {$a=="stop"} {
+         tel$::eqmod::telno put :K1
+      }
+   }
+
+
+
+   proc ::eqmod_move::move_to_coord_equatorial { ra_deg dec_deg } {
+
+      set r       [::eqmod::coord_equatorial_to_hour $ra_deg $dec_deg]
+      set h_deg   [lindex $r 0]
+      set dec_deg [lindex $r 1]
+      
+      ::eqmod::move_to_coord_celestial $h_deg $dec_deg
 
    }
 
