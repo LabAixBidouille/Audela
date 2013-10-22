4d8a63e94cfce73d983a4f89a957d1750074b8ef
alainklotz <alainklotz@04217e9b-e68c-4f9b-beca-5a77afb74039>
2013-10-22 19:53:50 +0000

    Début d'écriture du plugin d'observations robotiques par thèmes.

--- trunk/audela/gui/audace/plugin/tool/robobs/loopscript_light_curve.tcl
+++ trunk/audela/gui/audace/plugin/tool/robobs/loopscript_light_curve.tcl
@@ -27,12 +27,43 @@ if {($robobs(planif,mode)=="asteroid_light_curve")&&($robobs(image,ffilenames)!=
    set dec [lindex [buf$bufNo getkwd DEC] 1]		
    ::robobs::log "Compute the light curve of $objename."
    photrel_cat2mes tmp $objename $ra $dec C
+   # --- display results
+   set f [open "$robobs(conf,folders,rep_images,value)/$objename.txt" r]
+   set lignes [split [read $f] \n]
+   close $f
+   set jds ""
+   set mags ""
+   set errmags ""
+   foreach ligne $lignes {
+      set car [string index $ligne 0]
+      set err [catch {expr $car+1} msg]
+      if {($err==1)||([llength $ligne]<4)} {
+         continue
+      }
+      lappend jds [lindex $ligne 0]
+      lappend mags [lindex $ligne 1]
+      lappend errmags  [lindex $ligne 2]
+   }
+   # --- Graphics
+   if {[llength $jds]>1} {
+      ::plotxy::figure 1
+      ::plotxy::clf
+      ::plotxy::caption "$objename"
+      ::plotxy::plot $jds $mags or
+      ::plotxy::plotbackground #FFFFFF
+      ::plotxy::ydir reverse
+      ::plotxy::bgcolor #FFFFFF
+      ::plotxy::ylabel "relative mag"
+      ::plotxy::xlabel "JD UT"
+      ::plotxy::title "$objename Light-curve"
+      ::plotxy::writegif "$robobs(conf,folders,rep_images,value)/${objename}.gif"
+   }
+   
    # --- Copy the light curve for the web site
    if {$robobsconf(webserver)==1} {
-      lassign [visu1 cut] sh sb
-      buf$bufNo savejpeg $robobsconf(webserver,htdocs)/robobs_last.jpg 60 $sb $sh
       file mkdir $robobsconf(webserver,htdocs)/rrlyr/$robobs(private,nightdate)
-      file copy -force -- $robobsconf(webserver,htdocs)/$starname.txt $robobsconf(webserver,htdocs)/rrlyr/$robobs(private,nightdate)/${starname}.txt
+      file copy -force -- $robobs(conf,folders,rep_images,value)/${objename}.txt $robobsconf(webserver,htdocs)/rrlyr/$robobs(private,nightdate)/${objename}.txt
+      file copy -force -- $robobs(conf,folders,rep_images,value)/${objename}.gif $robobsconf(webserver,htdocs)/rrlyr/$robobs(private,nightdate)/${objename}.gif
    }      
 }
 
