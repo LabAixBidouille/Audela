280854194333fdd053d9d3aa18199cefb2dc0321
alainklotz <alainklotz@04217e9b-e68c-4f9b-beca-5a77afb74039>
2013-10-23 04:47:25 +0000

    Début d'écriture du plugin d'observations robotiques par thèmes.

--- trunk/audela/gui/audace/plugin/tool/robobs/loopscript_light_curve.tcl
+++ trunk/audela/gui/audace/plugin/tool/robobs/loopscript_light_curve.tcl
@@ -14,60 +14,65 @@ if {($robobs(planif,mode)=="asteroid_light_curve")&&($robobs(image,ffilenames)!=
    ::robobs::log "Light curve : robobs(image,ffilenames)=$robobs(image,ffilenames)"
    set index 0
    foreach fname $robobs(image,ffilenames) {
-      incr index
       buf$bufNo load $fname
-      set fname "$robobs(conf,folders,rep_images,value)/tmp${index}$robobs(conf,fichier_image,extension,value)"      
-      buf$bufNo save $fname
+      set catastar [lindex buf$bufNo getkwd CATASTAR] 1]
+      if {$catastar>10} {
+         incr index
+         set fname "$robobs(conf,folders,rep_images,value)/tmp${index}$robobs(conf,fichier_image,extension,value)"      
+         buf$bufNo save $fname
+      }
    }
    set ni $index
-   ::robobs::log "photrel_wcs2cat tmp $ni append"
-   photrel_wcs2cat tmp $ni append
-   set objename [lindex [buf$bufNo getkwd OBJENAME] 1]
-   set ra [lindex [buf$bufNo getkwd RA] 1]
-   set dec [lindex [buf$bufNo getkwd DEC] 1]		
-   ::robobs::log "Compute the light curve of $objename."
-   photrel_cat2mes tmp $objename $ra $dec C
-   # --- display results
-   set f [open "$robobs(conf,folders,rep_images,value)/$objename.txt" r]
-   set lignes [split [read $f] \n]
-   close $f
-   set jds ""
-   set mags ""
-   set errmags ""
-   foreach ligne $lignes {
-      set car [string index $ligne 0]
-      set err [catch {expr $car+1} msg]
-      if {($err==1)||([llength $ligne]<3)} {
-         continue
+   if {$ni>0} {
+      ::robobs::log "photrel_wcs2cat tmp $ni append"
+      photrel_wcs2cat tmp $ni append
+      set objename [lindex [buf$bufNo getkwd OBJENAME] 1]
+      set ra [lindex [buf$bufNo getkwd RA] 1]
+      set dec [lindex [buf$bufNo getkwd DEC] 1]		
+      ::robobs::log "Compute the light curve of $objename."
+      photrel_cat2mes tmp $objename $ra $dec C
+      # --- display results
+      set f [open "$robobs(conf,folders,rep_images,value)/$objename.txt" r]
+      set lignes [split [read $f] \n]
+      close $f
+      set jds ""
+      set mags ""
+      set errmags ""
+      foreach ligne $lignes {
+         set car [string index $ligne 0]
+         set err [catch {expr $car+1} msg]
+         if {($err==1)||([llength $ligne]<3)} {
+            continue
+         }
+         lappend jds [lindex $ligne 0]
+         lappend mags [lindex $ligne 1]
+         lappend errmags  [lindex $ligne 2]
       }
-      lappend jds [lindex $ligne 0]
-      lappend mags [lindex $ligne 1]
-      lappend errmags  [lindex $ligne 2]
-   }
-   # --- Graphics
-   ::robobs::log "Light curve contains [llength $jds] dates"
-   if {[llength $jds]>1} {
-      ::plotxy::figure 1
-      ::plotxy::clf
-      ::plotxy::caption "$objename"
-      ::plotxy::plot $jds $mags or
-      ::plotxy::plotbackground #FFFFFF
-      ::plotxy::ydir reverse
-      ::plotxy::bgcolor #FFFFFF
-      ::plotxy::ylabel "relative mag"
-      ::plotxy::xlabel "JD UT"
-      ::plotxy::title "$objename Light-curve"
-      ::plotxy::writegif "$robobs(conf,folders,rep_images,value)/${objename}.gif"
-   }
-   
-   # --- Copy the light curve for the web site
-   if {$robobsconf(webserver)==1} {
-      file mkdir $robobsconf(webserver,htdocs)/rrlyr/$robobs(private,nightdate)
-      catch {
-         file copy -force -- $robobs(conf,folders,rep_images,value)/${objename}.txt $robobsconf(webserver,htdocs)/rrlyr/$robobs(private,nightdate)/${objename}.txt
-         file copy -force -- $robobs(conf,folders,rep_images,value)/${objename}.gif $robobsconf(webserver,htdocs)/rrlyr/$robobs(private,nightdate)/${objename}.gif
+      # --- Graphics
+      ::robobs::log "Light curve contains [llength $jds] dates"
+      if {[llength $jds]>1} {
+         ::plotxy::figure 1
+         ::plotxy::clf
+         ::plotxy::caption "$objename"
+         ::plotxy::plot $jds $mags or
+         ::plotxy::plotbackground #FFFFFF
+         ::plotxy::ydir reverse
+         ::plotxy::bgcolor #FFFFFF
+         ::plotxy::ylabel "relative mag"
+         ::plotxy::xlabel "JD UT"
+         ::plotxy::title "$objename Light-curve"
+         ::plotxy::writegif "$robobs(conf,folders,rep_images,value)/${objename}.gif"
       }
-   }      
+      
+      # --- Copy the light curve for the web site
+      if {$robobsconf(webserver)==1} {
+         file mkdir $robobsconf(webserver,htdocs)/rrlyr/$robobs(private,nightdate)
+         catch {
+            file copy -force -- $robobs(conf,folders,rep_images,value)/${objename}.txt $robobsconf(webserver,htdocs)/rrlyr/$robobs(private,nightdate)/${objename}.txt
+            file copy -force -- $robobs(conf,folders,rep_images,value)/${objename}.gif $robobsconf(webserver,htdocs)/rrlyr/$robobs(private,nightdate)/${objename}.gif
+         }
+      }      
+   }
 }
 
 # === End of script
