48b099b8f398844df0556e933fd5a326f4997738
jacquesmichelet <jacquesmichelet@04217e9b-e68c-4f9b-beca-5a77afb74039>
2014-01-19 07:56:40 +0000

    diminution du niveau de trace dans le fichier log

--- trunk/audela/src/libcam/libgrabber/src/camera.cpp
+++ trunk/audela/src/libcam/libgrabber/src/camera.cpp
@@ -202,7 +202,7 @@ struct _PrivateParams {
 #define LOG_WARNING 2
 #define LOG_INFO    3
 #define LOG_DEBUG   4
-int webcam_debug_level = LOG_DEBUG;
+int webcam_debug_level = LOG_INFO;
 
 #include <time.h>
 #include <sys/timeb.h>          /* ftime, struct timebuffer */
