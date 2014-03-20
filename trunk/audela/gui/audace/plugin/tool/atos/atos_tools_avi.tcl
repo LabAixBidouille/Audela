b7fb253cd764c104218565185ec60e24957799ab
fredvachier <fredvachier@04217e9b-e68c-4f9b-beca-5a77afb74039>
2014-03-20 12:42:57 +0000

    metis

--- trunk/audela/gui/audace/plugin/tool/atos/atos_tools_avi.tcl
+++ trunk/audela/gui/audace/plugin/tool/atos/atos_tools_avi.tcl
@@ -208,9 +208,8 @@ namespace eval ::atos_tools_avi {
       }
       #::console::affiche_resultat "next_image fin : $::atos_tools::cur_idframe \n"
 
-      set nbf [expr  $::atos_tools::nb_frames * 1.0]
-      set pc [expr ($::atos_tools::cur_idframe-1) / ($nbf+1.0) ]
-      #::console::affiche_resultat "next_image idframe = $::atos_tools::cur_idframe ; pc = $pc\n"
+      set pc [expr ($::atos_tools::cur_idframe-1) / ($::atos_tools::nb_frames+1.0) ]
+      ::console::affiche_resultat "next_image idframe = $::atos_tools::cur_idframe ; pc = $pc\n"
 
    }
 
@@ -282,15 +281,15 @@ namespace eval ::atos_tools_avi {
       set ::atos_tools::cur_idframe [expr int($idframe)]
       set pc [expr ($idframe-1) / ($nbf+1.0) ]
 
-      #::console::affiche_resultat "set_frame idframe = $idframe ; pc = $pc\n"
+      ::console::affiche_resultat "set_frame idframe = $idframe ; pc = $pc\n"
 
       ::atos_tools_avi::avi1 seektoframe [expr $idframe -1 ]
       #::atos_tools_avi::avi1 seekpercent $pc
 
-      set ::atos_tools::cur_idframe [expr int($idframe-1)]
+      set ::atos_tools::cur_idframe [expr $idframe -1]
 
       ::atos_tools_avi::next_image
-      #::console::affiche_resultat "set_frame next_image cur_idframe = $::atos_tools::cur_idframe\n"
+      ::console::affiche_resultat "set_frame next_image cur_idframe = $::atos_tools::cur_idframe\n"
 
       set ::atos_tools::cur_idframe [expr int($idframe)]
       #::console::affiche_resultat "set_frame cur_idframe fin = $::atos_tools::cur_idframe\n"
