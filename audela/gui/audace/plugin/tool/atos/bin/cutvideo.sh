491cd6ba8273c07194311504c74e55a42387cfa9
Jérôme BERTHIER <berthier@imcce.fr>
2014-06-19 15:51:53 +0000

    Maj atos - script cutvideo

---
+++ audela/gui/audace/plugin/tool/atos/bin/cutvideo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Coupe une portion de video et reformattage codec (huff) 
+#
+# usage: $0 video start end
+# avec start et end exprimes en seconde
+#
+video=$1
+start=$2
+end=$3
+
+if [ "x$start" == "x" -o "x$end" == "x" ]; then
+   ffmpeg -i $video -map 0:v -c:v huffyuv ${video%%.*}-huff.avi
+else
+   ffmpeg -ss $start -t $end -i $video -map 0:v -c:v huffyuv ${video%%.*}-huff-crop.avi
+fi
+
